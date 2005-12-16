2c6156085c68facdfd288aeeb795b3b284171e9f
James Strachan <jstrachan@apache.org>
2005-12-16 17:48:17 +0000

    removed dead code
    git-svn-id: https://svn.apache.org/repos/asf/incubator/activemq/trunk@357194 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-core/src/main/java/org/activemq/broker/Main.java
+++ activemq-core/src/main/java/org/activemq/broker/Main.java
@@ -176,8 +176,6 @@ public class Main {
 
     public String getVersion() throws Throwable {
         ClassLoader cl = getClassLoader();
-        // Use reflection to get teh version
-        Object broker;
         try {
             Class activeMQConnectionMetaData = cl.loadClass("org.activemq.ActiveMQConnectionMetaData");
             Field field = activeMQConnectionMetaData.getField("PROVIDER_VERSION");
