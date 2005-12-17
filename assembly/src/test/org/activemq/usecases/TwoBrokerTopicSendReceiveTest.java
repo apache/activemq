41d63b355a54197839cbc6d9a50a0fdcdaa44695
Adrian T. Co <aco@apache.org>
2005-12-17 05:49:15 +0000

    Ported some of the broker network config files to the new xml format. Added initial test cases to test the configurations.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/activemq/trunk@357304 13f79535-47bb-0310-9956-ffa450edef68

---
+++ assembly/src/test/org/activemq/usecases/TwoBrokerTopicSendReceiveTest.java
@@ -0,0 +1,79 @@
+/** 
+ * 
+ * Copyright 2004 Protique Ltd
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ * 
+ **/
+package org.activemq.usecases;
+
+import javax.jms.Connection;
+import javax.jms.JMSException;
+
+import org.activemq.ActiveMQConnectionFactory;
+import org.activemq.broker.TransportConnector;
+import org.activemq.broker.BrokerService;
+import org.activemq.xbean.BrokerFactoryBean;
+import org.activemq.test.JmsTopicSendReceiveWithTwoConnectionsTest;
+import org.springframework.core.io.ClassPathResource;
+
+/**
+ * @version $Revision: 1.1.1.1 $
+ */
+public class TwoBrokerTopicSendReceiveTest extends JmsTopicSendReceiveWithTwoConnectionsTest {
+
+    protected ActiveMQConnectionFactory sendFactory;
+    protected ActiveMQConnectionFactory receiveFactory;
+
+    protected void setUp() throws Exception {
+        sendFactory = createSenderConnectionFactory();
+        receiveFactory = createReceiverConnectionFactory();
+        super.setUp();
+    }
+
+    protected ActiveMQConnectionFactory createReceiverConnectionFactory() throws JMSException {
+        return createConnectionFactory("org/activemq/usecases/receiver.xml", "receiver", "vm://receiver");
+    }
+
+    protected ActiveMQConnectionFactory createSenderConnectionFactory() throws JMSException {
+        return createConnectionFactory("org/activemq/usecases/sender.xml", "sender", "vm://sender");
+    }
+
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    protected Connection createReceiveConnection() throws JMSException {
+        return receiveFactory.createConnection();
+    }
+
+    protected Connection createSendConnection() throws JMSException {
+        return sendFactory.createConnection();
+    }
+
+    protected ActiveMQConnectionFactory createConnectionFactory(String config, String brokerName, String connectUrl) throws JMSException {
+        try {
+            BrokerFactoryBean brokerFactory = new BrokerFactoryBean(new ClassPathResource(config));
+            brokerFactory.afterPropertiesSet();
+
+            BrokerService broker = brokerFactory.getBroker();
+            ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(((TransportConnector)broker.getTransportConnectors().get(0)).getConnectUri());
+
+            return factory;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ assembly/src/test/org/activemq/usecases/TwoBrokerTopicSendReceiveUsingHttpTest.java
@@ -0,0 +1,36 @@
+/**
+ *
+ * Copyright 2004 Protique Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **/
+package org.activemq.usecases;
+
+import org.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.JMSException;
+
+/**
+ * @version $Revision: 1.1.1.1 $
+ */
+public class TwoBrokerTopicSendReceiveUsingHttpTest extends TwoBrokerTopicSendReceiveTest {
+
+    protected ActiveMQConnectionFactory createReceiverConnectionFactory() throws JMSException {
+        return createConnectionFactory("org/activemq/usecases/receiver-http.xml", "receiver", "vm://receiver");
+    }
+
+    protected ActiveMQConnectionFactory createSenderConnectionFactory() throws JMSException {
+        return createConnectionFactory("org/activemq/usecases/sender-http.xml", "sender", "vm://sender");
+    }
+}
