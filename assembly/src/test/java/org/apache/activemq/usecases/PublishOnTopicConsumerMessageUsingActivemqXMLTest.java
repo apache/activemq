e0c143f4883a2788cb203b888ab13cced26f9d74
Hiram R. Chirino <chirino@apache.org>
2005-12-28 19:42:01 +0000

    doing package refactor
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/activemq/trunk@359609 13f79535-47bb-0310-9956-ffa450edef68

---
+++ assembly/src/test/java/org/apache/activemq/usecases/PublishOnTopicConsumerMessageUsingActivemqXMLTest.java
@@ -0,0 +1,115 @@
+/**
+ *
+ * Copyright 2004 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.activemq.usecases;
+
+import org.apache.activemq.broker.BrokerService;
+import org.apache.activemq.xbean.BrokerFactoryBean;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import java.io.File;
+
+/**
+ *
+ * Test Publish/Consume topic  using the release activemq.xml configuration file
+ *
+ * @version $Revision: 1.2 $
+ */
+public class PublishOnTopicConsumerMessageUsingActivemqXMLTest extends PublishOnTopicConsumedMessageTest {
+    protected static final String JOURNAL_ROOT = "../data/";
+    BrokerService broker;
+
+
+
+     /**
+     * Use the transportConnector uri configured on the activemq.xml
+     *
+     * @return ActiveMQConnectionFactory
+     * @throws Exception
+     */
+    protected ActiveMQConnectionFactory createConnectionFactory() throws Exception {
+        return new ActiveMQConnectionFactory("tcp://localhost:61616");
+    }
+
+
+    /**
+     * Sets up a test where the producer and consumer have their own connection.
+     *
+     * @see junit.framework.TestCase#setUp()
+     */
+    protected void setUp() throws Exception {
+        ;
+        File journalFile = new File(JOURNAL_ROOT);
+        recursiveDelete(journalFile);
+        // Create broker from resource
+        System.out.print("Creating broker... ");
+        broker = createBroker("org/apache/activemq/usecases/activemq.xml");
+        System.out.println("Success");
+        super.setUp();
+
+    }
+
+
+
+     /*
+     * Stops the Broker
+     * @see junit.framework.TestCase#tearDown()
+     */
+    protected void tearDown() throws Exception {
+         System.out.println("Closing Broker");
+            if (broker != null) {
+               broker.stop();
+            }
+         System.out.println("Broker closed...");
+
+
+    }
+
+
+
+
+
+    /*
+     * clean up the journal
+     */
+
+    protected static void recursiveDelete(File file) {
+        if( file.isDirectory() ) {
+            File[] files = file.listFiles();
+            for (int i = 0; i < files.length; i++) {
+                recursiveDelete(files[i]);
+            }
+        }
+        file.delete();
+    }
+
+    protected BrokerService createBroker(String resource) throws Exception {
+        return createBroker(new ClassPathResource(resource));
+    }
+
+    protected BrokerService createBroker(Resource resource) throws Exception {
+        BrokerFactoryBean factory = new BrokerFactoryBean(resource);
+        factory.afterPropertiesSet();
+
+        BrokerService broker = factory.getBroker();
+
+        //assertTrue("Should have a broker!", broker != null);
+
+
+        return broker;
+    }
+}
---
+++ assembly/src/test/java/org/apache/activemq/usecases/TransactionRollbackOrderTest.java
@@ -0,0 +1,161 @@
+/**
+ *
+ * Copyright 2004 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.activemq.usecases;
+
+import edu.emory.mathcs.backport.java.util.concurrent.CountDownLatch;
+import junit.framework.TestCase;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.activemq.command.ActiveMQTopic;
+
+import javax.jms.Connection;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Test case for AMQ-268
+ *
+ * @author Paul Smith
+ * @version $Revision: 1.1 $
+ */
+public final class TransactionRollbackOrderTest extends TestCase {
+
+    private volatile String receivedText;
+
+    private Session producerSession;
+    private Session consumerSession;
+    private Destination queue;
+
+    private MessageProducer producer;
+    private MessageConsumer consumer;
+    private Connection connection;
+    private CountDownLatch latch = new CountDownLatch(1);
+    private int NUM_MESSAGES = 5;
+    private List msgSent = new ArrayList();
+    private List msgCommitted = new ArrayList();
+    private List msgRolledBack = new ArrayList();
+    private List msgRedelivered = new ArrayList();
+
+    public void testTransaction() throws Exception {
+
+        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory("vm://localhost?broker.persistent=false");
+
+        connection = factory.createConnection();
+        queue = new ActiveMQQueue(getClass().getName() + "." + getName());
+
+        producerSession = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        consumerSession = connection.createSession(true, 0);
+
+        producer = producerSession.createProducer(queue);
+
+        consumer = consumerSession.createConsumer(queue);
+        consumer.setMessageListener(new MessageListener() {
+
+            int msgCount = 0;
+            int msgCommittedCount = 0;
+
+            public void onMessage(Message m) {
+                try {
+                    msgCount++;
+                    TextMessage tm = (TextMessage) m;
+                    receivedText = tm.getText();
+
+                    if (tm.getJMSRedelivered()) {
+                        msgRedelivered.add(receivedText);
+                    }
+
+                    System.out.println("consumer received message: " + receivedText + (tm.getJMSRedelivered() ? " ** Redelivered **" : ""));
+                    if (msgCount == 3) {
+                        msgRolledBack.add(receivedText);
+                        consumerSession.rollback();
+                        System.out.println("[msg: " + receivedText + "] ** rolled back **");
+                    }
+                    else {
+                        msgCommittedCount++;
+                        msgCommitted.add(receivedText);
+                        consumerSession.commit();
+                        System.out.println("[msg: " + receivedText + "] committed transaction ");
+                    }
+                    if (msgCommittedCount == NUM_MESSAGES) {
+                        latch.countDown();
+                    }
+                }
+                catch (JMSException e) {
+                    try {
+                        consumerSession.rollback();
+                        System.out.println("rolled back transaction");
+                    }
+                    catch (JMSException e1) {
+                        System.out.println(e1);
+                        e1.printStackTrace();
+                    }
+                    System.out.println(e);
+                    e.printStackTrace();
+                }
+            }
+        });
+        connection.start();
+
+        TextMessage tm = null;
+        try {
+            for (int i = 1; i <= NUM_MESSAGES; i++) {
+                tm = producerSession.createTextMessage();
+                tm.setText("Hello " + i);
+                msgSent.add(tm.getText());
+                producer.send(tm);
+                System.out.println("producer sent message: " + tm.getText());
+            }
+        }
+        catch (JMSException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Waiting for latch");
+        latch.await();
+
+        assertEquals(1, msgRolledBack.size());
+        assertEquals(1, msgRedelivered.size());
+
+        System.out.println("msg RolledBack = " + msgRolledBack.get(0));
+        System.out.println("msg Redelivered = " + msgRedelivered.get(0));
+
+        assertEquals(msgRolledBack.get(0), msgRedelivered.get(0));
+
+        assertEquals(NUM_MESSAGES, msgSent.size());
+        assertEquals(NUM_MESSAGES, msgCommitted.size());
+
+        assertEquals(msgSent, msgCommitted);
+
+    }
+
+    protected void tearDown() throws Exception {
+        if (connection != null) {
+            System.out.println("Closing the connection");
+            connection.close();
+        }
+        super.tearDown();
+    }
+}
