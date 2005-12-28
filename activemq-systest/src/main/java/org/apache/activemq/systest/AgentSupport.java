58d84dfa39249901193e189a4f5092fe5f8d434b
Hiram R. Chirino <chirino@apache.org>
2005-12-28 18:58:36 +0000

    refactoring to org.apache.activemq package names.
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/activemq/trunk@359595 13f79535-47bb-0310-9956-ffa450edef68

---
+++ activemq-systest/src/main/java/org/apache/activemq/systest/AgentSupport.java
@@ -0,0 +1,38 @@
+/**
+* <a href="http://activemq.org">ActiveMQ: The Open Source Message Fabric</a>
+*
+* Copyright 2005 (C) LogicBlaze, Inc. http://www.logicblaze.com
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+**/
+package org.apache.activemq.systest;
+
+/**
+ * A helper class for working with agents
+ *  
+ * @version $Revision: 1.1 $
+ */
+public abstract class AgentSupport implements Agent {
+        
+    public void stop() throws Exception {
+        AgentStopper stopper = new AgentStopper();
+        stop(stopper);
+        stopper.throwFirstException();
+    }
+
+    /**
+     * Provides a way for derived classes to stop resources cleanly, handling exceptions
+     */
+    public abstract void stop(AgentStopper stopper);
+}
---
+++ activemq-systest/src/main/java/org/apache/activemq/systest/impl/DiscoveryBrokerAgentImpl.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ * Copyright 2005 LogicBlaze, Inc. http://www.logicblaze.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ * 
+ **/
+package org.apache.activemq.systest.impl;
+
+import org.apache.activemq.broker.BrokerService;
+import org.apache.activemq.network.NetworkConnector;
+import org.apache.activemq.systest.BrokerAgent;
+
+/**
+ * A broker using discovery
+ * 
+ * @version $Revision: 1.1 $
+ */
+public class DiscoveryBrokerAgentImpl extends BrokerAgentImpl {
+
+    public DiscoveryBrokerAgentImpl() throws Exception {
+    }
+
+    public void connectTo(BrokerAgent remoteBroker) throws Exception {
+        NetworkConnector connector = getBroker().addNetworkConnector("rendezvous");
+        if (isStarted()) {
+            connector.start();
+        }
+    }
+
+    protected BrokerService createBroker() throws Exception {
+        BrokerService answer = new BrokerService();
+        answer.setBrokerName(getBrokerName());
+        answer.setPersistent(isPersistent());
+        answer.addConnector("discovery:" + getConnectionURI());
+        return answer;
+    }
+
+}
