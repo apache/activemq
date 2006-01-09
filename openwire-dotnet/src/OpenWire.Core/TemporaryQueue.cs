54a26dfe5f6d7af5dd973d36e92d959742ce71b0
James Strachan <jstrachan@apache.org>
2006-01-09 15:02:52 +0000

    latest generated openwire.net code which now compiles OK!
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/activemq/trunk@367314 13f79535-47bb-0310-9956-ffa450edef68

---
+++ openwire-dotnet/src/OpenWire.Core/TemporaryQueue.cs
@@ -0,0 +1,12 @@
+using System;
+using OpenWire.Core.Commands;
+
+namespace OpenWire.Core
+{
+	/// <summary>
+	/// Summary description for TemporaryQueue.
+	/// </summary>
+	public interface TemporaryQueue : Destination 
+	{
+	}
+}
