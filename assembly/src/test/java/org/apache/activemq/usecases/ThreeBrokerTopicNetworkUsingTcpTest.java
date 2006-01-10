5eff976d28e008a6c20620ad57bdd96333f71043
Adrian T. Co <aco@apache.org>
2006-01-10 06:14:27 +0000

    - Added test case to check if any message dispatch are sent over the bridge even when there is no consumer on the other end - Added better setup for some to the test case
    git-svn-id: https://svn.apache.org/repos/asf/incubator/activemq/trunk@367501 13f79535-47bb-0310-9956-ffa450edef68

--- assembly/src/test/java/org/apache/activemq/usecases/ThreeBrokerTopicNetworkUsingTcpTest.java
+++ assembly/src/test/java/org/apache/activemq/usecases/ThreeBrokerTopicNetworkUsingTcpTest.java
@@ -29,7 +29,7 @@ import java.net.URI;
  * @version $Revision: 1.1.1.1 $
  */
 public class ThreeBrokerTopicNetworkUsingTcpTest extends ThreeBrokerTopicNetworkTest {
-    protected List bridges = new ArrayList();
+    protected List bridges;
 
     protected void bridgeBrokers(BrokerService localBroker, BrokerService remoteBroker) throws Exception {
         List remoteTransports = remoteBroker.getTransportConnectors();
@@ -56,5 +56,11 @@ public class ThreeBrokerTopicNetworkUsingTcpTest extends ThreeBrokerTopicNetwork
         }
 
         MAX_SETUP_TIME = 2000;
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+        bridges = new ArrayList();
     }
 }
