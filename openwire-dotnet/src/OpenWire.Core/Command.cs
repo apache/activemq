6dacb8eced531c5e3e6ef5ef29ec7065efaed443
James Strachan <jstrachan@apache.org>
2006-01-10 12:24:16 +0000

    Removed old cruft
    git-svn-id: https://svn.apache.org/repos/asf/incubator/activemq/trunk@367582 13f79535-47bb-0310-9956-ffa450edef68

--- openwire-dotnet/src/OpenWire.Core/Command.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace OpenWire.Core {
-        /// <summary>
-        /// An OpenWire command
-        /// </summary>
-        public interface Command : DataStructure {
-        } 
-}
