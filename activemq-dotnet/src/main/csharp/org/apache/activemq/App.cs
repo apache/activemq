ef5444b6cff792845255515435ee1b7f6e84241e
Hiram R. Chirino <chirino@apache.org>
2006-03-06 15:49:26 +0000

    Adding initial maven layout for activemq-dotnet project
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/activemq/trunk@383568 13f79535-47bb-0310-9956-ffa450edef68

---
+++ activemq-dotnet/src/main/csharp/org/apache/activemq/App.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace org.apache.activemq
+{
+	/// <summary>
+	/// Sample Maven Dotnet App
+	/// </summary>
+	public class App
+	{
+		/// <summary>
+		/// A public call on the library...
+		/// </summary>
+		public void doSomething()
+		{
+			Console.WriteLine("Hello World");
+		}
+	}
+}
