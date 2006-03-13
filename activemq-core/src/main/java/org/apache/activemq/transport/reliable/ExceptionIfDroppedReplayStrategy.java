bf1d11e5cd857b815db2d23d2e0f2c3891f7c672
James Strachan <jstrachan@apache.org>
2006-03-13 09:17:43 +0000

    refactored the reliable transport into the reliable package and added a test case demonstrating the transport handling duplicate packets and dealing with reordering OK
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/activemq/trunk@385478 13f79535-47bb-0310-9956-ffa450edef68

---
+++ activemq-core/src/main/java/org/apache/activemq/transport/reliable/ExceptionIfDroppedReplayStrategy.java
@@ -0,0 +1,53 @@
+/**
+ *
+ * Copyright 2005-2006 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.activemq.transport.reliable;
+
+import java.io.IOException;
+
+/**
+ * Throws an exception if packets are dropped causing the transport to be
+ * closed.
+ * 
+ * @version $Revision$
+ */
+public class ExceptionIfDroppedReplayStrategy implements ReplayStrategy {
+
+    private int maximumDifference = 5;
+
+    public void onDroppedPackets(ReliableTransport transport, int expectedCounter, int actualCounter) throws IOException {
+        long count = Math.abs(actualCounter - expectedCounter);
+        if (count > maximumDifference) {
+            throw new IOException("Packets dropped on: " + transport + " count: " + count + " expected: " + expectedCounter + " but was: " + actualCounter);
+        }
+    }
+
+    public void onReceivedPacket(ReliableTransport transport, long expectedCounter) {
+    }
+
+    public int getMaximumDifference() {
+        return maximumDifference;
+    }
+
+    /**
+     * Sets the maximum allowed difference between an expected packet and an
+     * actual packet before an error occurs
+     */
+    public void setMaximumDifference(int maximumDifference) {
+        this.maximumDifference = maximumDifference;
+    }
+
+}
