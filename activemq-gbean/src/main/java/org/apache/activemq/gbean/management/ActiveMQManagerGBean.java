3d43a18e2add8ad1378495f74cfb9c410ca3659e
Hiram R. Chirino <chirino@apache.org>
2006-03-27 16:02:48 +0000

    Moved to the org.apache.activemq package and updated copyright headers.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/activemq/trunk@389180 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-gbean/src/main/java/org/apache/activemq/gbean/management/ActiveMQManagerGBean.java
+++ activemq-gbean/src/main/java/org/apache/activemq/gbean/management/ActiveMQManagerGBean.java
@@ -1,21 +1,20 @@
 /**
  *
- * Copyright 2004 Protique Ltd
+ * Copyright 2005-2006 The Apache Software Foundation
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- **/
-package org.activemq.gbean.management;
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.activemq.gbean.management;
 
 import java.util.Set;
 import java.util.Iterator;
@@ -24,10 +23,6 @@ import java.util.List;
 import java.util.Hashtable;
 import javax.management.ObjectName;
 import javax.management.MalformedObjectNameException;
-import org.activemq.gbean.ActiveMQManager;
-import org.activemq.gbean.ActiveMQBroker;
-import org.activemq.gbean.ActiveMQConnector;
-import org.activemq.gbean.TransportConnectorGBeanImpl;
 import org.apache.geronimo.gbean.GBeanInfo;
 import org.apache.geronimo.gbean.GBeanInfoBuilder;
 import org.apache.geronimo.gbean.GBeanQuery;
@@ -36,6 +31,10 @@ import org.apache.geronimo.kernel.Kernel;
 import org.apache.geronimo.kernel.GBeanNotFoundException;
 import org.apache.geronimo.j2ee.management.impl.Util;
 import org.apache.geronimo.j2ee.j2eeobjectnames.NameFactory;
+import org.apache.activemq.gbean.ActiveMQBroker;
+import org.apache.activemq.gbean.ActiveMQConnector;
+import org.apache.activemq.gbean.ActiveMQManager;
+import org.apache.activemq.gbean.TransportConnectorGBeanImpl;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
