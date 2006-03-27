ccfe9372f2a3ee65fd0ebad8db0c08a22b559816
James Strachan <jstrachan@apache.org>
2006-03-27 04:33:08 +0000

    added a visualisation plugin to render the current queue and topic hierarchies graphically
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/activemq/trunk@389018 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-core/src/test/java/org/apache/activemq/test/Main.java
+++ activemq-core/src/test/java/org/apache/activemq/test/Main.java
@@ -17,12 +17,17 @@
 package org.apache.activemq.test;
 
 import org.apache.activemq.broker.BrokerFactory;
+import org.apache.activemq.broker.BrokerPlugin;
 import org.apache.activemq.broker.BrokerService;
+import org.apache.activemq.broker.view.DestinationDotFileInterceptor;
+import org.apache.activemq.broker.view.DestinationDotFilePlugin;
+import org.apache.activemq.demo.DefaultQueueSender;
 
 import java.net.URI;
 
 /**
- * A helper class which can be handy for running a broker in your IDE from the activemq-core module.
+ * A helper class which can be handy for running a broker in your IDE from the
+ * activemq-core module.
  * 
  * @version $Revision$
  */
@@ -37,8 +42,18 @@ public class Main {
             brokerURI = args[0];
         }
         try {
-            BrokerService broker = BrokerFactory.createBroker(new URI(brokerURI));
+            //BrokerService broker = BrokerFactory.createBroker(new URI(brokerURI));
+            BrokerService broker = new BrokerService();
+            broker.setPersistent(false);
+            broker.setUseJmx(true);
+            broker.setPlugins(new BrokerPlugin[] { new DestinationDotFilePlugin() });
+            broker.addConnector("tcp://localhost:61616");
+            broker.addConnector("stomp://localhost:61613");
             broker.start();
+            
+            // lets publish some messages so that there is some stuff to browse
+            DefaultQueueSender.main(new String[] {"Prices.Equity.IBM"});
+            DefaultQueueSender.main(new String[] {"Prices.Equity.MSFT"});
         }
         catch (Exception e) {
             System.out.println("Failed: " + e);
