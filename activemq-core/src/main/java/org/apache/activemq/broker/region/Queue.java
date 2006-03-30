217e0c8527d636e1b92160848d8085aca0998c3f
James Strachan <jstrachan@apache.org>
2006-03-30 14:37:24 +0000

    fixed typeo and made synchronized collection private
    git-svn-id: https://svn.apache.org/repos/asf/incubator/activemq/trunk@390149 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-core/src/main/java/org/apache/activemq/broker/region/Queue.java
+++ activemq-core/src/main/java/org/apache/activemq/broker/region/Queue.java
@@ -60,7 +60,7 @@ public class Queue implements Destination {
 
     protected final ActiveMQDestination destination;
     protected final List consumers = new CopyOnWriteArrayList();
-    protected final LinkedList messages = new LinkedList();
+    private final LinkedList messages = new LinkedList();
     protected final Valve dispatchValve = new Valve(true);
     protected final UsageManager usageManager;
     protected final DestinationStatistics destinationStatistics = new DestinationStatistics();
@@ -330,7 +330,7 @@ public class Queue implements Destination {
     public String toString() {
         int size = 0;
         synchronized (messages) {
-            size = message.size();
+            size = messages.size();
         }
         return "Queue: destination=" + destination.getPhysicalName() + ", subscriptions=" + consumers.size()
                 + ", memory=" + usageManager.getPercentUsage() + "%, size=" + size + ", in flight groups="
