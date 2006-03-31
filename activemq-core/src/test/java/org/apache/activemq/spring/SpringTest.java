d5c86ac543f07840d44b3aab7858c5cbae60a9f8
Hiram R. Chirino <chirino@apache.org>
2006-03-31 18:12:24 +0000

    Implemented: http://issues.apache.org/activemq/browse/AMQ-673
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/activemq/trunk@390470 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-core/src/test/java/org/apache/activemq/spring/SpringTest.java
+++ activemq-core/src/test/java/org/apache/activemq/spring/SpringTest.java
@@ -22,6 +22,7 @@ import java.util.List;
 
 import junit.framework.TestCase;
 
+import org.apache.activemq.broker.BrokerService;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -33,6 +34,50 @@ public class SpringTest extends TestCase {
     protected AbstractApplicationContext context;
     protected SpringConsumer consumer;
     protected SpringProducer producer;
+
+    /**
+     * Make sure that brokers are being pooled properly.
+     *
+     * @throws Exception
+     */
+    public void testSenderWithSpringXmlEmbeddedPooledBrokerConfiguredViaXml() throws Exception {
+        String config = "spring-embedded-pooled.xml";
+        
+        Thread.currentThread().setContextClassLoader(SpringTest.class.getClassLoader());
+        ClassPathXmlApplicationContext context1 = new ClassPathXmlApplicationContext(config);
+
+        BrokerService bs1 = (BrokerService) context1.getBean("broker1");
+        assertNotNull(bs1);
+        BrokerService bs2 = (BrokerService) context1.getBean("broker2");
+        assertNotNull(bs1);
+        
+        // It should be the same broker;
+        assertEquals(bs1, bs2);
+
+        // Even if we load up another context, it should still be the same broker.
+        ClassPathXmlApplicationContext context2 = new ClassPathXmlApplicationContext(config);
+
+        BrokerService bs3 = (BrokerService) context2.getBean("broker1");
+        assertNotNull(bs3);
+        BrokerService bs4 = (BrokerService) context2.getBean("broker2");
+        assertNotNull(bs4);
+
+        // It should be the same broker;
+        assertEquals(bs1, bs3);
+        assertEquals(bs1, bs4);
+        
+        // And it should be started.
+        assertTrue(bs1.isStarted());
+        
+        // should still be started asfter the 2nd context closes.
+        context2.close();
+        assertTrue(bs1.isStarted());
+        
+        // Should stop once all contexts close.
+        context1.close();
+        assertFalse(bs1.isStarted());
+
+    }
 
     /**
      * Uses ActiveMQConnectionFactory to create the connection context.
@@ -79,7 +124,7 @@ public class SpringTest extends TestCase {
         String config = "spring-embedded.xml";
         assertSenderConfig(config);
     }
-
+    
     /**
      * assert method that is used by all the test method to send and receive messages
      * based on each spring configuration.
