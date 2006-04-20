c10129351d0ee99386a94dbe404b271e1ffc5a48
Hiram R. Chirino <chirino@apache.org>
2006-04-20 16:58:58 +0000

    If we get an async exception, report it to all blocked sync requests. https://issues.apache.org/activemq/browse/AMQ-691
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/activemq/trunk@395641 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-core/src/main/java/org/apache/activemq/transport/ResponseCorrelator.java
+++ activemq-core/src/main/java/org/apache/activemq/transport/ResponseCorrelator.java
@@ -18,8 +18,12 @@ package org.apache.activemq.transport;
 
 import java.io.IOException;
 import java.io.InterruptedIOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 
 import org.apache.activemq.command.Command;
+import org.apache.activemq.command.ExceptionResponse;
 import org.apache.activemq.command.Response;
 import org.apache.activemq.util.IntSequenceGenerator;
 import org.apache.commons.logging.Log;
@@ -92,6 +96,28 @@ public class ResponseCorrelator extends TransportFilter {
         } else {
             getTransportListener().onCommand(command);
         }
+    }
+    
+    /**
+     * If an async exception occurs, then assume no responses will arrive for any of
+     * current requests.  Lets let them know of the problem.
+     */
+    public void onException(IOException error) {
+        
+        // Copy and Clear the request Map
+        ArrayList requests = new ArrayList(requestMap.values());
+        requestMap.clear();
+        
+        for (Iterator iter = requests.iterator(); iter.hasNext();) {
+            try {
+                FutureResponse fr = (FutureResponse) iter.next();
+                fr.set(new ExceptionResponse(error));
+            } catch (InterruptedIOException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        
+        super.onException(error);
     }
     
     public IntSequenceGenerator getSequenceGenerator() {
