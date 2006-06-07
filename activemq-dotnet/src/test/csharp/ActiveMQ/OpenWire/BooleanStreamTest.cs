fbdc95e6a3504a33fa8ab8b87a30b19a38b5afd5
James Strachan <jstrachan@apache.org>
2006-06-07 16:50:56 +0000

    added back csharp m2 repo
    git-svn-id: https://svn.apache.org/repos/asf/incubator/activemq/trunk@412454 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-dotnet/src/test/csharp/ActiveMQ/OpenWire/BooleanStreamTest.cs
+++ activemq-dotnet/src/test/csharp/ActiveMQ/OpenWire/BooleanStreamTest.cs
@@ -18,111 +18,112 @@ using NUnit.Framework;
 using System;
 using System.IO;
 
-namespace ActiveMQ.OpenWire
-{
-    [TestFixture]
-    public class BooleanStreamTest
-    {
-		protected int endOfStreamMarker = 0x12345678;
-		int numberOfBytes = 8 * 200;
-		
-		public delegate bool GetBooleanValueDelegate(int index, int count);
-		
-        [Test]
-		public void TestBooleanMarshallingUsingAllTrue()
-		{
-			DoTestBooleanStream(numberOfBytes, new GetBooleanValueDelegate(GetBooleanValueAllTrue));
-		}
-		public bool GetBooleanValueAllTrue(int index, int count)
-		{
-			return true;
-		}
-		
-        [Test]
-		public void TestBooleanMarshallingUsingAllFalse()
-		{
-			DoTestBooleanStream(numberOfBytes, new GetBooleanValueDelegate(GetBooleanValueAllFalse));
-		}
-		public bool GetBooleanValueAllFalse(int index, int count)
-		{
-			return false;
-		}
-		
-        [Test]
-		public void TestBooleanMarshallingUsingAlternateTrueFalse()
-		{
-			DoTestBooleanStream(numberOfBytes, new GetBooleanValueDelegate(GetBooleanValueAlternateTrueFalse));
-		}
-		public bool GetBooleanValueAlternateTrueFalse(int index, int count)
-		{
-			return (index & 1) == 0;
-		}
-		
-		[Test]
-		public void TestBooleanMarshallingUsingAlternateFalseTrue()
-		{
-			DoTestBooleanStream(numberOfBytes, new GetBooleanValueDelegate(GetBooleanValueAlternateFalseTrue));
-		}
-		public bool GetBooleanValueAlternateFalseTrue(int index, int count)
-		{
-			return (index & 1) != 0;
-		}
-		
-		protected void DoTestBooleanStream(int numberOfBytes, GetBooleanValueDelegate valueDelegate)
-		{
-			for (int i = 1017; i < numberOfBytes; i++)
-			{
-				AssertMarshalBooleans(i, valueDelegate);
-			}
-		}
-		
-		protected void AssertMarshalBooleans(int count, GetBooleanValueDelegate valueDelegate)
-		{
-			BooleanStream bs = new BooleanStream();
-			for (int i = 0; i < count; i++)
-			{
-				bs.WriteBoolean(valueDelegate(i, count));
-			}
-			MemoryStream buffer = new MemoryStream();
-			BinaryWriter ds = new OpenWireBinaryWriter(buffer);
-			bs.Marshal(ds);
-			ds.Write(endOfStreamMarker);
-			
-			// now lets read from the stream
-			
-			MemoryStream ins = new MemoryStream(buffer.ToArray());
-			BinaryReader dis = new OpenWireBinaryReader(ins);
-			bs = new BooleanStream();
-			bs.Unmarshal(dis);
-			
-			for (int i = 0; i < count; i++)
-			{
-				bool expected = valueDelegate(i, count);
-				
-				try
-				{
-					bool actual = bs.ReadBoolean();
-					Assert.AreEqual(expected, actual);
-				}
-				catch (Exception e)
-				{
-					Assert.Fail("Failed to parse bool: " + i + " out of: " + count + " due to: " + e);
-				}
-			}
-			int marker = dis.ReadInt32();
-			Assert.AreEqual(endOfStreamMarker, marker, "did not match: "+endOfStreamMarker+" and "+marker);
-			
-			// lets try read and we should get an exception
-			try
-			{
-				dis.ReadByte();
-				Assert.Fail("Should have reached the end of the stream");
-			}
-			catch (IOException)
-			{
-			}
-		}
-		
-	}
-}
+namespace ActiveMQ.OpenWire {
+        [ TestFixture ]
+        public class BooleanStreamTest
+        {
+                protected int endOfStreamMarker = 0x12345678;
+                int numberOfBytes = 8 * 200;
 
+                public delegate bool GetBooleanValueDelegate(int index, int count);
+
+                [ Test ]
+                public void TestBooleanMarshallingUsingAllTrue()
+                {
+                        DoTestBooleanStream(numberOfBytes, new GetBooleanValueDelegate(GetBooleanValueAllTrue));
+                }
+                public bool GetBooleanValueAllTrue(int index, int count)
+                {
+                        return true;
+                }
+
+                [ Test ]
+                public void TestBooleanMarshallingUsingAllFalse()
+                {
+                        DoTestBooleanStream(numberOfBytes, new GetBooleanValueDelegate(GetBooleanValueAllFalse));
+                }
+                public bool GetBooleanValueAllFalse(int index, int count)
+                {
+                        return false;
+                }
+
+                [ Test ]
+                public void TestBooleanMarshallingUsingAlternateTrueFalse()
+                {
+                        DoTestBooleanStream(
+                                numberOfBytes, new GetBooleanValueDelegate(GetBooleanValueAlternateTrueFalse));
+                }
+                public bool GetBooleanValueAlternateTrueFalse(int index, int count)
+                {
+                        return (index & 1) == 0;
+                }
+
+                [ Test ]
+                public void TestBooleanMarshallingUsingAlternateFalseTrue()
+                {
+                        DoTestBooleanStream(
+                                numberOfBytes, new GetBooleanValueDelegate(GetBooleanValueAlternateFalseTrue));
+                }
+                public bool GetBooleanValueAlternateFalseTrue(int index, int count)
+                {
+                        return (index & 1) != 0;
+                }
+
+                protected void DoTestBooleanStream(int numberOfBytes, GetBooleanValueDelegate valueDelegate)
+                {
+                        for (int i = 1017; i < numberOfBytes; i++)
+                        {
+                                AssertMarshalBooleans(i, valueDelegate);
+                        }
+                }
+
+                protected void AssertMarshalBooleans(int count, GetBooleanValueDelegate valueDelegate)
+                {
+                        BooleanStream bs = new BooleanStream();
+                        for (int i = 0; i < count; i++)
+                        {
+                                bs.WriteBoolean(valueDelegate(i, count));
+                        }
+                        MemoryStream buffer = new MemoryStream();
+                        BinaryWriter ds = new OpenWireBinaryWriter(buffer);
+                        bs.Marshal(ds);
+                        ds.Write(endOfStreamMarker);
+
+                        // now lets read from the stream
+
+                        MemoryStream ins = new MemoryStream(buffer.ToArray());
+                        BinaryReader dis = new OpenWireBinaryReader(ins);
+                        bs = new BooleanStream();
+                        bs.Unmarshal(dis);
+
+                        for (int i = 0; i < count; i++)
+                        {
+                                bool expected = valueDelegate(i, count);
+
+                                try
+                                {
+                                        bool actual = bs.ReadBoolean();
+                                        Assert.AreEqual(expected, actual);
+                                }
+                                catch (Exception e)
+                                {
+                                        Assert.Fail(
+                                                "Failed to parse bool: " + i + " out of: " + count + " due to: " + e);
+                                }
+                        }
+                        int marker = dis.ReadInt32();
+                        Assert.AreEqual(
+                                endOfStreamMarker, marker, "did not match: " + endOfStreamMarker + " and " + marker);
+
+                        // lets try read and we should get an exception
+                        try
+                        {
+                                dis.ReadByte();
+                                Assert.Fail("Should have reached the end of the stream");
+                        }
+                        catch (IOException)
+                        {
+                        }
+                }
+        }
+}
