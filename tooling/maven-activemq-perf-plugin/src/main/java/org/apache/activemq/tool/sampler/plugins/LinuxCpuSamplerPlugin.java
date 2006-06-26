a6f93512b426f121f67c5758cd99a454f777fe22
Adrian T. Co <aco@apache.org>
2006-06-26 06:29:46 +0000

    - Modify delimiters for cpu sampler data
    git-svn-id: https://svn.apache.org/repos/asf/incubator/activemq/trunk@417128 13f79535-47bb-0310-9956-ffa450edef68

--- tooling/maven-activemq-perf-plugin/src/main/java/org/apache/activemq/tool/sampler/plugins/LinuxCpuSamplerPlugin.java
+++ tooling/maven-activemq-perf-plugin/src/main/java/org/apache/activemq/tool/sampler/plugins/LinuxCpuSamplerPlugin.java
@@ -7,19 +7,19 @@ import java.util.StringTokenizer;
 public class LinuxCpuSamplerPlugin implements CpuSamplerPlugin {
 
 	private String vmstat = "vmstat";
-	
+
 	public String getCpuUtilizationStats() {
 		try {
 			Process p = Runtime.getRuntime().exec(vmstat);
 			BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()), 1024);
-			
+
 			br.readLine(); // throw away the first line
-			
+
 			String header = br.readLine();
 			String data   = br.readLine();
-			
+
 			br.close();
-			
+
 			// Convert to CSV of key=value pair
 			return convertToCSV(header, data);
 		} catch (Exception e) {
@@ -35,16 +35,16 @@ public class LinuxCpuSamplerPlugin implements CpuSamplerPlugin {
 	public void setVmstat(String vmstat) {
 		this.vmstat = vmstat;
 	}
-	
+
 	protected String convertToCSV(String header, String data) {
 		StringTokenizer headerTokens = new StringTokenizer(header, " ");
 		StringTokenizer dataTokens   = new StringTokenizer(data, " ");
-		
+
 		String csv = "";
 		while (headerTokens.hasMoreTokens()) {
-			csv += (headerTokens.nextToken() + "=" + dataTokens.nextToken() + ";");
+			csv += (headerTokens.nextToken() + "=" + dataTokens.nextToken() + ",");
 		}
-		
+
 		return csv;
 	}
 }
