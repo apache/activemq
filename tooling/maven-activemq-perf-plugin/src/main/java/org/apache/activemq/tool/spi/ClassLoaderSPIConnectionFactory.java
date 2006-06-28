6086520836c0e19ec362096ec08d497daba4cf62
Adrian T. Co <aco@apache.org>
2006-06-28 03:11:11 +0000

    Added ability to load extension dirs to the spi classes.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/activemq/trunk@417653 13f79535-47bb-0310-9956-ffa450edef68

--- tooling/maven-activemq-perf-plugin/src/main/java/org/apache/activemq/tool/spi/ClassLoaderSPIConnectionFactory.java
+++ tooling/maven-activemq-perf-plugin/src/main/java/org/apache/activemq/tool/spi/ClassLoaderSPIConnectionFactory.java
@@ -16,22 +16,65 @@
  */
 package org.apache.activemq.tool.spi;
 
-import org.apache.activemq.tool.properties.ReflectionUtil;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import javax.jms.ConnectionFactory;
 import java.util.Properties;
+import java.util.StringTokenizer;
+import java.util.List;
+import java.util.ArrayList;
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
 
 public abstract class ClassLoaderSPIConnectionFactory implements SPIConnectionFactory {
-    public ConnectionFactory createConnectionFactory(Properties settings) throws Exception {
-        Class factoryClass = Class.forName(getClassName());
-        ConnectionFactory factory = (ConnectionFactory)factoryClass.newInstance();
-        configureConnectionFactory(factory, settings);
-        return factory;
+    private static final Log log = LogFactory.getLog(ClassLoaderSPIConnectionFactory.class);
+
+    public static final String KEY_EXT_DIR = "extDir";
+
+    public final ConnectionFactory createConnectionFactory(Properties settings) throws Exception {
+
+        // Load new context class loader
+        ClassLoader newClassLoader = getContextClassLoader(settings);
+        Thread.currentThread().setContextClassLoader(newClassLoader);
+
+        return instantiateConnectionFactory(newClassLoader, settings);
     }
 
-    public void configureConnectionFactory(ConnectionFactory jmsFactory, Properties settings) throws Exception {
-        ReflectionUtil.configureClass(jmsFactory, settings);
+    protected ClassLoader getContextClassLoader(Properties settings) {
+        String extDir = (String)settings.remove(KEY_EXT_DIR);
+        if (extDir != null) {
+            StringTokenizer tokens = new StringTokenizer(extDir, ";,");
+            List urls = new ArrayList();
+            while (tokens.hasMoreTokens()) {
+                String dir = tokens.nextToken();
+                try {
+                    File f = new File(dir);
+                    dir = f.getAbsolutePath();
+                    System.out.println(dir);
+                    urls.add(f.toURL());
+
+                    File[] files = f.listFiles();
+                    if( files!=null ) {
+                        for (int j = 0; j < files.length; j++) {
+                            if( files[j].getName().endsWith(".zip") || files[j].getName().endsWith(".jar") ) {
+                                dir = files[j].getAbsolutePath();
+                                urls.add(files[j].toURL());
+                            }
+                        }
+                    }
+                } catch (Exception e) {
+                    log.warn("Failed to load ext dir: " + dir + ". Reason: " + e);
+                }
+            }
+
+            URL u[] = new URL[urls.size()];
+            urls.toArray(u);
+            return new URLClassLoader(u, ClassLoaderSPIConnectionFactory.class.getClassLoader());
+        }
+        return ClassLoaderSPIConnectionFactory.class.getClassLoader();
     }
 
-    public abstract String getClassName();
+    protected abstract ConnectionFactory instantiateConnectionFactory(ClassLoader cl, Properties settings) throws Exception;
 }
