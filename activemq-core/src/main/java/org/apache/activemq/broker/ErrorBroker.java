ba9eb7505f2528fb915f3acc69ded1878ceea027
Hiram R. Chirino <chirino@apache.org>
2006-06-29 19:20:12 +0000

    http://issues.apache.org/activemq/browse/AMQ-780
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/activemq/trunk@418119 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-core/src/main/java/org/apache/activemq/broker/ErrorBroker.java
+++ activemq-core/src/main/java/org/apache/activemq/broker/ErrorBroker.java
@@ -19,6 +19,7 @@ package org.apache.activemq.broker;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
+
 import org.apache.activemq.broker.region.Destination;
 import org.apache.activemq.broker.region.Subscription;
 import org.apache.activemq.command.ActiveMQDestination;
@@ -37,7 +38,7 @@ import org.apache.activemq.command.SessionInfo;
 import org.apache.activemq.command.TransactionId;
 
 /**
- * Implementation of the broker where all it's methods throw an IllegalStateException.
+ * Implementation of the broker where all it's methods throw an BrokerStoppedException.
  * 
  * @version $Revision$
  */
@@ -61,132 +62,132 @@ public class ErrorBroker implements Broker {
     }
     
     public BrokerId getBrokerId() {
-        throw new IllegalStateException(this.message);
+        throw new BrokerStoppedException(this.message);
     }
 
     public String getBrokerName() {
-        throw new IllegalStateException(this.message);
+        throw new BrokerStoppedException(this.message);
     }
 	
     public void addConnection(ConnectionContext context, ConnectionInfo info) throws Exception {
-        throw new IllegalStateException(this.message);
+        throw new BrokerStoppedException(this.message);
     }
 
     public void removeConnection(ConnectionContext context, ConnectionInfo info, Throwable error) throws Exception {
-        throw new IllegalStateException(this.message);
+        throw new BrokerStoppedException(this.message);
     }
 
     public void addSession(ConnectionContext context, SessionInfo info) throws Exception {
-        throw new IllegalStateException(this.message);
+        throw new BrokerStoppedException(this.message);
     }
 
     public void removeSession(ConnectionContext context, SessionInfo info) throws Exception {
-        throw new IllegalStateException(this.message);
+        throw new BrokerStoppedException(this.message);
     }
 
     public void addProducer(ConnectionContext context, ProducerInfo info) throws Exception {
-        throw new IllegalStateException(this.message);
+        throw new BrokerStoppedException(this.message);
     }
 
     public void removeProducer(ConnectionContext context, ProducerInfo info) throws Exception {
-        throw new IllegalStateException(this.message);
+        throw new BrokerStoppedException(this.message);
     }
 
     public Connection[] getClients() throws Exception {
-        throw new IllegalStateException(this.message);
+        throw new BrokerStoppedException(this.message);
     }
 
     public ActiveMQDestination[] getDestinations() throws Exception {
-        throw new IllegalStateException(this.message);
+        throw new BrokerStoppedException(this.message);
     }
 
     public TransactionId[] getPreparedTransactions(ConnectionContext context) throws Exception {
-        throw new IllegalStateException(this.message);
+        throw new BrokerStoppedException(this.message);
     }
 
     public void beginTransaction(ConnectionContext context, TransactionId xid) throws Exception {
-        throw new IllegalStateException(this.message);
+        throw new BrokerStoppedException(this.message);
     }
 
     public int prepareTransaction(ConnectionContext context, TransactionId xid) throws Exception {
-        throw new IllegalStateException(this.message);
+        throw new BrokerStoppedException(this.message);
     }
 
     public void rollbackTransaction(ConnectionContext context, TransactionId xid) throws Exception {
-        throw new IllegalStateException(this.message);
+        throw new BrokerStoppedException(this.message);
     }
 
     public void commitTransaction(ConnectionContext context, TransactionId xid, boolean onePhase) throws Exception {
-        throw new IllegalStateException(this.message);
+        throw new BrokerStoppedException(this.message);
     }
 
     public void forgetTransaction(ConnectionContext context, TransactionId transactionId) throws Exception {
-        throw new IllegalStateException(this.message);
+        throw new BrokerStoppedException(this.message);
     }
 
     public Destination addDestination(ConnectionContext context, ActiveMQDestination destination) throws Exception {
-        throw new IllegalStateException(this.message);
+        throw new BrokerStoppedException(this.message);
     }
 
     public void removeDestination(ConnectionContext context, ActiveMQDestination destination, long timeout) throws Exception {
-        throw new IllegalStateException(this.message);
+        throw new BrokerStoppedException(this.message);
     }
 
     public Subscription addConsumer(ConnectionContext context, ConsumerInfo info) throws Exception {
-        throw new IllegalStateException(this.message);
+        throw new BrokerStoppedException(this.message);
     }
 
     public void removeConsumer(ConnectionContext context, ConsumerInfo info) throws Exception {
-        throw new IllegalStateException(this.message);
+        throw new BrokerStoppedException(this.message);
     }
 
     public void removeSubscription(ConnectionContext context, RemoveSubscriptionInfo info) throws Exception {
-        throw new IllegalStateException(this.message);
+        throw new BrokerStoppedException(this.message);
     }
 
     public void send(ConnectionContext context, Message message) throws Exception {
-        throw new IllegalStateException(this.message);
+        throw new BrokerStoppedException(this.message);
     }
 
     public void acknowledge(ConnectionContext context, MessageAck ack) throws Exception {
-        throw new IllegalStateException(this.message);
+        throw new BrokerStoppedException(this.message);
     }
 
     public void gc() {
-        throw new IllegalStateException(this.message);
+        throw new BrokerStoppedException(this.message);
     }
 
     public void start() throws Exception {
-        throw new IllegalStateException(this.message);
+        throw new BrokerStoppedException(this.message);
     }
 
     public void stop() throws Exception {
-        throw new IllegalStateException(this.message);
+        throw new BrokerStoppedException(this.message);
     }
 
     public void addBroker(Connection connection,BrokerInfo info){
-        throw new IllegalStateException(this.message);
+        throw new BrokerStoppedException(this.message);
         
     }
     
     public void removeBroker(Connection connection,BrokerInfo info){
-        throw new IllegalStateException(this.message);
+        throw new BrokerStoppedException(this.message);
     }
 
     public BrokerInfo[] getPeerBrokerInfos(){
-        throw new IllegalStateException(this.message);
+        throw new BrokerStoppedException(this.message);
     }
     
     public void processDispatch(MessageDispatch messageDispatch){
-        throw new IllegalStateException(this.message);
+        throw new BrokerStoppedException(this.message);
     }
     
     public void processDispatchNotification(MessageDispatchNotification messageDispatchNotification) throws Exception{
-        throw new IllegalStateException(this.message);
+        throw new BrokerStoppedException(this.message);
     }
     
     public boolean isSlaveBroker(){
-        throw new IllegalStateException(this.message);
+        throw new BrokerStoppedException(this.message);
     }
     
     public boolean isStopped(){
@@ -194,21 +195,21 @@ public class ErrorBroker implements Broker {
     }
     
     public Set getDurableDestinations(){
-        throw new IllegalStateException(this.message);
+        throw new BrokerStoppedException(this.message);
     }
 
     public void addDestinationInfo(ConnectionContext context,DestinationInfo info) throws Exception{
-        throw new IllegalStateException(this.message);
+        throw new BrokerStoppedException(this.message);
         
     }
 
     public void removeDestinationInfo(ConnectionContext context,DestinationInfo info) throws Exception{
-        throw new IllegalStateException(this.message);
+        throw new BrokerStoppedException(this.message);
         
     }
 
     public boolean isFaultTolerantConfiguration(){
-        throw new IllegalStateException(this.message);
+        throw new BrokerStoppedException(this.message);
     }
     
    
