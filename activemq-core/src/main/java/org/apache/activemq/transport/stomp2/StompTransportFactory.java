dc98d967b59397aa1e977e4006c78fda4297a367
Hiram R. Chirino <chirino@apache.org>
2006-07-02 04:18:44 +0000

    Added a new/highly refactored version of the STOMP protocol implementation.
    The biggest difference between this and previous implementation is that conversion between the STOMP protocol and 
    the ActiveMQ protocol happens at a Transport Filter layer instead of doing it all at the WireFormat layer.
    
    I think this has resulted in cleaner code since there's a better seperating between marshalling/unmarshalling of 
    the STOMP protocol and converting the stomp protocol to the activemq protocol.
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/activemq/trunk@418550 13f79535-47bb-0310-9956-ffa450edef68

---
+++ activemq-core/src/main/java/org/apache/activemq/transport/stomp2/StompTransportFactory.java
@@ -0,0 +1,40 @@
+/**
+ *
+ * Copyright 2005-2006 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.activemq.transport.stomp2;
+
+import java.util.Map;
+
+import org.apache.activeio.command.WireFormat;
+import org.apache.activemq.transport.Transport;
+import org.apache.activemq.transport.tcp.TcpTransportFactory;
+
+/**
+ * A <a href="http://stomp.codehaus.org/">STOMP</a> transport factory
+ * 
+ * @version $Revision: 1.1.1.1 $
+ */
+public class StompTransportFactory extends TcpTransportFactory {
+
+    protected String getDefaultWireFormatType() {
+        return "stomp";
+    }
+    
+    public Transport compositeConfigure(Transport transport, WireFormat format, Map options) {
+    	transport = new StompTransportFilter(transport);
+    	return super.compositeConfigure(transport, format, options);
+    }
+}
