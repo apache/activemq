873a8f1e215d4b514c058d89dcb4266ab4efa8ce
James Strachan <jstrachan@apache.org>
2006-07-07 08:57:09 +0000

    moved the tests across from the assembly module that can be run inside the activemq-core module
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/activemq/trunk@419843 13f79535-47bb-0310-9956-ffa450edef68

---
+++ activemq-core/src/test/java/org/apache/activemq/usecases/CompositeConsumeTest.java
@@ -0,0 +1,70 @@
+/**
+ *
+ * Copyright 2005-2006 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.activemq.usecases;
+
+import org.apache.activemq.command.ActiveMQTopic;
+import org.apache.activemq.test.JmsTopicSendReceiveWithTwoConnectionsTest;
+
+import javax.jms.Destination;
+import javax.jms.Message;
+
+/**
+ * @version $Revision: 1.1.1.1 $
+ */
+public class CompositeConsumeTest extends JmsTopicSendReceiveWithTwoConnectionsTest {
+
+    public void testSendReceive() throws Exception {
+        messages.clear();
+
+        Destination[] destinations = getDestinations();
+        int destIdx = 0;
+
+        for (int i = 0; i < data.length; i++) {
+            Message message = session.createTextMessage(data[i]);
+
+            if (verbose) {
+                log.info("About to send a message: " + message + " with text: " + data[i]);
+            }
+
+            producer.send(destinations[destIdx], message);
+
+            if (++destIdx >= destinations.length) {
+                destIdx = 0;
+            }
+        }
+
+        assertMessagesAreReceived();
+    }
+
+    /**
+     * Returns the subscription subject
+     */
+    protected String getSubject() {
+        return getPrefix() + "FOO.BAR," + getPrefix() + "FOO.X.Y," + getPrefix() + "BAR.>";
+    }
+
+    /**
+     * Returns the destinations on which we publish
+     */
+    protected Destination[] getDestinations() {
+        return new Destination[]{new ActiveMQTopic(getPrefix() + "FOO.BAR"), new ActiveMQTopic(getPrefix() + "BAR.WHATNOT.XYZ"), new ActiveMQTopic(getPrefix() + "FOO.X.Y")};
+    }
+
+    protected String getPrefix() {
+        return super.getSubject() + ".";
+    }
+}
---
+++ activemq-core/src/test/java/org/apache/activemq/usecases/ThreeBrokerQueueNetworkTest.java
@@ -0,0 +1,196 @@
+/**
+ *
+ * Copyright 2005-2006 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.activemq.usecases;
+
+import org.apache.activemq.JmsMultipleBrokersTestSupport;
+import org.apache.activemq.util.MessageIdList;
+
+import javax.jms.Destination;
+import javax.jms.MessageConsumer;
+import java.net.URI;
+
+/**
+ * @version $Revision: 1.1.1.1 $
+ */
+public class ThreeBrokerQueueNetworkTest extends JmsMultipleBrokersTestSupport {
+    protected static final int MESSAGE_COUNT = 100;
+
+    /**
+     * BrokerA -> BrokerB -> BrokerC
+     */
+    public void test_AB_BC_BrokerNetwork() throws Exception {
+        // Setup broker networks
+        bridgeBrokers("BrokerA", "BrokerB");
+        bridgeBrokers("BrokerB", "BrokerC");
+
+        startAllBrokers();
+
+        // Setup destination
+        Destination dest = createDestination("TEST.FOO", false);
+
+        // Setup consumers
+        MessageConsumer clientC = createConsumer("BrokerC", dest);
+
+        // Send messages
+        sendMessages("BrokerA", dest, MESSAGE_COUNT);
+
+        // Let's try to wait for any messages. Should be none.
+        Thread.sleep(1000);
+
+        // Get message count
+        MessageIdList msgsC = getConsumerMessages("BrokerC", clientC);
+        assertEquals(0, msgsC.getMessageCount());
+    }
+
+    /**
+     * BrokerA <- BrokerB -> BrokerC
+     */
+    public void test_BA_BC_BrokerNetwork() throws Exception {
+        // Setup broker networks
+        bridgeBrokers("BrokerB", "BrokerA");
+        bridgeBrokers("BrokerB", "BrokerC");
+
+        startAllBrokers();
+
+        // Setup destination
+        Destination dest = createDestination("TEST.FOO", false);
+
+        // Setup consumers
+        MessageConsumer clientA = createConsumer("BrokerA", dest);
+        MessageConsumer clientC = createConsumer("BrokerC", dest);
+        Thread.sleep(2000); //et subscriptions get propagated
+        // Send messages
+        sendMessages("BrokerB", dest, MESSAGE_COUNT);
+
+        // Let's try to wait for any messages.
+        Thread.sleep(1000);
+
+        // Get message count
+        MessageIdList msgsA = getConsumerMessages("BrokerA", clientA);
+        MessageIdList msgsC = getConsumerMessages("BrokerC", clientC);
+
+        // Total received should be 100
+        assertEquals(MESSAGE_COUNT, msgsA.getMessageCount() + msgsC.getMessageCount());
+    }
+
+    /**
+     * BrokerA -> BrokerB <- BrokerC
+     */
+    public void test_AB_CB_BrokerNetwork() throws Exception {
+        // Setup broker networks
+        bridgeBrokers("BrokerA", "BrokerB");
+        bridgeBrokers("BrokerC", "BrokerB");
+
+        startAllBrokers();
+
+        // Setup destination
+        Destination dest = createDestination("TEST.FOO", false);
+
+        // Setup consumers
+        MessageConsumer clientB = createConsumer("BrokerB", dest);
+
+        // Send messages
+        sendMessages("BrokerA", dest, MESSAGE_COUNT);
+        sendMessages("BrokerC", dest, MESSAGE_COUNT);
+
+        // Get message count
+        MessageIdList msgsB = getConsumerMessages("BrokerB", clientB);
+
+        msgsB.waitForMessagesToArrive(MESSAGE_COUNT * 2);
+
+        assertEquals(MESSAGE_COUNT * 2, msgsB.getMessageCount());
+    }
+
+    /**
+     * BrokerA <-> BrokerB <-> BrokerC
+     */
+    public void testAllConnectedBrokerNetwork() throws Exception {
+        // Setup broker networks
+        bridgeBrokers("BrokerA", "BrokerB");
+        bridgeBrokers("BrokerB", "BrokerA");
+        bridgeBrokers("BrokerB", "BrokerC");
+        bridgeBrokers("BrokerC", "BrokerB");
+        bridgeBrokers("BrokerA", "BrokerC");
+        bridgeBrokers("BrokerC", "BrokerA");
+
+        startAllBrokers();
+
+        // Setup destination
+        Destination dest = createDestination("TEST.FOO", false);
+
+        // Setup consumers
+        MessageConsumer clientA = createConsumer("BrokerA", dest);
+        MessageConsumer clientB = createConsumer("BrokerB", dest);
+        MessageConsumer clientC = createConsumer("BrokerC", dest);
+
+        // Send messages
+        sendMessages("BrokerA", dest, MESSAGE_COUNT);
+        sendMessages("BrokerB", dest, MESSAGE_COUNT);
+        sendMessages("BrokerC", dest, MESSAGE_COUNT);
+
+        // Let's try to wait for any messages.
+        Thread.sleep(1000);
+
+        // Get message count
+        MessageIdList msgsA = getConsumerMessages("BrokerA", clientA);
+        MessageIdList msgsB = getConsumerMessages("BrokerB", clientB);
+        MessageIdList msgsC = getConsumerMessages("BrokerC", clientC);
+
+        assertEquals(MESSAGE_COUNT * 3, msgsA.getMessageCount() + msgsB.getMessageCount() + msgsC.getMessageCount());
+    }
+
+    /**
+     * BrokerA <-> BrokerB <-> BrokerC
+     */
+    public void testAllConnectedUsingMulticast() throws Exception {
+        // Setup broker networks
+        bridgeAllBrokers();
+
+        startAllBrokers();
+
+        // Setup destination
+        Destination dest = createDestination("TEST.FOO", false);
+
+        // Setup consumers
+        MessageConsumer clientA = createConsumer("BrokerA", dest);
+        MessageConsumer clientB = createConsumer("BrokerB", dest);
+        MessageConsumer clientC = createConsumer("BrokerC", dest);
+
+        // Send messages
+        sendMessages("BrokerA", dest, MESSAGE_COUNT);
+        sendMessages("BrokerB", dest, MESSAGE_COUNT);
+        sendMessages("BrokerC", dest, MESSAGE_COUNT);
+
+        // Let's try to wait for any messages.
+        Thread.sleep(1000);
+
+        // Get message count
+        MessageIdList msgsA = getConsumerMessages("BrokerA", clientA);
+        MessageIdList msgsB = getConsumerMessages("BrokerB", clientB);
+        MessageIdList msgsC = getConsumerMessages("BrokerC", clientC);
+
+        assertEquals(MESSAGE_COUNT * 3, msgsA.getMessageCount() + msgsB.getMessageCount() + msgsC.getMessageCount());
+    }
+
+    public void setUp() throws Exception {
+        super.setAutoFail(true);
+        super.setUp();
+        createBroker(new URI("broker:(tcp://localhost:61616)/BrokerA?persistent=false&useJmx=false"));
+        createBroker(new URI("broker:(tcp://localhost:61617)/BrokerB?persistent=false&useJmx=false"));
+        createBroker(new URI("broker:(tcp://localhost:61618)/BrokerC?persistent=false&useJmx=false"));
+    }
+}
