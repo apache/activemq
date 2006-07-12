44fb4933ccceeeeff20b6b37215fbd63c79ccb4f
Adrian T. Co <aco@apache.org>
2006-07-12 02:53:53 +0000

    Added test scripts
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/activemq/trunk@421092 13f79535-47bb-0310-9956-ffa450edef68

---
+++ activemq-perftest/src/main/resources/test-scripts/amq-prod.bat
@@ -0,0 +1,131 @@
+echo off
+
+set SPI_CLASS=org.apache.activemq.tool.spi.ActiveMQReflectionSPI
+set EXT_DIR=""
+set PROVIDER=AMQ
+
+REM Config for 1-1-1-Queue-NonPersistent
+echo Will run producer 1-1-1-Queue-NonPersistent...
+pause
+set CONFIG_FILE=./src/main/resources/producer-conf/AMQ-Prod-1-1-queue-nonpersistent
+set REPORT_NAME=%PROVIDER%_Prod_Queue_NonPersistent_1_1_1.xml
+mvn activemq-perf:producer -DsysTest.propsConfigFile=%CONFIG_FILE% -DsysTest.spiClass=%SPI_CLASS% -DsysTest.extDir=%EXT_DIR% -DsysTest.reportName=%REPORT_NAME%
+
+REM Config for 10-10-1-Queue-NonPersistent
+echo Will run producer 10-10-1-Queue-NonPersistent...
+pause
+set CONFIG_FILE=./src/main/resources/producer-conf/AMQ-Prod-10-1-queue-nonpersistent
+set REPORT_NAME=%PROVIDER%_Prod_Queue_NonPersistent_10_10_1.xml
+mvn activemq-perf:producer -DsysTest.propsConfigFile=%CONFIG_FILE% -DsysTest.spiClass=%SPI_CLASS% -DsysTest.extDir=%EXT_DIR% -DsysTest.reportName=%REPORT_NAME%
+
+REM Config for 10-10-10-Queue-NonPersistent
+echo Will run producer 10-10-10-Queue-NonPersistent...
+pause
+set CONFIG_FILE=./src/main/resources/producer-conf/AMQ-Prod-10-10-queue-nonpersistent
+set REPORT_NAME=%PROVIDER%_Prod_Queue_NonPersistent_10_10_10.xml
+mvn activemq-perf:producer -DsysTest.propsConfigFile=%CONFIG_FILE% -DsysTest.spiClass=%SPI_CLASS% -DsysTest.extDir=%EXT_DIR% -DsysTest.reportName=%REPORT_NAME%
+
+REM Config for 1-1-1-Queue-Persistent
+echo Will run producer 1-1-1-Queue-Persistent...
+pause
+set CONFIG_FILE=./src/main/resources/producer-conf/AMQ-Prod-1-1-queue-persistent
+set REPORT_NAME=%PROVIDER%_Prod_Queue_Persistent_1_1_1.xml
+mvn activemq-perf:producer -DsysTest.propsConfigFile=%CONFIG_FILE% -DsysTest.spiClass=%SPI_CLASS% -DsysTest.extDir=%EXT_DIR% -DsysTest.reportName=%REPORT_NAME%
+
+REM Config for 10-10-1-Queue-Persistent
+echo Will run producer 10-10-1-Queue-Persistent...
+pause
+set CONFIG_FILE=./src/main/resources/producer-conf/AMQ-Prod-10-1-queue-persistent
+set REPORT_NAME=%PROVIDER%_Prod_Queue_Persistent_10_10_1.xml
+mvn activemq-perf:producer -DsysTest.propsConfigFile=%CONFIG_FILE% -DsysTest.spiClass=%SPI_CLASS% -DsysTest.extDir=%EXT_DIR% -DsysTest.reportName=%REPORT_NAME%
+
+REM Config for 10-10-10-Queue-Persistent
+echo Will run producer 10-10-10-Queue-Persistent...
+pause
+set CONFIG_FILE=./src/main/resources/producer-conf/AMQ-Prod-10-10-queue-persistent
+set REPORT_NAME=%PROVIDER%_Prod_Queue_Persistent_10_10_10.xml
+mvn activemq-perf:producer -DsysTest.propsConfigFile=%CONFIG_FILE% -DsysTest.spiClass=%SPI_CLASS% -DsysTest.extDir=%EXT_DIR% -DsysTest.reportName=%REPORT_NAME%
+
+REM Config for 1-1-1-Topic-NonDurable-NonPersistent
+echo Will run producer 1-1-1-Topic-NonDurable-NonPersistent...
+pause
+set CONFIG_FILE=./src/main/resources/producer-conf/AMQ-Prod-1-1-topic-nonpersistent
+set REPORT_NAME=%PROVIDER%_Prod_Topic_NonDurable_NonPersistent_1_1_1.xml
+mvn activemq-perf:producer -DsysTest.propsConfigFile=%CONFIG_FILE% -DsysTest.spiClass=%SPI_CLASS% -DsysTest.extDir=%EXT_DIR% -DsysTest.reportName=%REPORT_NAME%
+
+REM Config for 10-10-1-Topic-NonDurable-NonPersistent
+echo Will run producer 10-10-1-Topic-NonDurable-NonPersistent...
+pause
+set CONFIG_FILE=./src/main/resources/producer-conf/AMQ-Prod-10-1-topic-nonpersistent
+set REPORT_NAME=%PROVIDER%_Prod_Topic_NonDurable_NonPersistent_10_10_1.xml
+mvn activemq-perf:producer -DsysTest.propsConfigFile=%CONFIG_FILE% -DsysTest.spiClass=%SPI_CLASS% -DsysTest.extDir=%EXT_DIR% -DsysTest.reportName=%REPORT_NAME%
+
+REM Config for 10-10-10-Topic-NonDurable-NonPersistent
+echo Will run producer 10-10-10-Topic-NonDurable-NonPersistent...
+pause
+set CONFIG_FILE=./src/main/resources/producer-conf/AMQ-Prod-10-10-topic-nonpersistent
+set REPORT_NAME=%PROVIDER%_Prod_Topic_NonDurable_NonPersistent_10_10_10.xml
+mvn activemq-perf:producer -DsysTest.propsConfigFile=%CONFIG_FILE% -DsysTest.spiClass=%SPI_CLASS% -DsysTest.extDir=%EXT_DIR% -DsysTest.reportName=%REPORT_NAME%
+
+REM Config for 1-1-1-Topic-NonDurable-Persistent
+echo Will run producer 1-1-1-Topic-NonDurable-Persistent...
+pause
+set CONFIG_FILE=./src/main/resources/producer-conf/AMQ-Prod-1-1-topic-persistent
+set REPORT_NAME=%PROVIDER%_Prod_Topic_NonDurable_Persistent_1_1_1.xml
+mvn activemq-perf:producer -DsysTest.propsConfigFile=%CONFIG_FILE% -DsysTest.spiClass=%SPI_CLASS% -DsysTest.extDir=%EXT_DIR% -DsysTest.reportName=%REPORT_NAME%
+
+REM Config for 10-10-1-Topic-NonDurable-Persistent
+echo Will run producer 10-10-1-Topic-NonDurable-Persistent...
+pause
+set CONFIG_FILE=./src/main/resources/producer-conf/AMQ-Prod-10-1-topic-persistent
+set REPORT_NAME=%PROVIDER%_Prod_Topic_NonDurable_Persistent_10_10_1.xml
+mvn activemq-perf:producer -DsysTest.propsConfigFile=%CONFIG_FILE% -DsysTest.spiClass=%SPI_CLASS% -DsysTest.extDir=%EXT_DIR% -DsysTest.reportName=%REPORT_NAME%
+
+REM Config for 10-10-10-Topic-NonDurable-Persistent
+echo Will run producer 10-10-10-Topic-NonDurable-Persistent...
+pause
+set CONFIG_FILE=./src/main/resources/producer-conf/AMQ-Prod-10-10-topic-persistent
+set REPORT_NAME=%PROVIDER%_Prod_Topic_NonDurable_Persistent_10_10_10.xml
+mvn activemq-perf:producer -DsysTest.propsConfigFile=%CONFIG_FILE% -DsysTest.spiClass=%SPI_CLASS% -DsysTest.extDir=%EXT_DIR% -DsysTest.reportName=%REPORT_NAME%
+
+REM Config for 1-1-1-Topic-Durable-NonPersistent
+echo Will run producer 1-1-1-Topic-Durable-NonPersistent...
+pause
+set CONFIG_FILE=./src/main/resources/producer-conf/AMQ-Prod-1-1-topic-nonpersistent
+set REPORT_NAME=%PROVIDER%_Prod_Topic_Durable_NonPersistent_1_1_1.xml
+mvn activemq-perf:producer -DsysTest.propsConfigFile=%CONFIG_FILE% -DsysTest.spiClass=%SPI_CLASS% -DsysTest.extDir=%EXT_DIR% -DsysTest.reportName=%REPORT_NAME%
+
+REM Config for 10-10-1-Topic-Durable-NonPersistent
+echo Will run producer 10-10-1-Topic-Durable-NonPersistent...
+pause
+set CONFIG_FILE=./src/main/resources/producer-conf/AMQ-Prod-10-1-topic-nonpersistent
+set REPORT_NAME=%PROVIDER%_Prod_Topic_Durable_NonPersistent_10_10_1.xml
+mvn activemq-perf:producer -DsysTest.propsConfigFile=%CONFIG_FILE% -DsysTest.spiClass=%SPI_CLASS% -DsysTest.extDir=%EXT_DIR% -DsysTest.reportName=%REPORT_NAME%
+
+REM Config for 10-10-10-Topic-Durable-NonPersistent
+echo Will run producer 10-10-10-Topic-Durable-NonPersistent...
+pause
+set CONFIG_FILE=./src/main/resources/producer-conf/AMQ-Prod-10-10-topic-nonpersistent
+set REPORT_NAME=%PROVIDER%_Prod_Topic_Durable_NonPersistent_10_10_10.xml
+mvn activemq-perf:producer -DsysTest.propsConfigFile=%CONFIG_FILE% -DsysTest.spiClass=%SPI_CLASS% -DsysTest.extDir=%EXT_DIR% -DsysTest.reportName=%REPORT_NAME%
+
+REM Config for 1-1-1-Topic-Durable-Persistent
+echo Will run producer 1-1-1-Topic-Durable-Persistent...
+pause
+set CONFIG_FILE=./src/main/resources/producer-conf/AMQ-Prod-1-1-topic-persistent
+set REPORT_NAME=%PROVIDER%_Prod_Topic_Durable_Persistent_1_1_1.xml
+mvn activemq-perf:producer -DsysTest.propsConfigFile=%CONFIG_FILE% -DsysTest.spiClass=%SPI_CLASS% -DsysTest.extDir=%EXT_DIR% -DsysTest.reportName=%REPORT_NAME%
+
+REM Config for 10-10-1-Topic-Durable-Persistent
+echo Will run producer 10-10-1-Topic-Durable-Persistent...
+pause
+set CONFIG_FILE=./src/main/resources/producer-conf/AMQ-Prod-10-1-topic-persistent
+set REPORT_NAME=%PROVIDER%_Prod_Topic_Durable_Persistent_10_10_1.xml
+mvn activemq-perf:producer -DsysTest.propsConfigFile=%CONFIG_FILE% -DsysTest.spiClass=%SPI_CLASS% -DsysTest.extDir=%EXT_DIR% -DsysTest.reportName=%REPORT_NAME%
+
+REM Config for 10-10-10-Topic-Durable-Persistent
+echo Will run producer 10-10-10-Topic-Durable-Persistent...
+pause
+set CONFIG_FILE=./src/main/resources/producer-conf/AMQ-Prod-10-10-topic-persistent
+set REPORT_NAME=%PROVIDER%_Prod_Topic_Durable_Persistent_10_10_10.xml
+mvn activemq-perf:producer -DsysTest.propsConfigFile=%CONFIG_FILE% -DsysTest.spiClass=%SPI_CLASS% -DsysTest.extDir=%EXT_DIR% -DsysTest.reportName=%REPORT_NAME%
---
+++ activemq-perftest/src/main/resources/test-scripts/sonic-cons.bat
@@ -0,0 +1,131 @@
+echo off
+
+set SPI_CLASS=org.apache.activemq.tool.spi.SonicMQReflectionSPI
+set EXT_DIR="C:/Sonic/MQ7.0/lib"
+set PROVIDER=SONIC
+
+REM Config for 1-1-1-Queue-NonPersistent
+echo Will run consumer 1-1-1-Queue-NonPersistent...
+pause
+set CONFIG_FILE=./src/main/resources/consumer-conf/AMQ-Cons-1-1-queue
+set REPORT_NAME=%PROVIDER%_Cons_Queue_NonPersistent_1_1_1.xml
+mvn activemq-perf:consumer -DsysTest.propsConfigFile=%CONFIG_FILE% -DsysTest.spiClass=%SPI_CLASS% -DsysTest.extDir=%EXT_DIR% -DsysTest.reportName=%REPORT_NAME%
+
+REM Config for 10-10-1-Queue-NonPersistent
+echo Will run consumer 10-10-1-Queue-NonPersistent...
+pause
+set CONFIG_FILE=./src/main/resources/consumer-conf/AMQ-Cons-10-1-queue
+set REPORT_NAME=%PROVIDER%_Cons_Queue_NonPersistent_10_10_1.xml
+mvn activemq-perf:consumer -DsysTest.propsConfigFile=%CONFIG_FILE% -DsysTest.spiClass=%SPI_CLASS% -DsysTest.extDir=%EXT_DIR% -DsysTest.reportName=%REPORT_NAME%
+
+REM Config for 10-10-10-Queue-NonPersistent
+echo Will run consumer 10-10-10-Queue-NonPersistent...
+pause
+set CONFIG_FILE=./src/main/resources/consumer-conf/AMQ-Cons-10-10-queue
+set REPORT_NAME=%PROVIDER%_Cons_Queue_NonPersistent_10_10_10.xml
+mvn activemq-perf:consumer -DsysTest.propsConfigFile=%CONFIG_FILE% -DsysTest.spiClass=%SPI_CLASS% -DsysTest.extDir=%EXT_DIR% -DsysTest.reportName=%REPORT_NAME%
+
+REM Config for 1-1-1-Queue-Persistent
+echo Will run consumer 1-1-1-Queue-Persistent...
+pause
+set CONFIG_FILE=./src/main/resources/consumer-conf/AMQ-Cons-1-1-queue
+set REPORT_NAME=%PROVIDER%_Cons_Queue_Persistent_1_1_1.xml
+mvn activemq-perf:consumer -DsysTest.propsConfigFile=%CONFIG_FILE% -DsysTest.spiClass=%SPI_CLASS% -DsysTest.extDir=%EXT_DIR% -DsysTest.reportName=%REPORT_NAME%
+
+REM Config for 10-10-1-Queue-Persistent
+echo Will run consumer 10-10-1-Queue-Persistent...
+pause
+set CONFIG_FILE=./src/main/resources/consumer-conf/AMQ-Cons-10-1-queue
+set REPORT_NAME=%PROVIDER%_Cons_Queue_Persistent_10_10_1.xml
+mvn activemq-perf:consumer -DsysTest.propsConfigFile=%CONFIG_FILE% -DsysTest.spiClass=%SPI_CLASS% -DsysTest.extDir=%EXT_DIR% -DsysTest.reportName=%REPORT_NAME%
+
+REM Config for 10-10-10-Queue-Persistent
+echo Will run consumer 10-10-10-Queue-Persistent...
+pause
+set CONFIG_FILE=./src/main/resources/consumer-conf/AMQ-Cons-10-10-queue
+set REPORT_NAME=%PROVIDER%_Cons_Queue_Persistent_10_10_10.xml
+mvn activemq-perf:consumer -DsysTest.propsConfigFile=%CONFIG_FILE% -DsysTest.spiClass=%SPI_CLASS% -DsysTest.extDir=%EXT_DIR% -DsysTest.reportName=%REPORT_NAME%
+
+REM Config for 1-1-1-Topic-NonDurable-NonPersistent
+echo Will run consumer 1-1-1-Topic-NonDurable-NonPersistent...
+pause
+set CONFIG_FILE=./src/main/resources/consumer-conf/AMQ-Cons-1-1-topic-nondurable
+set REPORT_NAME=%PROVIDER%_Cons_Topic_NonDurable_NonPersistent_1_1_1.xml
+mvn activemq-perf:consumer -DsysTest.propsConfigFile=%CONFIG_FILE% -DsysTest.spiClass=%SPI_CLASS% -DsysTest.extDir=%EXT_DIR% -DsysTest.reportName=%REPORT_NAME%
+
+REM Config for 10-10-1-Topic-NonDurable-NonPersistent
+echo Will run consumer 10-10-1-Topic-NonDurable-NonPersistent...
+pause
+set CONFIG_FILE=./src/main/resources/consumer-conf/AMQ-Cons-10-1-topic-nondurable
+set REPORT_NAME=%PROVIDER%_Cons_Topic_NonDurable_NonPersistent_10_10_1.xml
+mvn activemq-perf:consumer -DsysTest.propsConfigFile=%CONFIG_FILE% -DsysTest.spiClass=%SPI_CLASS% -DsysTest.extDir=%EXT_DIR% -DsysTest.reportName=%REPORT_NAME%
+
+REM Config for 10-10-10-Topic-NonDurable-NonPersistent
+echo Will run consumer 10-10-10-Topic-NonDurable-NonPersistent...
+pause
+set CONFIG_FILE=./src/main/resources/consumer-conf/AMQ-Cons-10-10-topic-nondurable
+set REPORT_NAME=%PROVIDER%_Cons_Topic_NonDurable_NonPersistent_10_10_10.xml
+mvn activemq-perf:consumer -DsysTest.propsConfigFile=%CONFIG_FILE% -DsysTest.spiClass=%SPI_CLASS% -DsysTest.extDir=%EXT_DIR% -DsysTest.reportName=%REPORT_NAME%
+
+REM Config for 1-1-1-Topic-NonDurable-Persistent
+echo Will run consumer 1-1-1-Topic-NonDurable-Persistent...
+pause
+set CONFIG_FILE=./src/main/resources/consumer-conf/AMQ-Cons-1-1-topic-nondurable
+set REPORT_NAME=%PROVIDER%_Cons_Topic_NonDurable_Persistent_1_1_1.xml
+mvn activemq-perf:consumer -DsysTest.propsConfigFile=%CONFIG_FILE% -DsysTest.spiClass=%SPI_CLASS% -DsysTest.extDir=%EXT_DIR% -DsysTest.reportName=%REPORT_NAME%
+
+REM Config for 10-10-1-Topic-NonDurable-Persistent
+echo Will run consumer 10-10-1-Topic-NonDurable-Persistent...
+pause
+set CONFIG_FILE=./src/main/resources/consumer-conf/AMQ-Cons-10-1-topic-nondurable
+set REPORT_NAME=%PROVIDER%_Cons_Topic_NonDurable_Persistent_10_10_1.xml
+mvn activemq-perf:consumer -DsysTest.propsConfigFile=%CONFIG_FILE% -DsysTest.spiClass=%SPI_CLASS% -DsysTest.extDir=%EXT_DIR% -DsysTest.reportName=%REPORT_NAME%
+
+REM Config for 10-10-10-Topic-NonDurable-Persistent
+echo Will run consumer 10-10-10-Topic-NonDurable-Persistent...
+pause
+set CONFIG_FILE=./src/main/resources/consumer-conf/AMQ-Cons-10-10-topic-nondurable
+set REPORT_NAME=%PROVIDER%_Cons_Topic_NonDurable_Persistent_10_10_10.xml
+mvn activemq-perf:consumer -DsysTest.propsConfigFile=%CONFIG_FILE% -DsysTest.spiClass=%SPI_CLASS% -DsysTest.extDir=%EXT_DIR% -DsysTest.reportName=%REPORT_NAME%
+
+REM Config for 1-1-1-Topic-Durable-NonPersistent
+echo Will run consumer 1-1-1-Topic-Durable-NonPersistent...
+pause
+set CONFIG_FILE=./src/main/resources/consumer-conf/AMQ-Cons-1-1-topic-durable
+set REPORT_NAME=%PROVIDER%_Cons_Topic_Durable_NonPersistent_1_1_1.xml
+mvn activemq-perf:consumer -DsysTest.propsConfigFile=%CONFIG_FILE% -DsysTest.spiClass=%SPI_CLASS% -DsysTest.extDir=%EXT_DIR% -DsysTest.reportName=%REPORT_NAME%
+
+REM Config for 10-10-1-Topic-Durable-NonPersistent
+echo Will run consumer 10-10-1-Topic-Durable-NonPersistent...
+pause
+set CONFIG_FILE=./src/main/resources/consumer-conf/AMQ-Cons-10-1-topic-durable
+set REPORT_NAME=%PROVIDER%_Cons_Topic_Durable_NonPersistent_10_10_1.xml
+mvn activemq-perf:consumer -DsysTest.propsConfigFile=%CONFIG_FILE% -DsysTest.spiClass=%SPI_CLASS% -DsysTest.extDir=%EXT_DIR% -DsysTest.reportName=%REPORT_NAME%
+
+REM Config for 10-10-10-Topic-Durable-NonPersistent
+echo Will run consumer 10-10-10-Topic-Durable-NonPersistent...
+pause
+set CONFIG_FILE=./src/main/resources/consumer-conf/AMQ-Cons-10-10-topic-durable
+set REPORT_NAME=%PROVIDER%_Cons_Topic_Durable_NonPersistent_10_10_10.xml
+mvn activemq-perf:consumer -DsysTest.propsConfigFile=%CONFIG_FILE% -DsysTest.spiClass=%SPI_CLASS% -DsysTest.extDir=%EXT_DIR% -DsysTest.reportName=%REPORT_NAME%
+
+REM Config for 1-1-1-Topic-Durable-Persistent
+echo Will run consumer 1-1-1-Topic-Durable-Persistent...
+pause
+set CONFIG_FILE=./src/main/resources/consumer-conf/AMQ-Cons-1-1-topic-durable
+set REPORT_NAME=%PROVIDER%_Cons_Topic_Durable_Persistent_1_1_1.xml
+mvn activemq-perf:consumer -DsysTest.propsConfigFile=%CONFIG_FILE% -DsysTest.spiClass=%SPI_CLASS% -DsysTest.extDir=%EXT_DIR% -DsysTest.reportName=%REPORT_NAME%
+
+REM Config for 10-10-1-Topic-Durable-Persistent
+echo Will run consumer 10-10-1-Topic-Durable-Persistent...
+pause
+set CONFIG_FILE=./src/main/resources/consumer-conf/AMQ-Cons-10-1-topic-durable
+set REPORT_NAME=%PROVIDER%_Cons_Topic_Durable_Persistent_10_10_1.xml
+mvn activemq-perf:consumer -DsysTest.propsConfigFile=%CONFIG_FILE% -DsysTest.spiClass=%SPI_CLASS% -DsysTest.extDir=%EXT_DIR% -DsysTest.reportName=%REPORT_NAME%
+
+REM Config for 10-10-10-Topic-Durable-Persistent
+echo Will run consumer 10-10-10-Topic-Durable-Persistent...
+pause
+set CONFIG_FILE=./src/main/resources/consumer-conf/AMQ-Cons-10-10-topic-durable
+set REPORT_NAME=%PROVIDER%_Cons_Topic_Durable_Persistent_10_10_10.xml
+mvn activemq-perf:consumer -DsysTest.propsConfigFile=%CONFIG_FILE% -DsysTest.spiClass=%SPI_CLASS% -DsysTest.extDir=%EXT_DIR% -DsysTest.reportName=%REPORT_NAME%
