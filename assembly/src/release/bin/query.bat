7f883f6fe207cd40211ad897ec96a56d33c46aa9
Hiram R. Chirino <chirino@apache.org>
2006-07-28 05:00:24 +0000

    Applied new source header boiler plate per http://www.apache.org/legal/src-headers.html
    git-svn-id: https://svn.apache.org/repos/asf/incubator/activemq/trunk@426384 13f79535-47bb-0310-9956-ffa450edef68

--- assembly/src/release/bin/query.bat
+++ assembly/src/release/bin/query.bat
@@ -1,22 +1,21 @@
 @echo off
 
-REM   ActiveMQ shell script
+REM ------------------------------------------------------------------------
+REM Licensed to the Apache Software Foundation (ASF) under one or more
+REM contributor license agreements.  See the NOTICE file distributed with
+REM this work for additional information regarding copyright ownership.
+REM The ASF licenses this file to You under the Apache License, Version 2.0
+REM (the "License"); you may not use this file except in compliance with
+REM the License.  You may obtain a copy of the License at
 REM
-REM   This script is heavily based on the Ant script
+REM http://www.apache.org/licenses/LICENSE-2.0
 REM
-REM   Copyright 2005-2006 The Apache Software Foundation
-REM
-REM   Licensed under the Apache License, Version 2.0 (the "License");
-REM   you may not use this file except in compliance with the License.
-REM   You may obtain a copy of the License at
-REM
-REM   http://www.apache.org/licenses/LICENSE-2.0
-REM
-REM   Unless required by applicable law or agreed to in writing, software
-REM   distributed under the License is distributed on an "AS IS" BASIS,
-REM   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-REM   See the License for the specific language governing permissions and
-REM   limitations under the License.
+REM Unless required by applicable law or agreed to in writing, software
+REM distributed under the License is distributed on an "AS IS" BASIS,
+REM WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+REM See the License for the specific language governing permissions and
+REM limitations under the License.
+REM ------------------------------------------------------------------------
 
 if exist "%HOME%\activemqrc_pre.bat" call "%HOME%\activemqrc_pre.bat"
 
--- systest/itests/ejb/src/java/org/activemq/itest/ejb/JMSToolBean.java
+++ systest/itests/ejb/src/java/org/activemq/itest/ejb/JMSToolBean.java
@@ -1,10 +1,11 @@
 /**
  *
- * Copyright 2004 Hiram Chirino
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -13,8 +14,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- **/
+ */
 package org.activemq.itest.ejb;
 
 import java.rmi.RemoteException;
