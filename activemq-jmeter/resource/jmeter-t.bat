decbe75a58a127ec6f49eeb8553465e688342e54
Hiram R. Chirino <chirino@apache.org>
2006-07-28 05:00:24 +0000

    Applied new source header boiler plate per http://www.apache.org/legal/src-headers.html
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/activemq/trunk@426384 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-jmeter/resource/jmeter-t.bat
+++ activemq-jmeter/resource/jmeter-t.bat
@@ -1,18 +1,20 @@
 @echo off
-rem   $Id: jmeter-t.bat,v 1.3 2005/03/18 15:26:54 mstover1 Exp $
-rem   Copyright 2001-2004 The Apache Software Foundation
-rem 
-rem   Licensed under the Apache License, Version 2.0 (the "License");
-rem   you may not use this file except in compliance with the License.
-rem   You may obtain a copy of the License at
-rem 
-rem       http://www.apache.org/licenses/LICENSE-2.0
-rem 
-rem   Unless required by applicable law or agreed to in writing, software
-rem   distributed under the License is distributed on an "AS IS" BASIS,
-rem   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-rem   See the License for the specific language governing permissions and
-rem   limitations under the License.
+REM ------------------------------------------------------------------------
+REM Licensed to the Apache Software Foundation (ASF) under one or more
+REM contributor license agreements.  See the NOTICE file distributed with
+REM this work for additional information regarding copyright ownership.
+REM The ASF licenses this file to You under the Apache License, Version 2.0
+REM (the "License"); you may not use this file except in compliance with
+REM the License.  You may obtain a copy of the License at
+REM
+REM http://www.apache.org/licenses/LICENSE-2.0
+REM
+REM Unless required by applicable law or agreed to in writing, software
+REM distributed under the License is distributed on an "AS IS" BASIS,
+REM WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+REM See the License for the specific language governing permissions and
+REM limitations under the License.
+REM ------------------------------------------------------------------------
 
 rem  ============================================
 rem
--- openwire-c/src/libactivemq/amqcs.c
+++ openwire-c/src/libactivemq/amqcs.c
@@ -1,33 +1,21 @@
 /**
  *
- * Copyright 2005-2006 The Apache Software Foundation
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-#include "amqcs.h"
-
-#include <apr.h>
-#include "amqcs.h"
-
-#ifdef WIN32
-#define snprintf _snprintf
-#endif
-
-static ow_byte ACTIVEMQ_MAGIC [] = {0x41,0x63,0x74,0x69,0x76,0x65,0x4D,0x51}; // ActiveMQ
-
-/**
-* An ActiveMQ Single Threaded Client Connection
- */ 
+ 
 struct amqcs_connection {
    ow_transport *transport;
    ow_ConnectionInfo *info;
