aaad682655965f6a5d6db3b684fc833d7a8db45e
Hiram R. Chirino <chirino@apache.org>
2006-07-28 03:22:58 +0000

    Applied new source header boiler plate per http://www.apache.org/legal/src-headers.html
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/activemq/trunk@426370 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-core/src/gram/script/GenerateJavaMarshalling.groovy
+++ activemq-core/src/gram/script/GenerateJavaMarshalling.groovy
@@ -27,11 +27,12 @@ class GenerateJavaMarshalling extends OpenWireJavaMarshallingScript {
  	void generateFile(PrintWriter out) {
         out << """/**
  *
- * Copyright 2005-2006 The Apache Software Foundation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -234,11 +235,12 @@ out << """
     	void generateFactory(PrintWriter out) {
             out << """/**
  *
- * Copyright 2005-2006 The Apache Software Foundation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
