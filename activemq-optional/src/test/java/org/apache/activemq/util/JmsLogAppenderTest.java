3c441d03aed96f092ec686e2666f2d264d0af51e
Adrian T. Co <aco@apache.org>
2006-09-29 08:49:59 +0000

    Added test case for JmsLogAppender For: https://issues.apache.org/activemq/browse/AMQ-935
    git-svn-id: https://svn.apache.org/repos/asf/incubator/activemq/trunk@451170 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-optional/src/test/java/org/apache/activemq/util/JmsLogAppenderTest.java
+++ activemq-optional/src/test/java/org/apache/activemq/util/JmsLogAppenderTest.java
@@ -24,23 +24,89 @@ import java.util.Properties;
 
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
+import org.apache.log4j.Level;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.broker.BrokerService;
+import org.apache.activemq.command.ActiveMQTopic;
 
-/**
- * @version $Revision$
- */
+import javax.jms.JMSException;
+import javax.jms.Connection;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
 public class JmsLogAppenderTest extends TestCase {
+    protected BrokerService broker;
 
+    public void testLoggingWithJMS() throws IOException, JMSException {
+        // Setup the consumers
+        MessageConsumer info, debug, warn;
+        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory("vm://localhost");
+        Connection conn = factory.createConnection();
+        conn.start();
 
-    public void testLoggingWithJMS() throws IOException {
+        warn  = conn.createSession(false, Session.AUTO_ACKNOWLEDGE).createConsumer(new ActiveMQTopic("log4j.MAIN.WARN"));
+        info  = conn.createSession(false, Session.AUTO_ACKNOWLEDGE).createConsumer(new ActiveMQTopic("log4j.MAIN.INFO"));
+        debug = conn.createSession(false, Session.AUTO_ACKNOWLEDGE).createConsumer(new ActiveMQTopic("log4j.MAIN.DEBUG"));
 
-         // lets try configure log4j
-         Properties properties = new Properties();
-         properties.load(getClass().getResourceAsStream("test-log4j.properties"));
-         PropertyConfigurator.configure(properties);
+        // lets try configure log4j
+        Properties properties = new Properties();
+        properties.load(getClass().getResourceAsStream("test-log4j.properties"));
+        PropertyConfigurator.configure(properties);
 
-         Logger.getLogger("FOO.BAR").info("Hello");
-         Logger.getLogger("FOO.BAR.WHATNOT").debug("A debug message");
-         Logger.getLogger("FOO.BAR.WHATNOT.ANOTHER").warn("Some warnings");
+        Logger warnLog, infoLog, debugLog;
 
+        warnLog = Logger.getLogger("MAIN.WARN");
+        warnLog.setLevel(Level.WARN);
+        warnLog.warn("Warn Message");
+        warnLog.info("Info Message");
+        warnLog.debug("Debug Message");
+
+        infoLog = Logger.getLogger("MAIN.INFO");
+        infoLog.setLevel(Level.INFO);
+        infoLog.warn("Warn Message");
+        infoLog.info("Info Message");
+        infoLog.debug("Debug Message");
+
+        debugLog = Logger.getLogger("MAIN.DEBUG");
+        debugLog.setLevel(Level.DEBUG);
+        debugLog.warn("Warn Message");
+        debugLog.info("Info Message");
+        debugLog.debug("Debug Message");
+
+        TextMessage msg;
+
+        // Test warn level
+        msg = (TextMessage)warn.receive(1000);
+        assertNotNull(msg);
+        assertEquals("Warn Message", msg.getText());
+
+        msg = (TextMessage)warn.receive(1000);
+        assertNull(msg); // We should not receive anymore message because our level is warning only
+
+        // Test info level
+        msg = (TextMessage)info.receive(1000);
+        assertNotNull(msg);
+        assertEquals("Warn Message", msg.getText());
+
+        msg = (TextMessage)info.receive(1000);
+        assertNotNull(msg);
+        assertEquals("Info Message", msg.getText());
+
+        msg = (TextMessage)info.receive(1000);
+        assertNull(msg); // We should not receive the debug message
+
+        // Test debug level
+        msg = (TextMessage)debug.receive(1000);
+        assertNotNull(msg);
+        assertEquals("Warn Message", msg.getText());
+
+        msg = (TextMessage)debug.receive(1000);
+        assertNotNull(msg);
+        assertEquals("Info Message", msg.getText());
+
+        msg = (TextMessage)debug.receive(1000);
+        assertNotNull(msg);
     }
+
 }
