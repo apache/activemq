0c6165f4bb6c959edd22bfc92ea07d0b7b0f4d32
James Strachan <jstrachan@apache.org>
2006-10-23 18:51:21 +0000

    added an implementation of stomp+ssl along with a test case (which doesn't quite work yet but is excluded from the pom.xml  :) for AMQ-998
    git-svn-id: https://svn.apache.org/repos/asf/incubator/activemq/trunk@467079 13f79535-47bb-0310-9956-ffa450edef68

---
+++ activemq-core/src/main/java/org/apache/activemq/transport/stomp/StompSslTransportFactory.java
@@ -0,0 +1,42 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.activemq.transport.stomp;
+
+import org.apache.activemq.transport.tcp.TcpTransportFactory;
+import org.apache.activemq.transport.tcp.SslTransportFactory;
+import org.apache.activemq.transport.Transport;
+import org.apache.activemq.wireformat.WireFormat;
+
+import java.util.Map;
+
+/**
+ * A <a href="http://stomp.codehaus.org/">STOMP</a> over SSL transport factory
+ *
+ * @version $Revision$
+ */
+public class StompSslTransportFactory extends SslTransportFactory {
+
+    protected String getDefaultWireFormatType() {
+        return "stomp";
+    }
+
+    public Transport compositeConfigure(Transport transport, WireFormat format, Map options) {
+    	transport = new StompTransportFilter(transport, new LegacyFrameTranslator());
+    	return super.compositeConfigure(transport, format, options);
+    }
+}
---
+++ activemq-core/src/test/java/org/apache/activemq/transport/stomp/StompSslTest.java
@@ -0,0 +1,43 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.activemq.transport.stomp;
+
+import org.apache.activemq.transport.tcp.SslSocketHelper;
+
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.SSLSocket;
+import javax.net.SocketFactory;
+import java.net.Socket;
+import java.net.URI;
+import java.io.IOException;
+
+/**
+ * @version $Revision$
+ */
+public class StompSslTest extends StompTest {
+
+    protected void setUp() throws Exception {
+        bindAddress = "stomp+ssl://localhost:0";
+        super.setUp();
+    }
+
+    protected Socket createSocket(URI connectUri) throws IOException {
+        SocketFactory factory = SSLSocketFactory.getDefault();
+        return factory.createSocket("127.0.0.1", connectUri.getPort());
+    }
+}
