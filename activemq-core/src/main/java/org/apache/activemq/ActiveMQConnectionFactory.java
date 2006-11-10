395bf8d74016964e6bb7afa19becb67e603fdec0
Hiram R. Chirino <chirino@apache.org>
2006-11-10 04:19:15 +0000

    Merged in revision 473167 from trunk
    git-svn-id: https://svn.apache.org/repos/asf/incubator/activemq/branches/activemq-4.1@473181 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-core/src/main/java/org/apache/activemq/ActiveMQConnectionFactory.java
+++ activemq-core/src/main/java/org/apache/activemq/ActiveMQConnectionFactory.java
@@ -260,7 +260,7 @@ public class ActiveMQConnectionFactory extends JNDIBaseStorable implements Conne
             transport.start();
 
             if( clientID !=null )
-                connection.setClientID(clientID);
+                connection.setDefaultClientID(clientID);
 
             return connection;
         }
