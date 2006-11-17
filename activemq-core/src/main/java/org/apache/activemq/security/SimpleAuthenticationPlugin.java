9dcd00823b64553a153ed1f1651f574ad8875aad
Jonas B. Lim <jlim@apache.org>
2006-11-17 10:24:04 +0000

    applied patch for http://issues.apache.org/activemq/browse/AMQ-1010
    git-svn-id: https://svn.apache.org/repos/asf/incubator/activemq/trunk@476099 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-core/src/main/java/org/apache/activemq/security/SimpleAuthenticationPlugin.java
+++ activemq-core/src/main/java/org/apache/activemq/security/SimpleAuthenticationPlugin.java
@@ -17,6 +17,16 @@
  */
 package org.apache.activemq.security;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+import org.apache.activemq.jaas.GroupPrincipal;
+
 import org.apache.activemq.broker.Broker;
 import org.apache.activemq.broker.BrokerPlugin;
 
@@ -26,13 +36,19 @@ import java.util.Map;
  * A simple authentication plugin
  *  
  * @org.apache.xbean.XBean element="simpleAuthenticationPlugin" description="Provides a simple authentication
- * plugin configured with a map of user-passwords and a map of user-groups"
+ * plugin configured with a map of user-passwords and a map of user-groups or a list of authentication users"
  * 
  * @version $Revision$
  */
 public class SimpleAuthenticationPlugin implements BrokerPlugin {
     private Map userPasswords;
     private Map userGroups;
+
+    public SimpleAuthenticationPlugin() {}
+
+    public SimpleAuthenticationPlugin(List users) {
+    	setUsers(users);
+    }
 
     public Broker installPlugin(Broker broker) {
         return new SimpleAuthenticationBroker(broker, userPasswords, userGroups);
@@ -41,6 +57,27 @@ public class SimpleAuthenticationPlugin implements BrokerPlugin {
     public Map getUserGroups() {
         return userGroups;
     }
+    
+    /**
+     * Sets individual users for authentication
+     * 
+     * @org.apache.xbean.ElementType class="org.apache.activemq.security.AuthenticationUser"
+     */
+    public void setUsers(List users) {
+    	userPasswords = new HashMap();
+    	userGroups = new HashMap();
+    	for (Iterator it = users.iterator(); it.hasNext();) {
+    	    AuthenticationUser user = (AuthenticationUser)it.next();
+    	    userPasswords.put(user.getUsername(), user.getPassword());
+            Set groups = new HashSet();
+            StringTokenizer iter = new StringTokenizer(user.getGroup(), ",");
+            while (iter.hasMoreTokens()) {
+                String name = iter.nextToken().trim();
+                groups.add(new GroupPrincipal(name));
+            }
+    	    userGroups.put(user.getUsername(), groups);
+    	}
+    }    
 
     /**
      * Sets the groups a user is in. The key is the user name and the value is a Set of groups
