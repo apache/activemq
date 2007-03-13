77aed0e4be1eb42bc63d8dba9b0564e1cce9afc4
Robert Davies <rajdavies@apache.org>
2007-03-13 16:39:46 +0000

    test for converting Properties to and from a String
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/trunk@517764 13f79535-47bb-0310-9956-ffa450edef68

---
+++ activemq-core/src/test/java/org/apache/activemq/util/MarshallingSupportTest.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+
+package org.apache.activemq.util;
+
+import java.io.IOException;
+import java.util.Properties;
+import junit.framework.TestCase;
+
+/**
+ * @author rajdavies
+ * 
+ */
+public class MarshallingSupportTest extends TestCase{
+
+    /**
+     * @throws java.lang.Exception
+     * @see junit.framework.TestCase#setUp()
+     */
+    protected void setUp() throws Exception{
+        super.setUp();
+    }
+
+    /**
+     * @throws java.lang.Exception
+     * @see junit.framework.TestCase#tearDown()
+     */
+    protected void tearDown() throws Exception{
+        super.tearDown();
+    }
+
+    /**
+     * Test method for {@link org.apache.activemq.util.MarshallingSupport#propertiesToString(java.util.Properties)}.
+     * 
+     * @throws Exception
+     */
+    public void testPropertiesToString() throws Exception{
+        Properties props=new Properties();
+        for(int i=0;i<10;i++){
+            String key="key"+i;
+            String value="value"+i;
+            props.put(key,value);
+        }
+        String str=MarshallingSupport.propertiesToString(props);
+        Properties props2=MarshallingSupport.stringToProperties(str);
+        assertEquals(props,props2);
+    }
+}
