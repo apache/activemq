72cee90f7a6b5bc8109fc97ca69bf4939a22fd50
James Strachan <jstrachan@apache.org>
2007-05-24 10:31:16 +0000

    fixed typeo
    git-svn-id: https://svn.apache.org/repos/asf/activemq/trunk@541260 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-core/src/main/java/org/apache/activemq/ActiveMQConnectionFactory.java
+++ activemq-core/src/main/java/org/apache/activemq/ActiveMQConnectionFactory.java
@@ -765,7 +765,7 @@ public class ActiveMQConnectionFactory extends JNDIBaseStorable implements Conne
     }
 
     /**
-     * Enables the timemout from a connection creation to when a warning is generated
+     * Enables the timeout from a connection creation to when a warning is generated
      * if the connection is not properly started via {@link Connection#start()}. It is a very
      * common gotcha to forget to
      * <a href="http://activemq.apache.org/i-am-not-receiving-any-messages-what-is-wrong.html">start the connection</a>
