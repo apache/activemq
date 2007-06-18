39a3d275d6a0d4a825007867e851b5c37eebe1c2
Adrian T. Co <aco@apache.org>
2007-06-18 10:48:41 +0000

    Patch applied for: https://issues.apache.org/activemq/browse/AMQ-1208 Test case added
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/trunk@548300 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-optional/src/main/java/org/apache/activemq/util/xstream/XStreamMessageTransformer.java
+++ activemq-optional/src/main/java/org/apache/activemq/util/xstream/XStreamMessageTransformer.java
@@ -36,40 +36,47 @@ import java.io.StringReader;
 import java.io.StringWriter;
 
 /**
- * Transforms object messages to text messages using {@link XStream}
+ * Transforms object messages to text messages and vice versa using {@link XStream}
  *
  * @version $Revision$
  */
 public class XStreamMessageTransformer extends MessageTransformerSupport {
     private XStream xStream;
 
+    /**
+     * Defines the direction in which transformation goes.
+     * If 'false' (default),
+     *  - producer transformation transforms from Object to XML.
+     *  - consumer transformation transforms from XML to Object.
+     * If 'true' transformation directions changes,
+     *  - producer transformation transforms from XML to Object.
+     *  - consumer transformation transforms from Object to XML.
+	 */
+	private boolean reverse;
 
-    public Message producerTransform(Session session, MessageProducer producer, Message message) throws JMSException {
-        if (message instanceof ObjectMessage) {
-            TextMessage answer = session.createTextMessage(marshall(session, producer, (ObjectMessage) message));
-            copyProperties(message, answer);
-            return answer;
-        }
-        return message;
-    }
+	public XStreamMessageTransformer() {
+		new XStreamMessageTransformer(false);
+	}
 
+	public XStreamMessageTransformer(boolean direction) {
+		this.reverse = direction;
+	}
 
     public Message consumerTransform(Session session, MessageConsumer consumer, Message message) throws JMSException {
-        if (message instanceof TextMessage) {
-            TextMessage textMessage = (TextMessage) message;
-            Object object = unmarshall(session, consumer, textMessage);
-            if (object instanceof Serializable) {
-                ObjectMessage answer = session.createObjectMessage((Serializable) object);
-                copyProperties(message, answer);
-                return answer;
-            }
-            else {
-                throw new JMSException("Object is not serializable: " + object);
-            }
-        }
-        return message;
-    }
+		if (reverse) {
+			return objectToText(session, message);
+		} else {
+			return textToObject(session, message);
+		}
+	}
 
+	public Message producerTransform(Session session, MessageProducer producer, Message message) throws JMSException {
+		if (reverse) {
+			return textToObject(session, message);
+		} else {
+			return objectToText(session, message);
+		}
+	}
 
     // Properties
     // -------------------------------------------------------------------------
@@ -90,10 +97,58 @@ public class XStreamMessageTransformer extends MessageTransformerSupport {
         return new XStream();
     }
 
+
+    public boolean isReverse() {
+        return reverse;
+    }
+
+    public void setReverse(boolean reverse) {
+        this.reverse = reverse;
+    }
+
+    /**
+     * Transforms an incoming XML encoded {@link TextMessage} to an {@link ObjectMessage}
+     * @param session - JMS session currently being used
+     * @param message - if this is a TextMessage, it will be transformed to an ObjectMessage
+     * @return ObjectMessage, if the incoming message is a TextMessage, the original message otherwise
+     * @throws JMSException
+     */
+    protected Message textToObject(Session session, Message message) throws JMSException {
+        if (message instanceof TextMessage) {
+            TextMessage textMessage = (TextMessage) message;
+            Object object = unmarshall(session, textMessage);
+            if (object instanceof Serializable) {
+                ObjectMessage answer = session.createObjectMessage((Serializable) object);
+                copyProperties(message, answer);
+                return answer;
+            }
+            else {
+                throw new JMSException("Object is not serializable: " + object);
+            }
+        }
+        return message;
+	}
+
+    /**
+     * Transforms an incoming {@link ObjectMessage} to an XML encoded {@link TextMessage}
+     * @param session - JMS session currently being used
+     * @param message - if this is an ObjectMessage, it will be transformed to a TextMessage
+     * @return XML encoded TextMessage, if the incoming messsage is an ObjectMessge, the original message otherwise
+     * @throws JMSException
+     */
+    protected Message objectToText(Session session, Message message) throws JMSException {
+        if (message instanceof ObjectMessage) {
+            TextMessage answer = session.createTextMessage(marshall(session, (ObjectMessage) message));
+            copyProperties(message, answer);
+            return answer;
+        }
+        return message;
+	}
+
     /**
      * Marshalls the Object in the {@link ObjectMessage} to a string using XML encoding
      */
-    protected String marshall(Session session, MessageProducer producer, ObjectMessage objectMessage) throws JMSException {
+    protected String marshall(Session session, ObjectMessage objectMessage) throws JMSException {
         Serializable object = objectMessage.getObject();
         StringWriter buffer = new StringWriter();
         HierarchicalStreamWriter out = new PrettyPrintWriter(buffer);
@@ -101,10 +156,11 @@ public class XStreamMessageTransformer extends MessageTransformerSupport {
         return buffer.toString();
     }
 
-    /**
-     * Unmarshalls the Object using XML encoding of the String
+
+	/**
+     * Unmarshalls the XML encoded message in the {@link TextMessage} to an Object
      */
-    protected Object unmarshall(Session session, MessageConsumer consumer, TextMessage textMessage) throws JMSException {
+    protected Object unmarshall(Session session, TextMessage textMessage) throws JMSException {
         HierarchicalStreamReader in = new XppReader(new StringReader(textMessage.getText()));
         return getXStream().unmarshal(in);
     }
