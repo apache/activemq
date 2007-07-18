63fb73959a0b29136989156c3ca82a0e3cb9a882
Robert Davies <rajdavies@apache.org>
2007-07-18 20:31:09 +0000

    Added some java doc
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/trunk@557386 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-core/src/main/java/org/apache/activemq/store/ReferenceStoreAdapter.java
+++ activemq-core/src/main/java/org/apache/activemq/store/ReferenceStoreAdapter.java
@@ -18,10 +18,13 @@
 package org.apache.activemq.store;
 
 import java.io.IOException;
+import java.util.Map;
 import java.util.Set;
 
 import org.apache.activemq.command.ActiveMQQueue;
 import org.apache.activemq.command.ActiveMQTopic;
+import org.apache.activemq.command.TransactionId;
+import org.apache.activemq.store.amq.AMQTx;
 
 /**
  * Adapter to the actual persistence mechanism used with ActiveMQ
@@ -32,14 +35,24 @@ public interface ReferenceStoreAdapter extends PersistenceAdapter {
 
     /**
      * Factory method to create a new queue message store with the given destination name
+     * @param destination 
+     * @return the QueueReferenceStore
+     * @throws IOException 
      */
     public ReferenceStore createQueueReferenceStore(ActiveMQQueue destination) throws IOException;
 
     /**
      * Factory method to create a new topic message store with the given destination name
+     * @param destination
+     * @return  the TopicRefererenceStore
+     * @throws IOException 
      */
     public TopicReferenceStore createTopicReferenceStore(ActiveMQTopic destination) throws IOException;
 
+	/**
+	 * @return Set of File ids in use
+	 * @throws IOException
+	 */
 	public Set<Integer> getReferenceFileIdsInUse() throws IOException;
     
     /**
@@ -60,5 +73,19 @@ public interface ReferenceStoreAdapter extends PersistenceAdapter {
      *
      */
     public void recoverState() throws IOException;
+    
+    /**
+     * Save prepared transactions
+     * @param map
+     * @throws IOException
+     */
+    public void savePreparedState(Map<TransactionId, AMQTx> map)throws IOException;
+    
+    /**
+     * 
+     * @return saved prepared transactions
+     * @throws IOException
+     */
+    public Map<TransactionId, AMQTx> retrievePreparedState() throws IOException;
 
 }
