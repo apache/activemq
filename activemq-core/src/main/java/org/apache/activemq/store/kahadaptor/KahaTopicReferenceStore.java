5bcab3ffe8a4cf0cd7295b9b660cb1ebda32235b
Robert Davies <rajdavies@apache.org>
2007-09-05 15:41:19 +0000

    use potentially shorter container name for the topic subscriber references
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/trunk@572974 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-core/src/main/java/org/apache/activemq/store/kahadaptor/KahaTopicReferenceStore.java
+++ activemq-core/src/main/java/org/apache/activemq/store/kahadaptor/KahaTopicReferenceStore.java
@@ -109,7 +109,7 @@ public class KahaTopicReferenceStore extends KahaReferenceStore implements Topic
     }
 
     protected ListContainer addSubscriberMessageContainer(String key) throws IOException {
-        ListContainer container = store.getListContainer(destination, "TSR-" + key);
+        ListContainer container = store.getListContainer(key, "TSR-" + destination);
         Marshaller marshaller = new ConsumerMessageRefMarshaller();
         container.setMarshaller(marshaller);
         TopicSubContainer tsc = new TopicSubContainer(container);
