b8f2f17d479e9f0174858e32a7e509517bbde48f
Hiram R. Chirino <chirino@apache.org>
2008-01-09 18:44:34 +0000

    Upgrade to xbean 3.3-SNAPSHOT to pull in the fix for: http://issues.apache.org/jira/browse/XBEAN-101 PropertyPlaceholderConfigurer can not be used to resolve values assigned to xbean properties that have a custom PropertyEditor
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/trunk@610502 13f79535-47bb-0310-9956-ffa450edef68

---
+++ activemq-core/src/test/java/org/apache/activemq/memory/MemoryPropertyTest.java
@@ -0,0 +1,80 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.activemq.memory;
+
+import java.io.File;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.broker.BrokerFactory;
+import org.apache.activemq.broker.BrokerService;
+import org.apache.activemq.xbean.BrokerFactoryBean;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import junit.framework.TestCase;
+
+public class MemoryPropertyTest extends TestCase {
+
+    private static final transient Log LOG = LogFactory.getLog(MemoryPropertyTest.class);
+    BrokerService broker;
+
+    
+    /**
+     * Sets up a test where the producer and consumer have their own connection.
+     * 
+     * @see junit.framework.TestCase#setUp()
+     */
+    protected void setUp() throws Exception {
+        // Create broker from resource
+        LOG.info("Creating broker... ");
+        broker = createBroker("xbean:org/apache/activemq/memory/activemq.xml");
+        LOG.info("Success");
+        super.setUp();
+    }
+    
+    protected BrokerService createBroker(String resource) throws Exception {
+        return BrokerFactory.createBroker(resource);
+    }
+
+
+    /*
+     * Stops the Broker
+     * 
+     * @see junit.framework.TestCase#tearDown()
+     */
+    protected void tearDown() throws Exception {
+        LOG.info("Closing Broker");
+        if (broker != null) {
+            broker.stop();
+        }
+        LOG.info("Broker closed...");
+    }
+
+
+
+    public void testBrokerInitialized() {
+        assertTrue("We should have a broker", broker != null);
+
+        assertEquals("test-broker", broker.getBrokerName());
+        assertEquals(1024, broker.getSystemUsage().getMemoryUsage().getLimit());
+        assertEquals(34, broker.getSystemUsage().getMemoryUsage().getPercentUsageMinDelta());
+        
+    }
+
+}
