06f81d8b67c5d09e87aab094de8a65aee2f69047
Hiram R. Chirino <chirino@apache.org>
2008-04-07 16:35:16 +0000

    Fix for AMQ-1381: makes client certs work
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/trunk@645599 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-core/src/main/java/org/apache/activemq/transport/tcp/SslTransport.java
+++ activemq-core/src/main/java/org/apache/activemq/transport/tcp/SslTransport.java
@@ -80,7 +80,7 @@ public class SslTransport extends TcpTransport {
      * 
      * @param command The Command coming in.
      */
-    public void doConsume(Command command) {
+    public void doConsume(Object command) {
         // The instanceof can be avoided, but that would require modifying the
         // Command clas tree and that would require too much effort right
         // now.
