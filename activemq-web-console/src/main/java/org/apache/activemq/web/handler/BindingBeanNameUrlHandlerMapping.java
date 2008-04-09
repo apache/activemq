508128e3e64aeb0c09339172aabb8dbd3434980c
James Strachan <jstrachan@apache.org>
2008-04-09 13:29:07 +0000

    updated to the latest/greatest jetty & sl4j along with patching the web console so it works again :)
    git-svn-id: https://svn.apache.org/repos/asf/activemq/trunk@646334 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-web-console/src/main/java/org/apache/activemq/web/handler/BindingBeanNameUrlHandlerMapping.java
+++ activemq-web-console/src/main/java/org/apache/activemq/web/handler/BindingBeanNameUrlHandlerMapping.java
@@ -22,6 +22,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.ServletRequestDataBinder;
 import org.springframework.web.servlet.handler.BeanNameUrlHandlerMapping;
+import org.springframework.web.servlet.HandlerExecutionChain;
 
 /**
  * @version $Revision$
@@ -33,17 +34,28 @@ public class BindingBeanNameUrlHandlerMapping extends BeanNameUrlHandlerMapping 
         Object object = super.getHandlerInternal(request);
 
         if (object instanceof String) {
-            String handlerName = (String)object;
+            String handlerName = (String) object;
             object = getApplicationContext().getBean(handlerName);
         }
+        if (object instanceof HandlerExecutionChain) {
+            HandlerExecutionChain handlerExecutionChain = (HandlerExecutionChain) object;
+            object = handlerExecutionChain.getHandler();
+        }
 
-        ServletRequestDataBinder binder = new ServletRequestDataBinder(object, "request");
-        binder.bind(request);
-        binder.setIgnoreUnknownFields(true);
-        if (LOG.isDebugEnabled()) {
-            LOG.debug("Bound POJO is now: " + object);
+        if (object != null) {
+            ServletRequestDataBinder binder = new ServletRequestDataBinder(object, "request");
+            try {
+                binder.bind(request);
+                binder.setIgnoreUnknownFields(true);
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("Bound POJO is now: " + object);
+                }
+            }
+            catch (Exception e) {
+                LOG.warn("Caught: " + e, e);
+                throw e;
+            }
         }
         return object;
     }
-
 }
