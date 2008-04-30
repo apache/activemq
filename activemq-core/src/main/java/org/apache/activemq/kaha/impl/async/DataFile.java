9341f1644234ef233d6762a91b7a298c361ec619
Robert Davies <rajdavies@apache.org>
2008-04-30 06:36:53 +0000

    Fix for https://issues.apache.org/activemq/browse/AMQ-1701
    git-svn-id: https://svn.apache.org/repos/asf/activemq/trunk@652300 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-core/src/main/java/org/apache/activemq/kaha/impl/async/DataFile.java
+++ activemq-core/src/main/java/org/apache/activemq/kaha/impl/async/DataFile.java
@@ -85,7 +85,7 @@ public class DataFile extends LinkedNode implements Comparable<DataFile> {
         return result;
     }
 
-    public RandomAccessFile openRandomAccessFile(boolean appender) throws IOException {
+    public synchronized RandomAccessFile openRandomAccessFile(boolean appender) throws IOException {
         RandomAccessFile rc = new RandomAccessFile(file, "rw");
         // When we start to write files size them up so that the OS has a chance
         // to allocate the file contigously.
@@ -97,7 +97,7 @@ public class DataFile extends LinkedNode implements Comparable<DataFile> {
         return rc;
     }
 
-    public void closeRandomAccessFile(RandomAccessFile file) throws IOException {
+    public synchronized void closeRandomAccessFile(RandomAccessFile file) throws IOException {
         // On close set the file size to the real size.
         if (length != file.length()) {
             file.setLength(getLength());
