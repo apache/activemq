b3ee59d7a106105ac3aff62a72921f9f740b7999
Hiram R. Chirino <chirino@apache.org>
2008-05-28 21:53:17 +0000

    Reverting recent chagnes to openwire v3 marshallers that snuck in with my last commit
    git-svn-id: https://svn.apache.org/repos/asf/activemq/trunk@661104 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-core/src/main/java/org/apache/activemq/openwire/v3/ConsumerInfoMarshaller.java
+++ activemq-core/src/main/java/org/apache/activemq/openwire/v3/ConsumerInfoMarshaller.java
@@ -1,5 +1,4 @@
 /**
- *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -7,7 +6,7 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -22,32 +21,32 @@ import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
-import org.apache.activemq.openwire.*;
-import org.apache.activemq.command.*;
-
-
+import org.apache.activemq.command.ConsumerInfo;
+import org.apache.activemq.command.DataStructure;
+import org.apache.activemq.openwire.BooleanStream;
+import org.apache.activemq.openwire.OpenWireFormat;
 
 /**
  * Marshalling code for Open Wire Format for ConsumerInfoMarshaller
- *
- *
- * NOTE!: This file is auto generated - do not modify!
- *        if you need to make a change, please see the modify the groovy scripts in the
- *        under src/gram/script and then use maven openwire:generate to regenerate 
- *        this file.
- *
+ * 
+ * 
+ * NOTE!: This file is auto generated - do not modify! if you need to make a
+ * change, please see the modify the groovy scripts in the under src/gram/script
+ * and then use maven openwire:generate to regenerate this file.
+ * 
  * @version $Revision$
  */
 public class ConsumerInfoMarshaller extends BaseCommandMarshaller {
 
     /**
      * Return the type of Data Structure we marshal
+     * 
      * @return short representation of the type data structure
      */
     public byte getDataStructureType() {
         return ConsumerInfo.DATA_STRUCTURE_TYPE;
     }
-    
+
     /**
      * @return a new object instance
      */
@@ -57,18 +56,24 @@ public class ConsumerInfoMarshaller extends BaseCommandMarshaller {
 
     /**
      * Un-marshal an object instance from the data input stream
-     *
+     * 
      * @param o the object to un-marshal
      * @param dataIn the data input stream to build the object from
      * @throws IOException
      */
-    public void tightUnmarshal(OpenWireFormat wireFormat, Object o, DataInput dataIn, BooleanStream bs) throws IOException {
+    public void tightUnmarshal(OpenWireFormat wireFormat, Object o, DataInput dataIn, BooleanStream bs)
+        throws IOException {
         super.tightUnmarshal(wireFormat, o, dataIn, bs);
 
         ConsumerInfo info = (ConsumerInfo)o;
-        info.setConsumerId((org.apache.activemq.command.ConsumerId) tightUnmarsalCachedObject(wireFormat, dataIn, bs));
+        info.setConsumerId((org.apache.activemq.command.ConsumerId)tightUnmarsalCachedObject(wireFormat,
+                                                                                             dataIn, bs));
         info.setBrowser(bs.readBoolean());
-        info.setDestination((org.apache.activemq.command.ActiveMQDestination) tightUnmarsalCachedObject(wireFormat, dataIn, bs));
+        info
+            .setDestination((org.apache.activemq.command.ActiveMQDestination)tightUnmarsalCachedObject(
+                                                                                                       wireFormat,
+                                                                                                       dataIn,
+                                                                                                       bs));
         info.setPrefetchSize(dataIn.readInt());
         info.setMaximumPendingMessageLimit(dataIn.readInt());
         info.setDispatchAsync(bs.readBoolean());
@@ -82,21 +87,24 @@ public class ConsumerInfoMarshaller extends BaseCommandMarshaller {
         if (bs.readBoolean()) {
             short size = dataIn.readShort();
             org.apache.activemq.command.BrokerId value[] = new org.apache.activemq.command.BrokerId[size];
-            for( int i=0; i < size; i++ ) {
-                value[i] = (org.apache.activemq.command.BrokerId) tightUnmarsalNestedObject(wireFormat,dataIn, bs);
+            for (int i = 0; i < size; i++) {
+                value[i] = (org.apache.activemq.command.BrokerId)tightUnmarsalNestedObject(wireFormat,
+                                                                                           dataIn, bs);
             }
             info.setBrokerPath(value);
-        }
-        else {
+        } else {
             info.setBrokerPath(null);
         }
-        info.setAdditionalPredicate((org.apache.activemq.filter.BooleanExpression) tightUnmarsalNestedObject(wireFormat, dataIn, bs));
+        info
+            .setAdditionalPredicate((org.apache.activemq.filter.BooleanExpression)tightUnmarsalNestedObject(
+                                                                                                            wireFormat,
+                                                                                                            dataIn,
+                                                                                                            bs));
         info.setNetworkSubscription(bs.readBoolean());
         info.setOptimizedAcknowledge(bs.readBoolean());
         info.setNoRangeAcks(bs.readBoolean());
 
     }
-
 
     /**
      * Write the booleans that this object uses to a BooleanStream
@@ -126,12 +134,13 @@ public class ConsumerInfoMarshaller extends BaseCommandMarshaller {
 
     /**
      * Write a object instance to data output stream
-     *
+     * 
      * @param o the instance to be marshaled
      * @param dataOut the output stream
      * @throws IOException thrown if an error occurs
      */
-    public void tightMarshal2(OpenWireFormat wireFormat, Object o, DataOutput dataOut, BooleanStream bs) throws IOException {
+    public void tightMarshal2(OpenWireFormat wireFormat, Object o, DataOutput dataOut, BooleanStream bs)
+        throws IOException {
         super.tightMarshal2(wireFormat, o, dataOut, bs);
 
         ConsumerInfo info = (ConsumerInfo)o;
@@ -157,7 +166,7 @@ public class ConsumerInfoMarshaller extends BaseCommandMarshaller {
 
     /**
      * Un-marshal an object instance from the data input stream
-     *
+     * 
      * @param o the object to un-marshal
      * @param dataIn the data input stream to build the object from
      * @throws IOException
@@ -166,9 +175,13 @@ public class ConsumerInfoMarshaller extends BaseCommandMarshaller {
         super.looseUnmarshal(wireFormat, o, dataIn);
 
         ConsumerInfo info = (ConsumerInfo)o;
-        info.setConsumerId((org.apache.activemq.command.ConsumerId) looseUnmarsalCachedObject(wireFormat, dataIn));
+        info.setConsumerId((org.apache.activemq.command.ConsumerId)looseUnmarsalCachedObject(wireFormat,
+                                                                                             dataIn));
         info.setBrowser(dataIn.readBoolean());
-        info.setDestination((org.apache.activemq.command.ActiveMQDestination) looseUnmarsalCachedObject(wireFormat, dataIn));
+        info
+            .setDestination((org.apache.activemq.command.ActiveMQDestination)looseUnmarsalCachedObject(
+                                                                                                       wireFormat,
+                                                                                                       dataIn));
         info.setPrefetchSize(dataIn.readInt());
         info.setMaximumPendingMessageLimit(dataIn.readInt());
         info.setDispatchAsync(dataIn.readBoolean());
@@ -182,21 +195,22 @@ public class ConsumerInfoMarshaller extends BaseCommandMarshaller {
         if (dataIn.readBoolean()) {
             short size = dataIn.readShort();
             org.apache.activemq.command.BrokerId value[] = new org.apache.activemq.command.BrokerId[size];
-            for( int i=0; i < size; i++ ) {
-                value[i] = (org.apache.activemq.command.BrokerId) looseUnmarsalNestedObject(wireFormat,dataIn);
+            for (int i = 0; i < size; i++) {
+                value[i] = (org.apache.activemq.command.BrokerId)looseUnmarsalNestedObject(wireFormat, dataIn);
             }
             info.setBrokerPath(value);
-        }
-        else {
+        } else {
             info.setBrokerPath(null);
         }
-        info.setAdditionalPredicate((org.apache.activemq.filter.BooleanExpression) looseUnmarsalNestedObject(wireFormat, dataIn));
+        info
+            .setAdditionalPredicate((org.apache.activemq.filter.BooleanExpression)looseUnmarsalNestedObject(
+                                                                                                            wireFormat,
+                                                                                                            dataIn));
         info.setNetworkSubscription(dataIn.readBoolean());
         info.setOptimizedAcknowledge(dataIn.readBoolean());
         info.setNoRangeAcks(dataIn.readBoolean());
 
     }
-
 
     /**
      * Write the booleans that this object uses to a BooleanStream
--- activemq-core/src/main/java/org/apache/activemq/openwire/v3/SessionIdMarshaller.java
+++ activemq-core/src/main/java/org/apache/activemq/openwire/v3/SessionIdMarshaller.java
@@ -1,5 +1,4 @@
 /**
- *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -7,7 +6,7 @@
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -22,32 +21,32 @@ import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
-import org.apache.activemq.openwire.*;
-import org.apache.activemq.command.*;
-
-
+import org.apache.activemq.command.DataStructure;
+import org.apache.activemq.command.SessionId;
+import org.apache.activemq.openwire.BooleanStream;
+import org.apache.activemq.openwire.OpenWireFormat;
 
 /**
  * Marshalling code for Open Wire Format for SessionIdMarshaller
- *
- *
- * NOTE!: This file is auto generated - do not modify!
- *        if you need to make a change, please see the modify the groovy scripts in the
- *        under src/gram/script and then use maven openwire:generate to regenerate 
- *        this file.
- *
+ * 
+ * 
+ * NOTE!: This file is auto generated - do not modify! if you need to make a
+ * change, please see the modify the groovy scripts in the under src/gram/script
+ * and then use maven openwire:generate to regenerate this file.
+ * 
  * @version $Revision$
  */
 public class SessionIdMarshaller extends BaseDataStreamMarshaller {
 
     /**
      * Return the type of Data Structure we marshal
+     * 
      * @return short representation of the type data structure
      */
     public byte getDataStructureType() {
         return SessionId.DATA_STRUCTURE_TYPE;
     }
-    
+
     /**
      * @return a new object instance
      */
@@ -57,12 +56,13 @@ public class SessionIdMarshaller extends BaseDataStreamMarshaller {
 
     /**
      * Un-marshal an object instance from the data input stream
-     *
+     * 
      * @param o the object to un-marshal
      * @param dataIn the data input stream to build the object from
      * @throws IOException
      */
-    public void tightUnmarshal(OpenWireFormat wireFormat, Object o, DataInput dataIn, BooleanStream bs) throws IOException {
+    public void tightUnmarshal(OpenWireFormat wireFormat, Object o, DataInput dataIn, BooleanStream bs)
+        throws IOException {
         super.tightUnmarshal(wireFormat, o, dataIn, bs);
 
         SessionId info = (SessionId)o;
@@ -70,7 +70,6 @@ public class SessionIdMarshaller extends BaseDataStreamMarshaller {
         info.setValue(tightUnmarshalLong(wireFormat, dataIn, bs));
 
     }
-
 
     /**
      * Write the booleans that this object uses to a BooleanStream
@@ -81,19 +80,20 @@ public class SessionIdMarshaller extends BaseDataStreamMarshaller {
 
         int rc = super.tightMarshal1(wireFormat, o, bs);
         rc += tightMarshalString1(info.getConnectionId(), bs);
-        rc+=tightMarshalLong1(wireFormat, info.getValue(), bs);
+        rc += tightMarshalLong1(wireFormat, info.getValue(), bs);
 
         return rc + 0;
     }
 
     /**
      * Write a object instance to data output stream
-     *
+     * 
      * @param o the instance to be marshaled
      * @param dataOut the output stream
      * @throws IOException thrown if an error occurs
      */
-    public void tightMarshal2(OpenWireFormat wireFormat, Object o, DataOutput dataOut, BooleanStream bs) throws IOException {
+    public void tightMarshal2(OpenWireFormat wireFormat, Object o, DataOutput dataOut, BooleanStream bs)
+        throws IOException {
         super.tightMarshal2(wireFormat, o, dataOut, bs);
 
         SessionId info = (SessionId)o;
@@ -104,7 +104,7 @@ public class SessionIdMarshaller extends BaseDataStreamMarshaller {
 
     /**
      * Un-marshal an object instance from the data input stream
-     *
+     * 
      * @param o the object to un-marshal
      * @param dataIn the data input stream to build the object from
      * @throws IOException
@@ -117,7 +117,6 @@ public class SessionIdMarshaller extends BaseDataStreamMarshaller {
         info.setValue(looseUnmarshalLong(wireFormat, dataIn));
 
     }
-
 
     /**
      * Write the booleans that this object uses to a BooleanStream
