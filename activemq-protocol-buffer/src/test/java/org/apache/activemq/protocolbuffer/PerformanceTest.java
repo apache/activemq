f9b668b05ba1be525804ae043ff3eacc4ab0435e
James Strachan <jstrachan@apache.org>
2008-07-08 14:15:35 +0000

    added initial spike of using Protocol Buffer as the wire format for https://issues.apache.org/activemq/browse/AMQ-1843
    git-svn-id: https://svn.apache.org/repos/asf/activemq/trunk@674837 13f79535-47bb-0310-9956-ffa450edef68

---
+++ activemq-protocol-buffer/src/test/java/org/apache/activemq/protocolbuffer/PerformanceTest.java
@@ -0,0 +1,71 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.activemq.protocolbuffer;
+
+import com.google.protobuf.CodedInputStream;
+import com.google.protobuf.CodedOutputStream;
+import junit.framework.TestCase;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+/**
+ * @version $Revision: 1.1 $
+ */
+public class PerformanceTest extends TestCase {
+    protected int messageCount = 10;
+    protected String fileName = "target/messages.openwire";
+
+
+    public void testPerformance() throws Exception {
+        BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(fileName));
+        CodedOutputStream cout = CodedOutputStream.newInstance(out);
+        OpenWire.Destination destination = OpenWire.Destination.newBuilder().setName("FOO.BAR").setType(OpenWire.Destination.DestinationType.QUEUE).build();
+
+        for (int i = 0; i < messageCount; i++) {
+            OpenWire.Message message = OpenWire.Message.newBuilder()
+                    .setDestination(destination)
+                    .setPersistent(true)
+                    .setProducerId(1234)
+                    .setProducerCounter(i)
+                    .setType("type:" + i)
+                    .build();
+
+            System.out.println("Writing message: " + i + " = " + message);
+            message.writeTo(cout);
+            //cout.flush();
+        }
+        cout.flush();
+        out.close();
+
+        // now lets try read them!
+        BufferedInputStream in = new BufferedInputStream(new FileInputStream(fileName));
+        CodedInputStream cin = CodedInputStream.newInstance(in);
+        for (int i = 0; i < messageCount; i++) {
+            OpenWire.Message message = OpenWire.Message.parseFrom(cin);
+            // todo test its not null or something?
+
+            System.out.println("Reading message: " + i + " = " + message);
+
+        }
+        in.close();
+    }
+
+}
