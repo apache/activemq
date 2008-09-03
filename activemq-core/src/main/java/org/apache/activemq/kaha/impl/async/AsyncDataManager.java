b26be0b3a6bcd8f57f3c3177037d297dd1687273
Gary Tully <gtully@apache.org>
2008-09-03 16:28:47 +0000

    adendum to https://issues.apache.org/activemq/browse/AMQ-1926
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/trunk@691665 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-core/src/main/java/org/apache/activemq/kaha/impl/async/AsyncDataManager.java
+++ activemq-core/src/main/java/org/apache/activemq/kaha/impl/async/AsyncDataManager.java
@@ -468,6 +468,7 @@ public class AsyncDataManager {
             throws IOException {
         accessorPool.disposeDataFileAccessors(dataFile);
         fileByFileMap.remove(dataFile.getFile());
+        fileMap.remove(dataFile.getFile());
         storeSize.addAndGet(-dataFile.getLength());
         dataFile.unlink();
         if (archiveDataLogs) {
