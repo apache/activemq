00bc8edd31c71ad0caf46711e990dd496715ef18
Bosanac Dejan <dejanb@apache.org>
2009-02-13 11:29:01 +0000

    fix for https://issues.apache.org/activemq/browse/AMQ-2096
    git-svn-id: https://svn.eu.apache.org/repos/asf/activemq/trunk@744082 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-core/src/main/java/org/apache/activemq/advisory/AdvisoryBroker.java
+++ activemq-core/src/main/java/org/apache/activemq/advisory/AdvisoryBroker.java
@@ -20,6 +20,7 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+
 import org.apache.activemq.broker.Broker;
 import org.apache.activemq.broker.BrokerFilter;
 import org.apache.activemq.broker.ConnectionContext;
@@ -40,6 +41,7 @@ import org.apache.activemq.command.Message;
 import org.apache.activemq.command.MessageId;
 import org.apache.activemq.command.ProducerId;
 import org.apache.activemq.command.ProducerInfo;
+import org.apache.activemq.security.SecurityContext;
 import org.apache.activemq.state.ProducerState;
 import org.apache.activemq.usage.Usage;
 import org.apache.activemq.util.IdGenerator;
@@ -346,6 +348,7 @@ public class AdvisoryBroker extends BrokerFilter {
             ActiveMQTopic topic = AdvisorySupport.getMasterBrokerAdvisoryTopic();
             ActiveMQMessage advisoryMessage = new ActiveMQMessage();                       
             ConnectionContext context = new ConnectionContext();
+            context.setSecurityContext(SecurityContext.BROKER_SECURITY_CONTEXT);
             context.setBroker(getBrokerService().getBroker());
             fireAdvisory(context, topic,null,null,advisoryMessage);
         } catch (Exception e) {
