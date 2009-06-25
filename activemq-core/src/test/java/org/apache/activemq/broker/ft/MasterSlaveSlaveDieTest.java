a0b9aec6e95c566c22e04e8874f9729ec4490819
Gary Tully <gtully@apache.org>
2009-06-25 16:57:14 +0000

    test case for https://issues.apache.org/activemq/browse/AMQ-2305 - seems to work fine
    git-svn-id: https://svn.apache.org/repos/asf/activemq/trunk@788429 13f79535-47bb-0310-9956-ffa450edef68

---
+++ activemq-core/src/test/java/org/apache/activemq/broker/ft/MasterSlaveSlaveDieTest.java
@@ -0,0 +1,70 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.activemq.broker.ft;
+
+import java.net.URI;
+import java.util.concurrent.Executors;
+
+import junit.framework.TestCase;
+
+import org.apache.activemq.broker.BrokerService;
+import org.apache.activemq.util.SocketProxy;
+
+public class MasterSlaveSlaveDieTest extends TestCase {
+
+    public void testSlaveDieMasterStays() throws Exception {
+        final BrokerService master = new BrokerService();
+        master.setBrokerName("master");
+        master.setPersistent(false);
+        master.addConnector("tcp://localhost:0");
+        master.setWaitForSlave(true);
+
+        final BrokerService slave = new BrokerService();
+        slave.setBrokerName("slave");
+        slave.setPersistent(false);
+        URI masterUri = master.getTransportConnectors().get(0).getConnectUri();
+        SocketProxy masterProxy = new SocketProxy(masterUri);
+        slave.setMasterConnectorURI(masterProxy.getUrl().toString());
+        
+        slave.setUseJmx(false);
+        slave.getManagementContext().setCreateConnector(false);
+
+        Executors.newSingleThreadExecutor().execute(new Runnable() {
+            public void run() {
+                try {
+                    master.start();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        slave.start();
+        slave.waitUntilStarted();
+        
+        master.waitUntilStarted();
+        
+        // kill socket to master
+        masterProxy.close();
+        
+        // in case a stop is too controlled an exit
+        //slave.stop();
+        Thread.sleep(5000);
+
+        assertTrue(master.isStarted());
+
+    }
+}
