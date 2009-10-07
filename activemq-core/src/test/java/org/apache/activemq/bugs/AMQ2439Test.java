79b4d44c927ef61afe252a08275339a24d6ea6ab
Gary Tully <gtully@apache.org>
2009-10-07 20:59:52 +0000

    apply fix for AMQ-2439 to 5.3 branch
    git-svn-id: https://svn.apache.org/repos/asf/activemq/branches/activemq-5.3@822911 13f79535-47bb-0310-9956-ffa450edef68

---
+++ activemq-core/src/test/java/org/apache/activemq/bugs/AMQ2439Test.java
@@ -0,0 +1,82 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.activemq.bugs;
+
+import java.net.URI;
+
+import javax.jms.Connection;
+import javax.jms.Destination;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+
+import org.apache.activemq.JmsMultipleBrokersTestSupport;
+import org.apache.activemq.broker.jmx.BrokerView;
+
+public class AMQ2439Test extends JmsMultipleBrokersTestSupport {
+    Destination dest;
+
+    
+    public void testDuplicatesThroughNetwork() throws Exception {
+        assertEquals("received expected amount", 500, receiveExactMessages("BrokerB", 500));
+        assertEquals("received expected amount", 500, receiveExactMessages("BrokerB", 500));
+        validateQueueStats();
+    }
+    
+    private void validateQueueStats() throws Exception {
+       BrokerView brokerView = brokers.get("BrokerA").broker.getAdminView();
+       assertEquals("enequeue is correct", 1000, brokerView.getTotalEnqueueCount());
+       assertEquals("dequeue is correct", 1000, brokerView.getTotalDequeueCount());
+    }
+
+    protected int receiveExactMessages(String brokerName, int msgCount) throws Exception {
+        
+        BrokerItem brokerItem = brokers.get(brokerName);
+        Connection connection = brokerItem.createConnection();
+        connection.start();
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);       
+        MessageConsumer consumer = session.createConsumer(dest);
+        
+        Message msg;
+        int i;
+        for (i = 0; i < msgCount; i++) {
+            msg = consumer.receive(1000);
+            if (msg == null) {
+                break;
+            }
+        }
+
+        connection.close();
+        brokerItem.connections.remove(connection);
+        
+        return i;
+    }
+    
+    public void setUp() throws Exception {
+        super.setUp();
+        createBroker(new URI("broker:(tcp://localhost:61616)/BrokerA?persistent=true&deleteAllMessagesOnStartup=true&advisorySupport=false"));
+        createBroker(new URI("broker:(tcp://localhost:61617)/BrokerB?persistent=true&deleteAllMessagesOnStartup=true&useJmx=false"));
+        bridgeBrokers("BrokerA", "BrokerB");
+        
+        startAllBrokers();
+        
+        // Create queue
+        dest = createDestination("TEST.FOO", false);
+        sendMessages("BrokerA", dest, 1000);
+    }   
+}
