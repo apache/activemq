9086693c6135e28de17b456e31231b351c7cccc3
Gary Tully <gtully@apache.org>
2009-11-04 18:15:57 +0000

    add some resilience to slow mbean registration
    git-svn-id: https://svn.apache.org/repos/asf/activemq/trunk@832829 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-core/src/test/java/org/apache/activemq/network/DuplexNetworkMBeanTest.java
+++ activemq-core/src/test/java/org/apache/activemq/network/DuplexNetworkMBeanTest.java
@@ -57,7 +57,7 @@ public class DuplexNetworkMBeanTest extends TestCase {
     public void testMbeanPresenceOnNetworkBrokerRestart() throws Exception {
         BrokerService broker = createBroker();
         broker.start();
-        assertEquals(1, countMbeans(broker, "Connector", 10000));
+        assertEquals(1, countMbeans(broker, "Connector", 30000));
         assertEquals(0, countMbeans(broker, "Connection"));
         BrokerService networkedBroker = null;
         for (int i=0; i<numRestarts; i++) {       
@@ -82,7 +82,7 @@ public class DuplexNetworkMBeanTest extends TestCase {
         
         BrokerService networkedBroker = createNetworkedBroker();
         networkedBroker.start();
-        assertEquals(1, countMbeans(networkedBroker, "Connector", 10000));
+        assertEquals(1, countMbeans(networkedBroker, "Connector", 30000));
         assertEquals(0, countMbeans(networkedBroker, "Connection"));
         
         BrokerService broker = null;
@@ -147,6 +147,7 @@ public class DuplexNetworkMBeanTest extends TestCase {
                 LOG.info(bean.getObjectName());
             }
         } catch (Exception ignored) {
+            LOG.warn("getMBeanServer ex: " + ignored);
         }
         return mbsc;
     }
