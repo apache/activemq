1e8acfadb2b6a2866448e85e45a0b35c141d2c6d
Timothy A. Bish <tabish@apache.org>
2010-02-04 00:30:16 +0000

    https://issues.apache.org/activemq/browse/AMQ-2490
    Increase the receive timeout to fix errors on slow/loaded machines.
    
    git-svn-id: https://svn.apache.org/repos/asf/activemq/trunk@906301 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-core/src/test/java/org/apache/activemq/transport/stomp/StompTest.java
+++ activemq-core/src/test/java/org/apache/activemq/transport/stomp/StompTest.java
@@ -1115,7 +1115,7 @@ public class StompTest extends CombinationTestSupport {
 
         stompConnection.sendFrame(frame);
 
-        TextMessage message = (TextMessage)consumer.receive(1000);
+        TextMessage message = (TextMessage)consumer.receive(5000);
         assertNotNull(message);
         assertEquals("system", message.getStringProperty(Stomp.Headers.Message.USERID));
 
@@ -1135,7 +1135,7 @@ public class StompTest extends CombinationTestSupport {
         frame = "SEND\n" + "destination:/queue/" + getQueueName() + "\n\n" + "Hello World" + Stomp.NULL;
         stompConnection.sendFrame(frame);
 
-        StompFrame message = stompConnection.receive(1000);
+        StompFrame message = stompConnection.receive(5000);
         assertEquals("system", message.getHeaders().get(Stomp.Headers.Message.USERID));
     }
 
