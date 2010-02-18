7dde293ea5dba3d1630192bdd71242f7f9feba0c
Gary Tully <gtully@apache.org>
2010-02-18 18:14:57 +0000

    tidy up merge 908453, revert for this file to fix compliation error, reference to new method committed in error
    git-svn-id: https://svn.apache.org/repos/asf/activemq/branches/activemq-5.3@911505 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-core/src/main/java/org/apache/activemq/transport/failover/FailoverTransport.java
+++ activemq-core/src/main/java/org/apache/activemq/transport/failover/FailoverTransport.java
@@ -212,9 +212,7 @@ public class FailoverTransport implements CompositeTransport {
                 failedConnectTransportURI=connectedTransportURI;
                 connectedTransportURI = null;
                 connected=false;
-
-                stateTracker.transportInterrupted();
-
+            
                 // notify before any reconnect attempt so ack state can be whacked
                 if (transportListener != null) {
                     transportListener.transportInterupted();
