249adf75b6d9f30f32d37b412e18adb7cc199b87
Gary Tully <gtully@apache.org>
2010-03-04 12:31:01 +0000

    undo additional tests committed in error with r918386
    git-svn-id: https://svn.apache.org/repos/asf/activemq/branches/activemq-5.3@918966 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-core/src/main/java/org/apache/activemq/ActiveMQMessageConsumer.java
+++ activemq-core/src/main/java/org/apache/activemq/ActiveMQMessageConsumer.java
@@ -1207,7 +1207,6 @@ public class ActiveMQMessageConsumer implements MessageAvailableConsumer, StatsC
                             } catch (RuntimeException e) {
                                 if (isAutoAcknowledgeBatch() || isAutoAcknowledgeEach() || session.isIndividualAcknowledge()) {
                                     // Redeliver the message
-                                    unconsumedMessages.enqueueFirst(md);
                                 } else {
                                     // Transacted or Client ack: Deliver the
                                     // next message.
