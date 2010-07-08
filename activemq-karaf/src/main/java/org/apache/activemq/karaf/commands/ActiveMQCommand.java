d1fa2baf404d0dbad19c8daf5e67d6d91178b301
Bosanac Dejan <dejanb@apache.org>
2010-07-08 11:11:22 +0000

    https://issues.apache.org/activemq/browse/AMQ-2815 - first stab at moving karaf commands to activemq
    git-svn-id: https://svn.apache.org/repos/asf/activemq/trunk@961712 13f79535-47bb-0310-9956-ffa450edef68

---
+++ activemq-karaf/src/main/java/org/apache/activemq/karaf/commands/ActiveMQCommand.java
@@ -0,0 +1,158 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.activemq.karaf.commands;
+
+import org.apache.felix.gogo.commands.Action;
+import org.apache.felix.gogo.commands.Argument;
+import org.apache.felix.gogo.commands.basic.AbstractCommand;
+import org.apache.felix.gogo.commands.basic.ActionPreparator;
+import org.apache.felix.gogo.commands.basic.DefaultActionPreparator;
+import org.apache.karaf.shell.console.BlueprintContainerAware;
+import org.apache.karaf.shell.console.BundleContextAware;
+import org.apache.karaf.shell.console.CompletableFunction;
+import org.apache.karaf.shell.console.Completer;
+import org.apache.karaf.shell.console.commands.GenericType;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.blueprint.container.BlueprintContainer;
+import org.osgi.service.blueprint.container.Converter;
+import org.osgi.service.command.CommandSession;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Base command to process options and wrap native ActiveMQ console commands.
+ */
+public class ActiveMQCommand extends AbstractCommand implements CompletableFunction
+{
+    protected BlueprintContainer blueprintContainer;
+    protected Converter blueprintConverter;
+    protected String actionId;
+    protected List<Completer> completers;
+
+    public void setBlueprintContainer(BlueprintContainer blueprintContainer) {
+        this.blueprintContainer = blueprintContainer;
+    }
+
+    public void setBlueprintConverter(Converter blueprintConverter) {
+        this.blueprintConverter = blueprintConverter;
+    }
+
+    public void setActionId(String actionId) {
+        this.actionId = actionId;
+    }
+
+    public List<Completer> getCompleters() {
+        return completers;
+    }
+
+    public void setCompleters(List<Completer> completers) {
+        this.completers = completers;
+    }
+
+    @Override
+    protected ActionPreparator getPreparator() throws Exception {
+        return new ActiveMQActionPreparator();
+    }
+
+    class ActiveMQActionPreparator extends DefaultActionPreparator {
+        @Override
+        public boolean prepare(Action action, CommandSession session, List<Object> params) throws Exception
+        {
+            Map<Argument, Field> arguments = new HashMap<Argument, Field>();
+            List<Argument> orderedArguments = new ArrayList<Argument>();
+            // Introspect
+            for (Class type = action.getClass(); type != null; type = type.getSuperclass()) {
+                for (Field field : type.getDeclaredFields()) {
+                    Argument argument = field.getAnnotation(Argument.class);
+                    if (argument != null) {
+                        arguments.put(argument, field);
+                        int index = argument.index();
+                        while (orderedArguments.size() <= index) {
+                            orderedArguments.add(null);
+                        }
+                        if (orderedArguments.get(index) != null) {
+                            throw new IllegalArgumentException("Duplicate argument index: " + index);
+                        }
+                        orderedArguments.set(index, argument);
+                    }
+                }
+            }
+            // Check indexes are correct
+            for (int i = 0; i < orderedArguments.size(); i++) {
+                if (orderedArguments.get(i) == null) {
+                    throw new IllegalArgumentException("Missing argument for index: " + i);
+                }
+            }
+            // Populate
+            Map<Argument, Object> argumentValues = new HashMap<Argument, Object>();
+            int argIndex = 0;
+            for (Iterator<Object> it = params.iterator(); it.hasNext();) {
+                Object param = it.next();
+                if (argIndex >= orderedArguments.size()) {
+                    throw new IllegalArgumentException("Too many arguments specified");
+                }
+                Argument argument = orderedArguments.get(argIndex);
+                if (!argument.multiValued()) {
+                    argIndex++;
+                }
+                if (argument.multiValued()) {
+                    List<Object> l = (List<Object>) argumentValues.get(argument);
+                    if (l == null) {
+                        l = new ArrayList<Object>();
+                        argumentValues.put(argument, l);
+                    }
+                    l.add(param);
+                } else {
+                    argumentValues.put(argument, param);
+                }
+            }
+
+            for (Map.Entry<Argument, Object> entry : argumentValues.entrySet()) {
+                Field field = arguments.get(entry.getKey());
+                Object value = convert(action, session, entry.getValue(), field.getGenericType());
+                field.setAccessible(true);
+                field.set(action, value);
+            }
+            return true;
+        }
+
+        @Override
+        protected Object convert(Action action, CommandSession commandSession, Object o, Type type) throws Exception {
+            return blueprintConverter.convert(o, new GenericType(type));
+        }
+    }
+
+    protected Action createNewAction() throws Exception {
+        Action action = (Action) blueprintContainer.getComponentInstance(actionId);
+        if (action instanceof BlueprintContainerAware) {
+            ((BlueprintContainerAware) action).setBlueprintContainer(blueprintContainer);
+        }
+        if (action instanceof BundleContextAware) {
+            BundleContext context = (BundleContext) blueprintContainer.getComponentInstance("blueprintBundleContext");
+            ((BundleContextAware) action).setBundleContext(context);
+        }
+        return action;
+    }
+
+}
---
+++ activemq-karaf/src/main/java/org/apache/activemq/karaf/commands/CreateBrokerCommand.java
@@ -0,0 +1,152 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.activemq.karaf.commands;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+import org.apache.felix.gogo.commands.Option;
+import org.apache.felix.gogo.commands.Command;
+import org.apache.karaf.shell.console.OsgiCommandSupport;
+
+/**
+ * @version $Rev: 960482 $ $Date: 2010-07-05 10:28:33 +0200 (Mon, 05 Jul 2010) $
+ */
+ @Command(scope="activemq", name="create-broker", description="Creates a broker instance.")
+public class CreateBrokerCommand extends OsgiCommandSupport {
+    
+    @Option(name = "-n", aliases = {"--name"}, description = "The name of the broker (defaults to localhost).")
+    private String name = "localhost";
+
+    /*
+     * (non-Javadoc)
+     * @see
+     * org.apache.karaf.shell.console.OsgiCommandSupport#doExecute()
+     */
+    protected Object doExecute() throws Exception {
+
+        try {
+            String name = getName();
+            File base = new File(System.getProperty("karaf.base"));
+            File deploy = new File(base, "deploy");
+
+            HashMap<String, String> props = new HashMap<String, String>();
+            props.put("${name}", name);
+
+            mkdir(deploy);
+            File configFile = new File(deploy, name + "-broker.xml");
+            copyFilteredResourceTo(configFile, "broker.xml", props);
+
+            System.out.println("");
+            System.out.println("Default ActiveMQ Broker (" + name + ") configuration file created at: "
+                           + configFile.getPath());
+            System.out.println("Please review the configuration and modify to suite your needs.  ");
+            System.out.println("");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+
+        return 0;
+    }
+
+    private void copyFilteredResourceTo(File outFile, String resource, HashMap<String, String> props)
+        throws Exception {
+        if (!outFile.exists()) {
+            System.out.println("Creating file: @|green " + outFile.getPath() + "|");
+            InputStream is = CreateBrokerCommand.class.getResourceAsStream(resource);
+            try {
+                // Read it line at a time so that we can use the platform line
+                // ending when we write it out.
+                PrintStream out = new PrintStream(new FileOutputStream(outFile));
+                try {
+                    Scanner scanner = new Scanner(is);
+                    while (scanner.hasNextLine()) {
+                        String line = scanner.nextLine();
+                        line = filter(line, props);
+                        out.println(line);
+                    }
+                } finally {
+                    safeClose(out);
+                }
+            } finally {
+                safeClose(is);
+            }
+        } else {
+            System.out.println("@|red File already exists|. Move it out of the way if you want it re-created: "
+                           + outFile.getPath() + "");
+        }
+    }
+
+    private void safeClose(InputStream is) throws IOException {
+        if (is == null)
+            return;
+        try {
+            is.close();
+        } catch (Throwable ignore) {
+        }
+    }
+
+    private void safeClose(OutputStream is) throws IOException {
+        if (is == null)
+            return;
+        try {
+            is.close();
+        } catch (Throwable ignore) {
+        }
+    }
+
+    private String filter(String line, HashMap<String, String> props) {
+        for (Map.Entry<String, String> i : props.entrySet()) {
+            int p1;
+            while ((p1 = line.indexOf(i.getKey())) >= 0) {
+                String l1 = line.substring(0, p1);
+                String l2 = line.substring(p1 + i.getKey().length());
+                line = l1 + i.getValue() + l2;
+            }
+        }
+        return line;
+    }
+
+    private void mkdir(File file) {
+        if (!file.exists()) {
+            System.out.println("Creating missing directory: @|green " + file.getPath() + "|");
+            file.mkdirs();
+        }
+    }
+
+    public String getName() {
+        if (name == null) {
+            File base = new File(System.getProperty("karaf.base"));
+            name = base.getName();
+        }
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
