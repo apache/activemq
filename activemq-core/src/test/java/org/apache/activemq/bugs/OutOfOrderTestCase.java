261ccabf6592e36a0d0b2f9d3bb36334c9c03ba8
Bosanac Dejan <dejanb@apache.org>
2010-08-10 14:24:26 +0000

    adding license headers
    git-svn-id: https://svn.apache.org/repos/asf/activemq/trunk@984049 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-core/src/test/java/org/apache/activemq/bugs/OutOfOrderTestCase.java
+++ activemq-core/src/test/java/org/apache/activemq/bugs/OutOfOrderTestCase.java
@@ -1,6 +1,21 @@
-package org.apache.activemq.bugs;
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import java.util.Date;
+package org.apache.activemq.bugs;
 
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
@@ -16,7 +31,6 @@ import junit.framework.TestCase;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.broker.BrokerService;
-import org.apache.activemq.bugs.AMQ1866.ConsumerThread;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
