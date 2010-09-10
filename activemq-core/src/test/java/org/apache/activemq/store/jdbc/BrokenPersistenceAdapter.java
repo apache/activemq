0cc5fd3652a9b59f5fafc46b25ebaddf86441435
Gary Tully <gtully@apache.org>
2010-09-10 13:20:27 +0000

    add missing class for:  https://issues.apache.org/activemq/browse/AMQ-2880
    git-svn-id: https://svn.apache.org/repos/asf/activemq/trunk@995786 13f79535-47bb-0310-9956-ffa450edef68

---
+++ activemq-core/src/test/java/org/apache/activemq/store/jdbc/BrokenPersistenceAdapter.java
@@ -0,0 +1,47 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.activemq.store.jdbc;
+
+import java.io.IOException;
+
+import org.apache.activemq.broker.ConnectionContext;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+class BrokenPersistenceAdapter extends JDBCPersistenceAdapter {
+
+    private final Log LOG = LogFactory.getLog(BrokenPersistenceAdapter.class);
+
+    private boolean shouldBreak = false;
+
+    @Override
+    public void commitTransaction(ConnectionContext context) throws IOException {
+        if ( shouldBreak ) {
+            LOG.warn("Throwing exception on purpose");
+            throw new IOException("Breaking on purpose");
+        }
+        LOG.debug("in commitTransaction");
+        super.commitTransaction(context);
+    }
+
+    public void setShouldBreak(boolean shouldBreak) {
+        this.shouldBreak = shouldBreak;
+    }
+}
+
