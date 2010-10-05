72b857230358580616a7dc118bf5b4639e387dcc
Bosanac Dejan <dejanb@apache.org>
2010-10-05 11:38:22 +0000

    spring message listener container tests
    git-svn-id: https://svn.apache.org/repos/asf/activemq/trunk@1004616 13f79535-47bb-0310-9956-ffa450edef68

---
+++ activemq-spring/src/test/java/org/apache/activemq/spring/Listener.java
@@ -0,0 +1,47 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.activemq.spring;
+
+import org.apache.activemq.command.ActiveMQTextMessage;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Listener implements MessageListener {
+
+	List<Message> messages = new ArrayList<Message>();
+	long lastReceived = 0L;
+
+
+	public void onMessage(Message message)  {
+
+        try {
+            System.out.println("LISTENER received " + message.getJMSDestination() + " " + ((ActiveMQTextMessage)message).getText());
+            lastReceived = System.currentTimeMillis();
+            synchronized (messages) {
+                messages.add(message);
+            }
+        } catch (JMSException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+    }
+
+}
