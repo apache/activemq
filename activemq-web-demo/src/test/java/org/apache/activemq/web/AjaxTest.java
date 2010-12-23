de66445d582033420ef873d1013b70f27e481909
Bosanac Dejan <dejanb@apache.org>
2010-12-23 13:18:20 +0000

    https://issues.apache.org/jira/browse/AMQ-3094 - fixing test license and imports
    git-svn-id: https://svn.apache.org/repos/asf/activemq/trunk@1052261 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-web-demo/src/test/java/org/apache/activemq/web/AjaxTest.java
+++ activemq-web-demo/src/test/java/org/apache/activemq/web/AjaxTest.java
@@ -1,19 +1,31 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.activemq.web;
 
 
-import java.util.Enumeration;
-import javax.jms.TextMessage;
-import javax.jms.MessageProducer;
-import javax.management.ObjectName;
-
-import org.apache.activemq.broker.jmx.DestinationViewMBean;
-import org.apache.activemq.broker.jmx.SubscriptionViewMBean;
-import org.apache.commons.lang.RandomStringUtils;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.commons.httpclient.*;
-import org.apache.commons.httpclient.methods.*;
-import java.util.Set;
+
+import javax.jms.MessageProducer;
+import javax.jms.TextMessage;
 
 public class AjaxTest extends JettyTestSupport {
     private static final Log LOG = LogFactory.getLog(AjaxTest.class);
