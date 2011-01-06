86565eb0e19af8ead04a0fa3a3ae7e99d5eb8fe0
Bosanac Dejan <dejanb@apache.org>
2011-01-06 13:44:31 +0000

    https://issues.apache.org/jira/browse/AMQ-3119 - proxy connector and failover
    git-svn-id: https://svn.apache.org/repos/asf/activemq/trunk@1055873 13f79535-47bb-0310-9956-ffa450edef68

---
+++ activemq-core/src/test/java/org/apache/activemq/util/ConsumerThread.java
@@ -0,0 +1,69 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.activemq.util;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.jms.*;
+
+public class ConsumerThread extends Thread {
+
+    private static final Log LOG = LogFactory.getLog(ConsumerThread.class);
+
+    int messageCount = 1000;
+    int received = 0;
+    Destination dest;
+    Session sess;
+
+    public ConsumerThread(Session sess, Destination dest) {
+        this.dest = dest;
+        this.sess = sess;
+    }
+
+    @Override
+    public void run() {
+      MessageConsumer consumer = null;
+
+        try {
+            consumer = sess.createConsumer(dest);
+            while (received < messageCount) {
+                Message msg = consumer.receive(3000);
+                if (msg != null) {
+                    LOG.info("Received " + ((TextMessage)msg).getText());
+                    received++;
+                } else {
+                    break;
+                }
+            }
+        } catch (JMSException e) {
+            e.printStackTrace();
+        } finally {
+            if (consumer != null) {
+                try {
+                    consumer.close();
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public int getReceived() {
+        return received;
+    }
+}
---
+++ activemq-core/src/test/java/org/apache/activemq/util/ProducerThread.java
@@ -0,0 +1,73 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.activemq.util;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+
+public class ProducerThread extends Thread {
+
+    private static final Log LOG = LogFactory.getLog(ProducerThread.class);
+
+    int messageCount = 1000;
+    Destination dest;
+    Session sess;
+    int sleep = 0;
+
+    public ProducerThread(Session sess, Destination dest) {
+        this.dest = dest;
+        this.sess = sess;
+    }
+
+    public void run() {
+        MessageProducer producer = null;
+        try {
+            producer = sess.createProducer(dest);
+            for (int i = 0; i < messageCount; i++) {
+                producer.send(sess.createTextMessage("test message: " + i));
+                LOG.info("Sent 'test message: " + i + "'");
+                if (sleep > 0) {
+                    Thread.sleep(sleep);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (producer != null) {
+                try {
+                    producer.close();
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+    public void setMessageCount(int messageCount) {
+        this.messageCount = messageCount;
+    }
+
+    public void setSleep(int sleep) {
+        this.sleep = sleep;
+    }
+}
