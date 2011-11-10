77f9b872944543fda7c7805fad6a1140aa95f40d
Timothy A. Bish <tabish@apache.org>
2011-11-10 19:28:59 +0000

    Add missing license header and a tearDown method to ensure the created BrokerService gets stopped.
    git-svn-id: https://svn.apache.org/repos/asf/activemq/trunk@1200489 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-pool/src/test/java/org/apache/activemq/pool/PooledSessionExhaustionTest.java
+++ activemq-pool/src/test/java/org/apache/activemq/pool/PooledSessionExhaustionTest.java
@@ -1,3 +1,19 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.activemq.pool;
 
 import org.apache.activemq.*;
@@ -22,6 +38,7 @@ public class PooledSessionExhaustionTest extends TestCase {
     private String connectionUri;
     private int numReceived = 0;
 
+    @Override
     protected void setUp() throws Exception {
         broker = new BrokerService();
         broker.setPersistent(false);
@@ -32,6 +49,13 @@ public class PooledSessionExhaustionTest extends TestCase {
         pooledFactory = new PooledConnectionFactory(factory);
         pooledFactory.setMaxConnections(1);
         pooledFactory.setBlockIfSessionPoolIsFull(false);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        broker.stop();
+        broker.waitUntilStopped();
+        broker = null;
     }
 
     public void sendMessages(ConnectionFactory connectionFactory) throws Exception {
