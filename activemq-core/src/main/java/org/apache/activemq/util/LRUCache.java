d95658f4b444af178b112e7aa8e1d4aacdcae8d0
Hiram R. Chirino <chirino@apache.org>
2012-01-04 15:12:58 +0000

    Allow LRUCache subclasses to trap eviction events
    git-svn-id: https://svn.apache.org/repos/asf/activemq/trunk@1227183 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-core/src/main/java/org/apache/activemq/util/LRUCache.java
+++ activemq-core/src/main/java/org/apache/activemq/util/LRUCache.java
@@ -80,6 +80,14 @@ public class LRUCache<K, V> extends LinkedHashMap<K, V> {
     }
 
     protected boolean removeEldestEntry(Map.Entry<K,V> eldest) {
-        return size() > maxCacheSize;
+        if( size() > maxCacheSize ) {
+            onCacheEviction(eldest);
+            return true;
+        }
+        return false;
     }
+
+    protected void onCacheEviction(Map.Entry<K,V> eldest) {
+    }
+
 }
