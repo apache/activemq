c4c9cf2e9497de13f37270e7f21250448a750427
Timothy A. Bish <tabish@apache.org>
2012-01-26 22:18:59 +0000

    fix for: https://issues.apache.org/jira/browse/AMQ-3683
    git-svn-id: https://svn.apache.org/repos/asf/activemq/trunk@1236414 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-core/src/main/java/org/apache/activemq/transport/AbstractInactivityMonitor.java
+++ activemq-core/src/main/java/org/apache/activemq/transport/AbstractInactivityMonitor.java
@@ -135,7 +135,7 @@ public abstract class AbstractInactivityMonitor extends TransportFilter {
             return;
         }
 
-        if (!commandSent.get() && useKeepAlive) {
+        if (!commandSent.get() && useKeepAlive && monitorStarted.get() && !ASYNC_TASKS.isTerminating()) {
             if (LOG.isTraceEnabled()) {
                 LOG.trace(this + " no message sent since last write check, sending a KeepAliveInfo");
             }
@@ -178,7 +178,7 @@ public abstract class AbstractInactivityMonitor extends TransportFilter {
             }
             return;
         }
-        if (!commandReceived.get()) {
+        if (!commandReceived.get() && monitorStarted.get() && !ASYNC_TASKS.isTerminating()) {
             if (LOG.isDebugEnabled()) {
                 LOG.debug("No message received since last read check for " + toString() + "! Throwing InactivityIOException.");
             }
