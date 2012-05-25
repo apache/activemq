afcbc4e5a38bb7c978147785ebed88d8c9d5a3a6
Timothy A. Bish <tabish@apache.org>
2012-05-25 21:07:52 +0000

    Add tests for Websockets using selenium
    git-svn-id: https://svn.apache.org/repos/asf/activemq/trunk@1342813 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-optional/src/test/java/org/apache/activemq/transport/ws/WSTransportTest.java
+++ activemq-optional/src/test/java/org/apache/activemq/transport/ws/WSTransportTest.java
@@ -17,29 +17,190 @@
 
 package org.apache.activemq.transport.ws;
 
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.Socket;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.UnknownHostException;
 
 import org.apache.activemq.broker.BrokerFactory;
 import org.apache.activemq.broker.BrokerService;
+import org.apache.activemq.transport.stomp.StompConnection;
+import org.apache.activemq.util.Wait;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
-
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class WSTransportTest {
-    
-    protected String getBindLocation() {
-        return "ws://localhost:61614";
+
+    private static final Logger LOG = LoggerFactory.getLogger(WSTransportTest.class);
+    private static final int MESSAGE_COUNT = 1000;
+
+    private BrokerService broker;
+    private WebDriver driver;
+    private File profileDir;
+
+    private String stompUri;
+    private String wsUri;
+
+    private StompConnection stompConnection = new StompConnection();
+
+    protected BrokerService createBroker(boolean deleteMessages) throws Exception {
+        BrokerService broker = BrokerFactory.createBroker(
+                new URI("broker:()/localhost?persistent=false&useJmx=false"));
+
+        stompUri = broker.addConnector("stomp://localhost:0").getPublishableConnectString();
+        wsUri = broker.addConnector("ws://127.0.0.1:61623").getPublishableConnectString();
+        broker.setDeleteAllMessagesOnStartup(deleteMessages);
+        broker.start();
+        broker.waitUntilStarted();
+
+        return broker;
+    }
+
+    protected void stopBroker() throws Exception {
+        if (broker != null) {
+            broker.stop();
+            broker.waitUntilStopped();
+            broker = null;
+        }
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        profileDir = new File("activemq-data/profiles");
+        broker = createBroker(true);
+        stompConnect();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        try {
+            stompDisconnect();
+        } catch(Exception e) {
+            // Some tests explicitly disconnect from stomp so can ignore
+        } finally {
+            stopBroker();
+            if (driver != null) {
+                driver.quit();
+                driver = null;
+            }
+        }
     }
 
     @Test
     public void testBrokerStart() throws Exception {
-        BrokerService broker = BrokerFactory.createBroker(new URI("broker:()/localhost?persistent=false&useJmx=false"));
-        broker.addConnector(getBindLocation());
-        broker.start();
-        broker.waitUntilStarted();
-        Thread.sleep(2000);
-        //System.in.read();
-        broker.stop();
-        broker.waitUntilStopped();
+        assertTrue(broker.isStarted());
     }
-    
+
+    @Ignore
+    @Test
+    public void testFireFoxWebSockets() throws Exception {
+        driver = createFireFoxWebDriver();
+        doTestWebSockets(driver);
+    }
+
+    @Ignore
+    @Test
+    public void testChromeWebSockets() throws Exception {
+        driver = createChromeWebDriver();
+        doTestWebSockets(driver);
+    }
+
+    protected WebDriver createChromeWebDriver() throws Exception {
+        File profile = new File(profileDir, "chrome");
+        profile.mkdirs();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--enable-udd-profiles",
+                             "--user-data-dir=" + profile,
+                             "--allow-file-access-from-files");
+        return new ChromeDriver(options);
+    }
+
+    protected WebDriver createFireFoxWebDriver() throws Exception {
+        File profile = new File(profileDir, "firefox");
+        profile.mkdirs();
+        return new FirefoxDriver(new FirefoxProfile(profile));
+    }
+
+    private void stompConnect() throws IOException, URISyntaxException, UnknownHostException {
+        URI connectUri = new URI(stompUri);
+        stompConnection.open(createSocket(connectUri));
+    }
+
+    private Socket createSocket(URI connectUri) throws IOException {
+        return new Socket("127.0.0.1", connectUri.getPort());
+    }
+
+    private void stompDisconnect() throws IOException {
+        if (stompConnection != null) {
+            stompConnection.close();
+            stompConnection = null;
+        }
+    }
+
+    public void doTestWebSockets(WebDriver driver) throws Exception {
+
+        URL url = getClass().getResource("websocket.html");
+
+        LOG.info("working dir = ");
+
+        LOG.info("page url: " + url);
+
+        driver.get(url + "#" + wsUri);
+
+        final WebElement webStatus = driver.findElement(By.id("status"));
+        final WebElement webReceived = driver.findElement(By.id("received"));
+
+        while ("Loading" == webStatus.getText()) {
+            Thread.sleep(100);
+        }
+
+        // Skip test if browser does not support websockets..
+        if (webStatus.getText() != "No WebSockets") {
+
+            assertTrue("Should have connected", Wait.waitFor(new Wait.Condition() {
+
+                @Override
+                public boolean isSatisified() throws Exception {
+                    return webStatus.getText().equals("Connected");
+                }
+            }));
+
+            stompConnection.connect("system", "manager");
+
+            stompConnection.send("/queue/websocket", "Hello");
+            assertTrue("Should have received message by now.", Wait.waitFor(new Wait.Condition() {
+                @Override
+                public boolean isSatisified() throws Exception {
+                    return webReceived.getText().equals("Hello");
+                }
+            }));
+
+            for (int i = 1; i <= MESSAGE_COUNT; ++i) {
+                stompConnection.send("/queue/websocket", "messages #" + i);
+            }
+
+            assertTrue("Should have received messages by now.", Wait.waitFor(new Wait.Condition() {
+                @Override
+                public boolean isSatisified() throws Exception {
+                    return webReceived.getText().equals("messages #" + MESSAGE_COUNT);
+                }
+            }));
+        }
+    }
 }
