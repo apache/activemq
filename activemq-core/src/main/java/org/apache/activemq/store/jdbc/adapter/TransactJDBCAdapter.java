97ac8e22df073a4c6af9891a7165a634fc05520b
Gary Tully <gtully@apache.org>
2012-07-11 10:12:31 +0000

    https://issues.apache.org/jira/browse/AMQ-1191 - apply patch from Mikhail Melamud with thanks. fix up lock statement when isUseLockCreateWhereClause is set
    git-svn-id: https://svn.apache.org/repos/asf/activemq/trunk@1360095 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-core/src/main/java/org/apache/activemq/store/jdbc/adapter/TransactJDBCAdapter.java
+++ activemq-core/src/main/java/org/apache/activemq/store/jdbc/adapter/TransactJDBCAdapter.java
@@ -28,12 +28,12 @@ import org.apache.activemq.store.jdbc.Statements;
 public class TransactJDBCAdapter extends ImageBasedJDBCAdaptor {
     @Override
     public void setStatements(Statements statements) {
-        String lockCreateStatement = "SELECT * FROM " + statements.getFullLockTableName();
+        String lockCreateStatement = "SELECT * FROM " + statements.getFullLockTableName() + " WITH (UPDLOCK, ROWLOCK)";
 
         if (statements.isUseLockCreateWhereClause()) {
             lockCreateStatement += " WHERE ID = 1";
         }
-        lockCreateStatement += " WITH (UPDLOCK, ROWLOCK)";
+
         statements.setLockCreateStatement(lockCreateStatement);
 
         super.setStatements(statements);
