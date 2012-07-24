edf5f64982a23c757e967c3519aa2bc9a380a155
Gary Tully <gtully@apache.org>
2012-07-24 12:54:36 +0000

    fix ignored param in waitfor - resolve regression of org.apache.activemq.broker.region.cursors.NegativeQueueTest#testWithDefaultPrefetchOneConsumer
    git-svn-id: https://svn.apache.org/repos/asf/activemq/trunk@1365019 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-core/src/test/java/org/apache/activemq/util/Wait.java
+++ activemq-core/src/test/java/org/apache/activemq/util/Wait.java
@@ -34,7 +34,7 @@ public class Wait {
     }
 
     public static boolean waitFor(final Condition condition, final long duration) throws Exception {
-        return waitFor(condition, MAX_WAIT_MILLIS, SLEEP_MILLIS);
+        return waitFor(condition, duration, SLEEP_MILLIS);
     }
 
     public static boolean waitFor(final Condition condition, final long duration, final int sleepMillis) throws Exception {
