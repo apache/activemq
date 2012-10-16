e9bf83fc92d9fcfb9bf01e5daa1dbeb378a9c8fe
Timothy A. Bish <tabish@apache.org>
2012-10-16 15:11:25 +0000

    Fix for: https://issues.apache.org/jira/browse/AMQ-4104
    Add the unit test for this
    
    git-svn-id: https://svn.apache.org/repos/asf/activemq/trunk@1398838 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-core/src/test/java/org/apache/activemq/transport/stomp/Stomp11Test.java
+++ activemq-core/src/test/java/org/apache/activemq/transport/stomp/Stomp11Test.java
@@ -127,6 +127,35 @@ public class Stomp11Test extends CombinationTestSupport {
         stompConnection.sendFrame(frame);
     }
 
+    public void testConnectedNeverEncoded() throws Exception {
+
+        String connectFrame = "STOMP\n" +
+                              "login:system\n" +
+                              "passcode:manager\n" +
+                              "accept-version:1.1\n" +
+                              "host:localhost\n" +
+                              "request-id:1\n" +
+                              "\n" + Stomp.NULL;
+        stompConnection.sendFrame(connectFrame);
+
+        String f = stompConnection.receiveFrame();
+        LOG.debug("Broker sent: " + f);
+
+        assertTrue(f.startsWith("CONNECTED"));
+        assertTrue(f.indexOf("response-id:1") >= 0);
+        assertTrue(f.indexOf("version:1.1") >= 0);
+        assertTrue(f.indexOf("session:") >= 0);
+
+        int sessionHeader = f.indexOf("session:");
+        f = f.substring(sessionHeader + "session:".length());
+
+        LOG.info("session header follows: " + f);
+        assertTrue(f.startsWith("ID:"));
+
+        String frame = "DISCONNECT\n" + "\n\n" + Stomp.NULL;
+        stompConnection.sendFrame(frame);
+    }
+
     public void testConnectWithVersionOptions() throws Exception {
 
         String connectFrame = "STOMP\n" +
