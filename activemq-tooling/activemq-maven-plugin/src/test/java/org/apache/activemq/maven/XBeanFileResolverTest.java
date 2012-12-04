87e878b2b59c9182d4ae5de34a0bff5f65621dd5
Timothy A. Bish <tabish@apache.org>
2012-12-04 17:19:35 +0000

    fix for: https://issues.apache.org/jira/browse/AMQ-4169
    Rename the activemq maven plugin so that they follow the maven plugin naming guidelines. 
    Also update the code to use non-deprecated annotations for properties. 
    
    git-svn-id: https://svn.apache.org/repos/asf/activemq/trunk@1417041 13f79535-47bb-0310-9956-ffa450edef68

---
+++ activemq-tooling/activemq-maven-plugin/src/test/java/org/apache/activemq/maven/XBeanFileResolverTest.java
@@ -0,0 +1,49 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.activemq.maven;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Test;
+
+/**
+ * Test for: Helper to convert relative paths to XBean description files to URL-compliant absolute paths.
+ */
+public class XBeanFileResolverTest {
+    private static final String XBEAN_FILE = "xbean:file:";
+
+    @Test
+    public void urlToXBeanFileShouldBeResolvedToAbsolutePath() throws IOException {
+        String currentDirectory = getCurrentDirectoryLinuxStyle();
+        String relativeXBeanFilePath = "src/main/resources/activemq.xml";
+
+        // e.g. xbean:file:C:/dev/src/active-mq/activemq-tooling/activemq-maven-plugin/src/main/resources/activemq.xml
+        String expectedUrl = XBEAN_FILE + currentDirectory + "/" + relativeXBeanFilePath;
+
+        String actualUrl = XBeanFileResolver.toUrlCompliantAbsolutePath(XBEAN_FILE + relativeXBeanFilePath);
+
+        assertEquals(expectedUrl, actualUrl);
+    }
+
+    private String getCurrentDirectoryLinuxStyle() throws IOException {
+        String currentDirectory = new File(".").getCanonicalPath();
+        return currentDirectory.replace("\\", "/");
+    }
+}
