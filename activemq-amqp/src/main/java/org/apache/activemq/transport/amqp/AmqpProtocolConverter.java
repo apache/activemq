15298cb1b4325e3cc8169356676027f21617bac8
Hiram R. Chirino <chirino@apache.org>
2013-01-25 16:07:12 +0000

    Switch to proton snapshot to pick up fix /w link closing.  Also make a link fail if the address is an empty string.
    git-svn-id: https://svn.apache.org/repos/asf/activemq/trunk@1438570 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-amqp/src/main/java/org/apache/activemq/transport/amqp/AmqpProtocolConverter.java
+++ activemq-amqp/src/main/java/org/apache/activemq/transport/amqp/AmqpProtocolConverter.java
@@ -634,13 +634,13 @@ class AmqpProtocolConverter {
             return null;
         } else if( terminus instanceof org.apache.qpid.proton.amqp.messaging.Source) {
             org.apache.qpid.proton.amqp.messaging.Source source = (org.apache.qpid.proton.amqp.messaging.Source)terminus;
-            if( source.getAddress() == null ) {
+            if( source.getAddress() == null || source.getAddress().length()==0) {
                 throw new AmqpProtocolException("amqp:invalid-field", "source address not set");
             }
             return ActiveMQDestination.createDestination(source.getAddress(), ActiveMQDestination.QUEUE_TYPE);
         } else if( terminus instanceof org.apache.qpid.proton.amqp.messaging.Target) {
             org.apache.qpid.proton.amqp.messaging.Target target = (org.apache.qpid.proton.amqp.messaging.Target)terminus;
-            if( target.getAddress() == null ) {
+            if( target.getAddress() == null || target.getAddress().length()==0) {
                 throw new AmqpProtocolException("amqp:invalid-field", "target address not set");
             }
             return ActiveMQDestination.createDestination(target.getAddress(), ActiveMQDestination.QUEUE_TYPE);
