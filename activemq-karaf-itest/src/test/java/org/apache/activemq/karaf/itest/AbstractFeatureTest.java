e20234e2cb32139530a4274814d52e989adee080
Gary Tully <gtully@apache.org>
2013-02-01 13:18:03 +0000

    fix up property file ref
    git-svn-id: https://svn.apache.org/repos/asf/activemq/trunk@1441439 13f79535-47bb-0310-9956-ffa450edef68

--- activemq-karaf-itest/src/test/java/org/apache/activemq/karaf/itest/AbstractFeatureTest.java
+++ activemq-karaf-itest/src/test/java/org/apache/activemq/karaf/itest/AbstractFeatureTest.java
@@ -159,7 +159,7 @@ public abstract class AbstractFeatureTest {
     public static Option[] configureBrokerStart(Option[] existingOptions) {
         ArrayList<Option> newOptions = new ArrayList<Option>();
         newOptions.addAll(Arrays.asList(existingOptions));
-        newOptions.add(replaceConfigurationFile("etc/org.apache.activemq.server-default.cfg", new File(basedir + "/src/test/resources/org/apache/activemq/karaf/itest/org.apache.activemq.server.cfg")));
+        newOptions.add(replaceConfigurationFile("etc/org.apache.activemq.server-default.cfg", new File(basedir + "/src/test/resources/org/apache/activemq/karaf/itest/org.apache.activemq.server-default.cfg")));
         newOptions.add(replaceConfigurationFile("etc/activemq.xml", new File(basedir + "/src/test/resources/org/apache/activemq/karaf/itest/activemq.xml")));
         return newOptions.toArray(new Option[]{});
     }
