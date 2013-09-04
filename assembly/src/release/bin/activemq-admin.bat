1f57a6408fe878424d0d32a963582f4befae7399
Dejan Bosanac <dejan@nighttale.net>
2013-09-04 17:36:08 +0200

    https://issues.apache.org/jira/browse/AMQ-4594 - fix hawtio issues; linkage error and authentication

--- assembly/src/release/bin/activemq-admin.bat
+++ assembly/src/release/bin/activemq-admin.bat
@@ -80,7 +80,7 @@ if "%ACTIVEMQ_TMP%" == "" set ACTIVEMQ_TMP=%ACTIVEMQ_DATA%\tmp
 if /i not "%1" == "start" goto debugOpts
 
 
-if "%ACTIVEMQ_OPTS%" == "" set ACTIVEMQ_OPTS=-Xmx512M -Dorg.apache.activemq.UseDedicatedTaskRunner=true -Djava.util.logging.config.file=logging.properties
+if "%ACTIVEMQ_OPTS%" == "" set ACTIVEMQ_OPTS=-Xmx1G -Dorg.apache.activemq.UseDedicatedTaskRunner=true -Djava.util.logging.config.file=logging.properties -Dhawtio.authenticationEnabled=false
 
 if "%SUNJMX%" == "" set SUNJMX=-Dcom.sun.management.jmxremote
 REM set SUNJMX=-Dcom.sun.management.jmxremote.port=1099 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false
