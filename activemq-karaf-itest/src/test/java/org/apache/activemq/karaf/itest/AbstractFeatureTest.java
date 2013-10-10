901867ec1007587d1d6a70cbb1062e00e8804778
Dejan Bosanac <dejan@nighttale.net>
2013-10-10 11:51:40 +0200

    fix karaf feature test; allow more time for feature to install

--- activemq-karaf-itest/src/test/java/org/apache/activemq/karaf/itest/AbstractFeatureTest.java
+++ activemq-karaf-itest/src/test/java/org/apache/activemq/karaf/itest/AbstractFeatureTest.java
@@ -137,10 +137,16 @@ public abstract class AbstractFeatureTest {
 	 * @param feature
 	 * @throws Exception
 	 */
-	public void installAndAssertFeature(String feature) throws Exception {
+	public void installAndAssertFeature(final String feature) throws Throwable {
 		System.err.println(executeCommand("features:install " + feature));
 		System.err.println(executeCommand("osgi:list -t 0"));
-		assertTrue("Expected " + feature + " feature to be installed.", featuresService.isInstalled(featuresService.getFeature(feature)));
+        withinReason(new Callable<Boolean>() {
+            @Override
+            public Boolean call() throws Exception {
+                assertTrue("Expected " + feature + " feature to be installed.", featuresService.isInstalled(featuresService.getFeature(feature)));
+                return true;
+            }
+        });
 	}
 
 
