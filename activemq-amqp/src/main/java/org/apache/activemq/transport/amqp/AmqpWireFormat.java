c4b95671aa31924f5b37714f26c5cfba90a01ca8
Timothy Bish <tabish121@gmai.com>
2014-01-08 11:27:39 -0500

    https://issues.apache.org/jira/browse/AMQ-4914
    Up the max frame size to a value of 1mb so that we are restricted by the
    default in the QPid client which is 32k.

--- activemq-amqp/src/main/java/org/apache/activemq/transport/amqp/AmqpWireFormat.java
+++ activemq-amqp/src/main/java/org/apache/activemq/transport/amqp/AmqpWireFormat.java
@@ -16,22 +16,26 @@
  */
 package org.apache.activemq.transport.amqp;
 
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 import org.apache.activemq.util.ByteArrayInputStream;
 import org.apache.activemq.util.ByteArrayOutputStream;
 import org.apache.activemq.util.ByteSequence;
 import org.apache.activemq.wireformat.WireFormat;
 import org.fusesource.hawtbuf.Buffer;
 
-import java.io.*;
-
-/**
- */
 public class AmqpWireFormat implements WireFormat {
 
+    public static final int DEFAULT_MAX_FRAME_SIZE = 1024 * 1024 * 1;
 
     private int version = 1;
-    private long maxFrameSize = 1024*1024*100;
+    private long maxFrameSize = DEFAULT_MAX_FRAME_SIZE;
 
+    @Override
     public ByteSequence marshal(Object command) throws IOException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         DataOutputStream dos = new DataOutputStream(baos);
@@ -40,27 +44,31 @@ public class AmqpWireFormat implements WireFormat {
         return baos.toByteSequence();
     }
 
+    @Override
     public Object unmarshal(ByteSequence packet) throws IOException {
         ByteArrayInputStream stream = new ByteArrayInputStream(packet);
         DataInputStream dis = new DataInputStream(stream);
         return unmarshal(dis);
     }
 
+    @Override
     public void marshal(Object command, DataOutput dataOut) throws IOException {
         Buffer frame = (Buffer) command;
         frame.writeTo(dataOut);
     }
 
     boolean magicRead = false;
+
+    @Override
     public Object unmarshal(DataInput dataIn) throws IOException {
-        if( !magicRead ) {
+        if (!magicRead) {
             Buffer magic = new Buffer(8);
             magic.readFrom(dataIn);
             magicRead = true;
             return new AmqpHeader(magic);
         } else {
             int size = dataIn.readInt();
-            if( size > maxFrameSize) {
+            if (size > maxFrameSize) {
                 throw new AmqpProtocolException("Frame size exceeded max frame length.");
             }
             Buffer frame = new Buffer(size);
@@ -71,8 +79,7 @@ public class AmqpWireFormat implements WireFormat {
         }
     }
 
-    /**
-     */
+    @Override
     public void setVersion(int version) {
         this.version = version;
     }
@@ -80,10 +87,10 @@ public class AmqpWireFormat implements WireFormat {
     /**
      * @return the version of the wire format
      */
+    @Override
     public int getVersion() {
         return this.version;
     }
-
 
     public long getMaxFrameSize() {
         return maxFrameSize;
