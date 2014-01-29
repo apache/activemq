f5e54cf75cbf7dc48fc894a8f6db5880c2abcd49
Timothy Bish <tabish121@gmai.com>
2014-01-29 17:20:59 -0500

    fix typo in enum where tests were commented out.

--- activemq-amqp/src/test/java/org/apache/activemq/transport/amqp/joram/JoramJmsNioPlusSslTest.java
+++ activemq-amqp/src/test/java/org/apache/activemq/transport/amqp/joram/JoramJmsNioPlusSslTest.java
@@ -28,28 +28,14 @@ import org.junit.Rule;
 import org.junit.rules.Timeout;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import org.objectweb.jtests.jms.conform.connection.ConnectionTest;
-import org.objectweb.jtests.jms.conform.message.MessageBodyTest;
-import org.objectweb.jtests.jms.conform.message.MessageDefaultTest;
-import org.objectweb.jtests.jms.conform.message.MessageTypeTest;
 import org.objectweb.jtests.jms.conform.message.headers.MessageHeaderTest;
-import org.objectweb.jtests.jms.conform.message.properties.JMSXPropertyTest;
-import org.objectweb.jtests.jms.conform.message.properties.MessagePropertyConversionTest;
-import org.objectweb.jtests.jms.conform.message.properties.MessagePropertyTest;
-import org.objectweb.jtests.jms.conform.queue.QueueBrowserTest;
-import org.objectweb.jtests.jms.conform.queue.TemporaryQueueTest;
-import org.objectweb.jtests.jms.conform.selector.SelectorSyntaxTest;
-import org.objectweb.jtests.jms.conform.selector.SelectorTest;
-import org.objectweb.jtests.jms.conform.session.QueueSessionTest;
-import org.objectweb.jtests.jms.conform.session.SessionTest;
-import org.objectweb.jtests.jms.conform.topic.TemporaryTopicTest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
     //    TopicSessionTest.class,    // Hangs, see https://issues.apache.org/jira/browse/PROTON-154
-    MessageHeaderTest.class,
+    MessageHeaderTest.class
     //QueueBrowserTest.class,
     //MessageTypeTest.class,
     //,UnifiedSessionTest.class    // https://issues.apache.org/jira/browse/AMQ-4375
