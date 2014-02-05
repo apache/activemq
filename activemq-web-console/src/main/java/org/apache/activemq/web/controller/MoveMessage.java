3321f99643edc5ebc4bd8f4b9eaea90744fc6d60
Daniel Kulp <dkulp@apache.org>
2014-02-05 15:19:44 -0500

    [AMQ-4174] Redirect back to queue overview after delete/moving message Patch from Robert Balent applied

--- activemq-web-console/src/main/java/org/apache/activemq/web/controller/MoveMessage.java
+++ activemq-web-console/src/main/java/org/apache/activemq/web/controller/MoveMessage.java
@@ -49,7 +49,7 @@ public class MoveMessage extends DestinationFacade implements Controller {
             	log.warn("No queue named: " + getPhysicalDestinationName());
             }
         }
-        return redirectToBrowseView();
+        return redirectToDestinationView();
     }
 
     public String getMessageId() {
