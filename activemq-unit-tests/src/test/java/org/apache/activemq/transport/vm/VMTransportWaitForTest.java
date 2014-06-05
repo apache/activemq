1e3f4f192b6240271112e2d8dc46413925b46364
Timothy Bish <tabish121@gmail.com>
2014-06-05 17:52:01 -0400

    Convert to JUnit 4 test and add a timeout, also remove the tcp transport connector on a fixed port as its not needed.

--- activemq-unit-tests/src/test/java/org/apache/activemq/transport/vm/VMTransportWaitForTest.java
+++ activemq-unit-tests/src/test/java/org/apache/activemq/transport/vm/VMTransportWaitForTest.java
@@ -16,28 +16,32 @@
  */
 package org.apache.activemq.transport.vm;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 import java.net.URI;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import javax.jms.JMSException;
 
-import junit.framework.TestCase;
-
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.broker.BrokerService;
+import org.junit.Test;
 
-public class VMTransportWaitForTest extends TestCase {
+public class VMTransportWaitForTest {
 
-    private static final String VM_BROKER_URI_NO_WAIT = 
+    private static final String VM_BROKER_URI_NO_WAIT =
         "vm://localhost?broker.persistent=false&create=false";
-    
-    private static final String VM_BROKER_URI_WAIT_FOR_START = 
+
+    private static final String VM_BROKER_URI_WAIT_FOR_START =
         VM_BROKER_URI_NO_WAIT + "&waitForStart=20000";
-    
+
     CountDownLatch started = new CountDownLatch(1);
     CountDownLatch gotConnection = new CountDownLatch(1);
 
+    @Test(timeout=90000)
     public void testWaitFor() throws Exception {
         try {
             ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory(new URI(VM_BROKER_URI_NO_WAIT));
@@ -45,32 +49,32 @@ public class VMTransportWaitForTest extends TestCase {
             fail("expect broker not exist exception");
         } catch (JMSException expectedOnNoBrokerAndNoCreate) {
         }
-        
-        // spawn a thread that will wait for an embedded broker to start via vm://..
+
+        // spawn a thread that will wait for an embedded broker to start via
+        // vm://..
         Thread t = new Thread() {
+            @Override
             public void run() {
-                    try {
-                        started.countDown();
-                        ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory(new URI(VM_BROKER_URI_WAIT_FOR_START));
-                        cf.createConnection();
-                        gotConnection.countDown();
-                   
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        fail("unexpected exception: " + e);
-                    }
+                try {
+                    started.countDown();
+                    ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory(new URI(VM_BROKER_URI_WAIT_FOR_START));
+                    cf.createConnection();
+                    gotConnection.countDown();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    fail("unexpected exception: " + e);
+                }
             }
         };
         t.start();
         started.await(20, TimeUnit.SECONDS);
         Thread.yield();
         assertFalse("has not got connection", gotConnection.await(2, TimeUnit.SECONDS));
-        
+
         BrokerService broker = new BrokerService();
         broker.setPersistent(false);
-        broker.addConnector("tcp://localhost:61616");
         broker.start();
         assertTrue("has got connection", gotConnection.await(400, TimeUnit.MILLISECONDS));
-        broker.stop(); 
+        broker.stop();
     }
 }
