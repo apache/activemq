533cedc4fc555b4df573c8e17594941019f5e50b
Timothy Bish <tabish121@gmail.com>
2014-08-07 14:18:40 -0400

    https://issues.apache.org/jira/browse/AMQ-5220
    Fixes empty message bodies from responses to statistics plugin queries
    over the STOMP transport. 
    
    This closes #41

--- activemq-stomp/src/test/java/org/apache/activemq/transport/stomp/StompAdvisoryTest.java
+++ activemq-stomp/src/test/java/org/apache/activemq/transport/stomp/StompAdvisoryTest.java
@@ -22,27 +22,39 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.HashMap;
+import java.util.List;
 
 import javax.jms.Connection;
 import javax.jms.Destination;
 import javax.jms.Message;
 import javax.jms.MessageConsumer;
 import javax.jms.MessageProducer;
+import javax.jms.Queue;
 import javax.jms.Session;
+import javax.jms.Topic;
 
 import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.broker.BrokerPlugin;
 import org.apache.activemq.broker.region.policy.ConstantPendingMessageLimitStrategy;
 import org.apache.activemq.broker.region.policy.PolicyEntry;
 import org.apache.activemq.broker.region.policy.PolicyMap;
 import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.activemq.plugin.StatisticsBrokerPlugin;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class StompAdvisoryTest extends StompTestSupport {
+    static final String STATS_DESTINATION_PREFIX = "ActiveMQ.Statistics.Destination";
+
     private static final Logger LOG = LoggerFactory.getLogger(StompAdvisoryTest.class);
 
     protected ActiveMQConnection connection;
+
+    @Override
+    protected void addAdditionalPlugins(List<BrokerPlugin> plugins) throws Exception {
+        plugins.add(new StatisticsBrokerPlugin());
+    }
 
     @Override
     protected void applyBrokerPolicies() throws Exception {
@@ -265,6 +277,52 @@ public class StompAdvisoryTest extends StompTestSupport {
         assertEquals(f.getAction(),"MESSAGE");
         assertTrue("Should have a body", f.getBody().length() > 0);
         assertTrue(f.getBody().startsWith("{\"ProducerInfo\":"));
+
+        c.stop();
+        c.close();
+    }
+
+    @Test
+    public void testStatisticsAdvisory() throws Exception {
+        Connection c = cf.createConnection("system", "manager");
+        c.start();
+        final Session session = c.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        final Topic replyTo = session.createTopic("stats");
+
+        // Dummy Queue used to later gather statistics.
+        final ActiveMQQueue testQueue = new ActiveMQQueue("queueToBeTestedForStats");
+        final MessageProducer producer = session.createProducer(null);
+        Message mess = session.createTextMessage("test");
+        producer.send(testQueue, mess);
+
+        // Create a request for Queue statistics
+        Thread child = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                    Queue query = session.createQueue(STATS_DESTINATION_PREFIX + testQueue.getQueueName());
+                    Message msg = session.createMessage();
+                    msg.setJMSReplyTo(replyTo);
+                    producer.send(query, msg);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        child.start();
+
+        // Attempt to gather the statistics response from the previous request.
+        stompConnection.connect("system", "manager");
+        stompConnection.subscribe("/topic/" + replyTo.getTopicName(), Stomp.Headers.Subscribe.AckModeValues.AUTO);
+        stompConnection.begin("TX");
+        StompFrame f = stompConnection.receive(5000);
+        stompConnection.commit("TX");
+
+        LOG.debug(f.toString());
+        assertEquals(f.getAction(),"MESSAGE");
+        assertTrue("Should have a body", f.getBody().length() > 0);
+        assertTrue("Should contains memoryUsage stats", f.getBody().contains("memoryUsage"));
 
         c.stop();
         c.close();
