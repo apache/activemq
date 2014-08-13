f07e6a53216f9388185ac2b39f366f3bfd6a8a55
Timothy Bish <tabish121@gmail.com>
2014-08-13 11:15:29 -0400

    Remove unused ConnectionControl handling.

--- activemq-broker/src/main/java/org/apache/activemq/broker/TransportConnection.java
+++ activemq-broker/src/main/java/org/apache/activemq/broker/TransportConnection.java
@@ -1534,10 +1534,6 @@ public class TransportConnection implements Connection, Task, CommandVisitor {
 
     @Override
     public Response processControlCommand(ControlCommand command) throws Exception {
-        String control = command.getCommand();
-        if (control != null && control.equals("shutdown")) {
-            System.exit(0);
-        }
         return null;
     }
 
