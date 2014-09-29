8163b0b95ea66a01ec7def01b709ffc05eacaeed
Kevin Earls <kevin@kevinearls.com>
2014-09-29 10:47:09 +0200

    Handle exceptions described in AMQ-5372 to avoid cascading test failures

--- activemq-unit-tests/src/test/java/org/apache/activemq/EmbeddedBrokerTestSupport.java
+++ activemq-unit-tests/src/test/java/org/apache/activemq/EmbeddedBrokerTestSupport.java
@@ -58,7 +58,10 @@ public abstract class EmbeddedBrokerTestSupport extends CombinationTestSupport {
 
     protected void tearDown() throws Exception {
         if (broker != null) {
-            broker.stop();
+            try {
+                broker.stop();
+            } catch (Exception e) {
+            }
         }
     }
 
