383c27e245ccd1ea9b8fbf59de8aa7a5d0f0b8b4
gtully <gary.tully@gmail.com>
2015-01-09 11:51:16 +0000

    add some more diagnostic logging to test

--- activemq-unit-tests/src/test/java/org/apache/activemq/network/NetworkBrokerDetachTest.java
+++ activemq-unit-tests/src/test/java/org/apache/activemq/network/NetworkBrokerDetachTest.java
@@ -19,6 +19,7 @@ package org.apache.activemq.network;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -267,6 +268,7 @@ public class NetworkBrokerDetachTest {
 
                         if (view.getName().equals(destination.getPhysicalName())) {
                             LOG.info("Consumers for " + destination.getPhysicalName() + " on " + broker + " : " + view.getConsumerCount());
+                            LOG.info("Subs: " + Arrays.asList(view.getSubscriptions()));
                             if (expectedCount == view.getConsumerCount()) {
                                 result = true;
                             }
@@ -290,7 +292,7 @@ public class NetworkBrokerDetachTest {
                 if (view != null) {
                     ObjectName[] subs = broker.getAdminView().getInactiveDurableTopicSubscribers();
                     if (subs != null) {
-                        LOG.info("inactive durable subs on " + broker + " : " + subs);
+                        LOG.info("inactive durable subs on " + broker + " : " + Arrays.asList(subs));
                         if (expectedCount == subs.length) {
                             result = true;
                         }
