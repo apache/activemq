cb370d06e9ac23b6c363ac93b0e484848a09f403
Timothy Bish <tabish121@gmail.com>
2015-04-14 12:33:05 -0400

    Add a test for the unsettled sate as well, and some logs.

--- activemq-amqp/src/test/java/org/apache/activemq/transport/amqp/interop/AmqpSenderTest.java
+++ activemq-amqp/src/test/java/org/apache/activemq/transport/amqp/interop/AmqpSenderTest.java
@@ -96,6 +96,39 @@ public class AmqpSenderTest extends AmqpClientTestSupport {
     }
 
     @Test(timeout = 60000)
+    public void testUnsettledSender() throws Exception {
+        final int MSG_COUNT = 1000;
+
+        AmqpClient client = createAmqpClient();
+        AmqpConnection connection = client.connect();
+        AmqpSession session = connection.createSession();
+
+        AmqpSender sender = session.createSender("topic://" + getTestName(), false);
+
+        for (int i = 1; i <= MSG_COUNT; ++i) {
+            AmqpMessage message = new AmqpMessage();
+            message.setText("Test-Message: " + i);
+            sender.send(message);
+
+            if (i % 1000 == 0) {
+                LOG.info("Sent message: {}", i);
+            }
+        }
+
+        final TopicViewMBean topic = getProxyToTopic(getTestName());
+        assertTrue("All messages should arrive", Wait.waitFor(new Wait.Condition() {
+
+            @Override
+            public boolean isSatisified() throws Exception {
+                return topic.getEnqueueCount() == MSG_COUNT;
+            }
+        }));
+
+        sender.close();
+        connection.close();
+    }
+
+    @Test(timeout = 60000)
     public void testPresettledSender() throws Exception {
         final int MSG_COUNT = 1000;
 
@@ -105,10 +138,14 @@ public class AmqpSenderTest extends AmqpClientTestSupport {
 
         AmqpSender sender = session.createSender("topic://" + getTestName(), true);
 
-        for (int i = 0; i < MSG_COUNT; ++i) {
+        for (int i = 1; i <= MSG_COUNT; ++i) {
             AmqpMessage message = new AmqpMessage();
             message.setText("Test-Message: " + i);
             sender.send(message);
+
+            if (i % 1000 == 0) {
+                LOG.info("Sent message: {}", i);
+            }
         }
 
         final TopicViewMBean topic = getProxyToTopic(getTestName());
