eea3230c3783dd22abceee4f53cb928bc4121af1
Timothy Bish <tabish121@gmail.com>
2015-04-28 18:10:24 -0400

    https://issues.apache.org/jira/browse/AMQ-5155
    Ensure that the inactivity monitor can see that new data is arriving to
    prevent wrongful disconnect of clients.

--- activemq-http/src/main/java/org/apache/activemq/transport/ws/jetty8/StompSocket.java
+++ activemq-http/src/main/java/org/apache/activemq/transport/ws/jetty8/StompSocket.java
@@ -17,20 +17,10 @@
 package org.apache.activemq.transport.ws.jetty8;
 
 import java.io.IOException;
-import java.util.concurrent.CountDownLatch;
 
-import org.apache.activemq.command.Command;
-import org.apache.activemq.command.KeepAliveInfo;
-import org.apache.activemq.transport.TransportSupport;
-import org.apache.activemq.transport.stomp.ProtocolConverter;
 import org.apache.activemq.transport.stomp.Stomp;
 import org.apache.activemq.transport.stomp.StompFrame;
-import org.apache.activemq.transport.stomp.StompInactivityMonitor;
-import org.apache.activemq.transport.stomp.StompTransport;
-import org.apache.activemq.transport.stomp.StompWireFormat;
-import org.apache.activemq.util.ByteSequence;
-import org.apache.activemq.util.IOExceptionSupport;
-import org.apache.activemq.util.ServiceStopper;
+import org.apache.activemq.transport.ws.AbstractStompSocket;
 import org.eclipse.jetty.websocket.WebSocket;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -38,14 +28,11 @@ import org.slf4j.LoggerFactory;
 /**
  * Implements web socket and mediates between servlet and the broker
  */
-class StompSocket extends TransportSupport implements WebSocket.OnTextMessage, StompTransport {
+class StompSocket extends AbstractStompSocket implements WebSocket.OnTextMessage {
+
     private static final Logger LOG = LoggerFactory.getLogger(StompSocket.class);
 
-    Connection outbound;
-    ProtocolConverter protocolConverter = new ProtocolConverter(this, null);
-    StompWireFormat wireFormat = new StompWireFormat();
-    private final CountDownLatch socketTransportStarted = new CountDownLatch(1);
-    private final StompInactivityMonitor stompInactivityMonitor = new StompInactivityMonitor(this, wireFormat);
+    private Connection outbound;
 
     @Override
     public void onOpen(Connection connection) {
@@ -63,80 +50,11 @@ class StompSocket extends TransportSupport implements WebSocket.OnTextMessage, S
 
     @Override
     public void onMessage(String data) {
-
-        if (!transportStartedAtLeastOnce()) {
-            LOG.debug("Waiting for StompSocket to be properly started...");
-            try {
-                socketTransportStarted.await();
-            } catch (InterruptedException e) {
-                LOG.warn("While waiting for StompSocket to be properly started, we got interrupted!! Should be okay, but you could see race conditions...");
-            }
-        }
-
-
-        try {
-            if (data != null) {
-                if (data.equals("\n")) {
-                    sendToActiveMQ(new KeepAliveInfo());
-                } else {
-                    protocolConverter.onStompCommand((StompFrame)wireFormat.unmarshal(new ByteSequence(data.getBytes("UTF-8"))));
-                }
-            }
-        } catch (Exception e) {
-            onException(IOExceptionSupport.create(e));
-        }
-    }
-
-    private boolean transportStartedAtLeastOnce() {
-        return socketTransportStarted.getCount() == 0;
-    }
-
-    @Override
-    protected void doStart() throws Exception {
-        socketTransportStarted.countDown();
-        stompInactivityMonitor.setTransportListener(getTransportListener());
-    }
-
-    @Override
-    protected void doStop(ServiceStopper stopper) throws Exception {
-    }
-
-    @Override
-    public int getReceiveCounter() {
-        return 0;
-    }
-
-    @Override
-    public String getRemoteAddress() {
-        return "StompSocket_" + this.hashCode();
-    }
-
-    @Override
-    public void oneway(Object command) throws IOException {
-        try {
-            protocolConverter.onActiveMQCommand((Command)command);
-        } catch (Exception e) {
-            onException(IOExceptionSupport.create(e));
-        }
-    }
-
-    @Override
-    public void sendToActiveMQ(Command command) {
-        doConsume(command);
+        processStompFrame(data);
     }
 
     @Override
     public void sendToStomp(StompFrame command) throws IOException {
         outbound.sendMessage(command.format());
-    }
-
-    @Override
-    public StompInactivityMonitor getInactivityMonitor() {
-        return stompInactivityMonitor;
-    }
-
-    @Override
-    public StompWireFormat getWireFormat() {
-        return this.wireFormat;
     }
 }
--- activemq-http/src/main/java/org/apache/activemq/transport/ws/jetty9/StompSocket.java
+++ activemq-http/src/main/java/org/apache/activemq/transport/ws/jetty9/StompSocket.java
@@ -17,20 +17,10 @@
 package org.apache.activemq.transport.ws.jetty9;
 
 import java.io.IOException;
-import java.util.concurrent.CountDownLatch;
 
-import org.apache.activemq.command.Command;
-import org.apache.activemq.command.KeepAliveInfo;
-import org.apache.activemq.transport.TransportSupport;
-import org.apache.activemq.transport.stomp.ProtocolConverter;
 import org.apache.activemq.transport.stomp.Stomp;
 import org.apache.activemq.transport.stomp.StompFrame;
-import org.apache.activemq.transport.stomp.StompInactivityMonitor;
-import org.apache.activemq.transport.stomp.StompTransport;
-import org.apache.activemq.transport.stomp.StompWireFormat;
-import org.apache.activemq.util.ByteSequence;
-import org.apache.activemq.util.IOExceptionSupport;
-import org.apache.activemq.util.ServiceStopper;
+import org.apache.activemq.transport.ws.AbstractStompSocket;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.WebSocketListener;
 import org.slf4j.Logger;
@@ -39,66 +29,15 @@ import org.slf4j.LoggerFactory;
 /**
  * Implements web socket and mediates between servlet and the broker
  */
-class StompSocket extends TransportSupport implements WebSocketListener, StompTransport {
+class StompSocket extends AbstractStompSocket implements WebSocketListener {
+
     private static final Logger LOG = LoggerFactory.getLogger(StompSocket.class);
 
-    Session session;
-    ProtocolConverter protocolConverter = new ProtocolConverter(this, null);
-    StompWireFormat wireFormat = new StompWireFormat();
-    private final CountDownLatch socketTransportStarted = new CountDownLatch(1);
-    private final StompInactivityMonitor stompInactivityMonitor = new StompInactivityMonitor(this, wireFormat);
-
-    private boolean transportStartedAtLeastOnce() {
-        return socketTransportStarted.getCount() == 0;
-    }
-
-    @Override
-    protected void doStart() throws Exception {
-        socketTransportStarted.countDown();
-        stompInactivityMonitor.setTransportListener(getTransportListener());
-    }
-
-    @Override
-    protected void doStop(ServiceStopper stopper) throws Exception {
-    }
-
-    @Override
-    public int getReceiveCounter() {
-        return 0;
-    }
-
-    @Override
-    public String getRemoteAddress() {
-        return "StompSocket_" + this.hashCode();
-    }
-
-    @Override
-    public void oneway(Object command) throws IOException {
-        try {
-            protocolConverter.onActiveMQCommand((Command)command);
-        } catch (Exception e) {
-            onException(IOExceptionSupport.create(e));
-        }
-    }
-
-    @Override
-    public void sendToActiveMQ(Command command) {
-        doConsume(command);
-    }
+    private Session session;
 
     @Override
     public void sendToStomp(StompFrame command) throws IOException {
         session.getRemote().sendString(command.format());
-    }
-
-    @Override
-    public StompInactivityMonitor getInactivityMonitor() {
-        return stompInactivityMonitor;
-    }
-
-    @Override
-    public StompWireFormat getWireFormat() {
-        return this.wireFormat;
     }
 
     @Override
@@ -125,25 +64,6 @@ class StompSocket extends TransportSupport implements WebSocketListener, StompTr
 
     @Override
     public void onWebSocketText(String data) {
-        if (!transportStartedAtLeastOnce()) {
-            LOG.debug("Waiting for StompSocket to be properly started...");
-            try {
-                socketTransportStarted.await();
-            } catch (InterruptedException e) {
-                LOG.warn("While waiting for StompSocket to be properly started, we got interrupted!! Should be okay, but you could see race conditions...");
-            }
-        }
-
-        try {
-            if (data != null) {
-                if (data.equals("\n")) {
-                    sendToActiveMQ(new KeepAliveInfo());
-                } else {
-                    protocolConverter.onStompCommand((StompFrame)wireFormat.unmarshal(new ByteSequence(data.getBytes("UTF-8"))));
-                }
-            }
-        } catch (Exception e) {
-            onException(IOExceptionSupport.create(e));
-        }
+        processStompFrame(data);
     }
 }
