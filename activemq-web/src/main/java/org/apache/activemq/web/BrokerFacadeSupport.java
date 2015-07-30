ac3d088647cacfe298a61ba5b9f1b3941249142d
Christopher L. Shannon (cshannon) <christopher.l.shannon@gmail.com>
2015-07-30 19:37:21 +0000

    https://issues.apache.org/jira/browse/AMQ-5896
    Fixing a potential race condition with BrokerFacacdeSupport that could
    cause an InstanceNotFoundException when getting a destination from
    a RemoteJMXBrokerFacade instance

--- activemq-web/src/main/java/org/apache/activemq/web/BrokerFacadeSupport.java
+++ activemq-web/src/main/java/org/apache/activemq/web/BrokerFacadeSupport.java
@@ -23,6 +23,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
+import javax.management.InstanceNotFoundException;
 import javax.management.ObjectName;
 import javax.management.QueryExp;
 import javax.management.openmbean.CompositeData;
@@ -37,10 +38,11 @@ import org.apache.activemq.broker.jmx.JobSchedulerViewMBean;
 import org.apache.activemq.broker.jmx.ManagementContext;
 import org.apache.activemq.broker.jmx.NetworkBridgeViewMBean;
 import org.apache.activemq.broker.jmx.NetworkConnectorViewMBean;
+import org.apache.activemq.broker.jmx.ProducerViewMBean;
 import org.apache.activemq.broker.jmx.QueueViewMBean;
 import org.apache.activemq.broker.jmx.SubscriptionViewMBean;
-import org.apache.activemq.broker.jmx.ProducerViewMBean;
 import org.apache.activemq.broker.jmx.TopicViewMBean;
+import org.apache.commons.lang.exception.ExceptionUtils;
 import org.springframework.util.StringUtils;
 
 /**
@@ -127,9 +129,17 @@ public abstract class BrokerFacadeSupport implements BrokerFacade {
             String name) {
         Iterator<? extends DestinationViewMBean> iter = collection.iterator();
         while (iter.hasNext()) {
-            DestinationViewMBean destinationViewMBean = iter.next();
-            if (name.equals(destinationViewMBean.getName())) {
-                return destinationViewMBean;
+            try {
+                DestinationViewMBean destinationViewMBean = iter.next();
+                if (name.equals(destinationViewMBean.getName())) {
+                    return destinationViewMBean;
+                }
+            } catch (Exception ex) {
+                Class<InstanceNotFoundException> infe = InstanceNotFoundException.class;
+                if (!infe.isInstance(ex) && !infe.isInstance(ExceptionUtils.getRootCause(ex))) {
+                    // Only throw if not an expected InstanceNotFoundException exception
+                    throw ex;
+                }
             }
         }
         return null;
