1bef4de4dc6744c23c22d2dac0faf0c9a6ab68d9
gtully <gary.tully@gmail.com>
2015-11-30 12:47:13 +0000

    Revert "https://issues.apache.org/jira/browse/AMQ-6065" CPL dependency is not fully compatable with ASF and is not really necessary
    This reverts commit 39a20bd788c664105c18886abf77c372cf9229aa.

--- activemq-broker/src/test/java/org/apache/activemq/util/DefaultIOExceptionHandlerTest.java
+++ activemq-broker/src/test/java/org/apache/activemq/util/DefaultIOExceptionHandlerTest.java
@@ -17,43 +17,27 @@
 
 package org.apache.activemq.util;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
 import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
-
 import org.apache.activemq.broker.BrokerService;
 import org.apache.activemq.broker.SuppressReplyException;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.contrib.java.lang.system.ExpectedSystemExit;
-import org.junit.contrib.java.lang.system.internal.NoExitSecurityManager;
+
+
+import static org.junit.Assert.*;
 
 public class DefaultIOExceptionHandlerTest {
 
-    @Rule
-    public final ExpectedSystemExit exit = ExpectedSystemExit.none();
-
     DefaultIOExceptionHandler underTest = new DefaultIOExceptionHandler();
 
-    @Test(timeout=20000)
+    @Test
     public void testHandleWithShutdownOnExit() throws Exception {
-        exit.expectSystemExit();
         doTest(true);
-        assertTrue(Wait.waitFor(new Wait.Condition() {
-            @Override
-            public boolean isSatisified() throws Exception {
-                return ((NoExitSecurityManager)System.getSecurityManager()).isCheckExitCalled();
-            }
-        }, 10000));
-
     }
 
-    @Test(timeout=20000)
+    @Test
     public void testHandleWithOutShutdownOnExit() throws Exception {
         doTest(false);
     }
