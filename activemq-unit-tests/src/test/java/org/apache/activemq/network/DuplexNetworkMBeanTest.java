5c8939741ac3dfc5943911e5aa486d32e41bca5f
Altaflux <pabloloz@gmail.com>
2016-01-04 15:00:00 -0600

    Network of brokers on duplex mode reports InstanceAlreadyExistsException on already existing destinations
    (cherry picked from commit 6b1e87410da4a2033c286fcaa758371e48da62ec)
    (cherry picked from commit aa8b64420be5734a8b70736dab4d037bf84af927)

--- activemq-unit-tests/src/test/java/org/apache/activemq/network/DuplexNetworkMBeanTest.java
+++ activemq-unit-tests/src/test/java/org/apache/activemq/network/DuplexNetworkMBeanTest.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,22 +16,27 @@
  */
 package org.apache.activemq.network;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assume.assumeNotNull;
-
-import java.net.MalformedURLException;
-import java.util.List;
-import java.util.Set;
-
-import javax.management.ObjectInstance;
-import javax.management.ObjectName;
-
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.broker.BrokerService;
 import org.apache.activemq.util.TestUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.jms.Connection;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.management.MBeanServer;
+import javax.management.ObjectInstance;
+import javax.management.ObjectName;
+import java.net.MalformedURLException;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assume.assumeNotNull;
 
 public class DuplexNetworkMBeanTest {
 
@@ -131,6 +136,74 @@ public class DuplexNetworkMBeanTest {
         } finally {
             networkedBroker.stop();
             networkedBroker.waitUntilStopped();
+        }
+    }
+
+    @Test
+    public void testMBeansNotOverwrittenOnCleanup() throws Exception {
+        BrokerService broker = createBroker();
+
+        BrokerService networkedBroker = createNetworkedBroker();
+        MessageProducer producerBroker = null;
+        MessageConsumer consumerBroker = null;
+        Session sessionNetworkBroker = null;
+        Session sessionBroker = null;
+        MessageProducer producerNetworkBroker = null;
+        MessageConsumer consumerNetworkBroker = null;
+        try {
+            broker.start();
+            broker.waitUntilStarted();
+            networkedBroker.start();
+            try {
+                assertEquals(2, countMbeans(networkedBroker, "connector=networkConnectors", 10000));
+                assertEquals(1, countMbeans(broker, "connector=duplexNetworkConnectors", 10000));
+
+                Connection brokerConnection = new ActiveMQConnectionFactory(broker.getVmConnectorURI()).createConnection();
+                brokerConnection.start();
+
+                sessionBroker = brokerConnection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+                producerBroker = sessionBroker.createProducer(sessionBroker.createTopic("testTopic"));
+                consumerBroker = sessionBroker.createConsumer(sessionBroker.createTopic("testTopic"));
+                Connection netWorkBrokerConnection = new ActiveMQConnectionFactory(networkedBroker.getVmConnectorURI()).createConnection();
+                netWorkBrokerConnection.start();
+                sessionNetworkBroker = netWorkBrokerConnection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+                producerNetworkBroker = sessionNetworkBroker.createProducer(sessionBroker.createTopic("testTopic"));
+                consumerNetworkBroker = sessionNetworkBroker.createConsumer(sessionBroker.createTopic("testTopic"));
+
+                assertEquals(4, countMbeans(broker, "destinationType=Topic,destinationName=testTopic", 15000));
+                assertEquals(4, countMbeans(networkedBroker, "destinationType=Topic,destinationName=testTopic", 15000));
+
+                producerBroker.send(sessionBroker.createTextMessage("test1"));
+                producerNetworkBroker.send(sessionNetworkBroker.createTextMessage("test2"));
+
+                assertEquals(2, countMbeans(networkedBroker, "destinationName=testTopic,direction=*", 10000));
+                assertEquals(2, countMbeans(broker, "destinationName=testTopic,direction=*", 10000));
+            } finally {
+                if (producerBroker != null) {
+                    producerBroker.close();
+                }
+                if (consumerBroker != null) {
+                    consumerBroker.close();
+                }
+                if (sessionBroker != null) {
+                    sessionBroker.close();
+                }
+                if (sessionNetworkBroker != null) {
+                    sessionNetworkBroker.close();
+                }
+                if (producerNetworkBroker != null) {
+                    producerNetworkBroker.close();
+                }
+                if (consumerNetworkBroker != null) {
+                    consumerNetworkBroker.close();
+                }
+                networkedBroker.stop();
+                networkedBroker.waitUntilStopped();
+            }
+            assertEquals(0, countMbeans(broker, "destinationName=testTopic,direction=*", 1500));
+        } finally {
+            broker.stop();
+            broker.waitUntilStopped();
         }
     }
 
