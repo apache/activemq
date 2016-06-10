3c7d3d395fb6cc19cc560c806da9bedd33e54a00
Jeroen Bastijns <jeroen.bastijns@aca-it.be>
2016-06-10 08:42:17 +0200

    AMQ-6317: fix formatting

--- activemq-jdbc-store/src/main/java/org/apache/activemq/store/jdbc/adapter/DefaultJDBCAdapter.java
+++ activemq-jdbc-store/src/main/java/org/apache/activemq/store/jdbc/adapter/DefaultJDBCAdapter.java
@@ -104,32 +104,31 @@ public class DefaultJDBCAdapter implements JDBCAdapter {
         boolean alreadyExists = false;
         ResultSet rs = null;
         try {
-			rs = transactionContext.getConnection().getMetaData().getTables(null, null, this.statements.getFullMessageTableName(),
-					new String[] { "TABLE" });
-			alreadyExists = rs.next();
-		} catch (Throwable ignore) {
-		} finally {
-			close(rs);
-		}
+            rs = transactionContext.getConnection().getMetaData().getTables(null, null, this.statements.getFullMessageTableName(), new String[] { "TABLE" });
+            alreadyExists = rs.next();
+        } catch (Throwable ignore) {
+        } finally {
+            close(rs);
+        }
         return alreadyExists;
     }
 
     private void executeStatement(TransactionContext transactionContext, String createStatement, boolean ignoreStatementExecutionFailure) throws IOException {
         Statement statement = null;
         try {
-			LOG.debug("Executing SQL: " + createStatement);
-			statement = transactionContext.getConnection().createStatement();
-			statement.execute(createStatement);
+            LOG.debug("Executing SQL: " + createStatement);
+            statement = transactionContext.getConnection().createStatement();
+            statement.execute(createStatement);
 
-			commitIfAutoCommitIsDisabled(transactionContext);
-		} catch (SQLException e) {
-			if (ignoreStatementExecutionFailure) {
-				LOG.debug("Could not create JDBC tables; The message table already existed. " + String.format(FAILURE_MESSAGE, createStatement, e.getMessage(), e.getSQLState(), e.getErrorCode()));
-			} else {
-				LOG.warn("Could not create JDBC tables; they could already exist. " + String.format(FAILURE_MESSAGE, createStatement, e.getMessage(), e.getSQLState(), e.getErrorCode()));
-				JDBCPersistenceAdapter.log("Failure details: ", e);
-			}
-		} finally {
+            commitIfAutoCommitIsDisabled(transactionContext);
+        } catch (SQLException e) {
+            if (ignoreStatementExecutionFailure) {
+                LOG.debug("Could not create JDBC tables; The message table already existed. " + String.format(FAILURE_MESSAGE, createStatement, e.getMessage(), e.getSQLState(), e.getErrorCode()));
+            } else {
+                LOG.warn("Could not create JDBC tables; they could already exist. " + String.format(FAILURE_MESSAGE, createStatement, e.getMessage(), e.getSQLState(), e.getErrorCode()));
+                JDBCPersistenceAdapter.log("Failure details: ", e);
+            }
+        } finally {
             closeStatement(statement);
         }
     }
@@ -144,8 +143,8 @@ public class DefaultJDBCAdapter implements JDBCAdapter {
 
     private void commitIfAutoCommitIsDisabled(TransactionContext c) throws SQLException, IOException {
         if (!c.getConnection().getAutoCommit()) {
-			c.getConnection().commit();
-		}
+            c.getConnection().commit();
+        }
     }
 
     @Override
