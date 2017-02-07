0ee4f5b84302daf8901363556d038e274c2defd5
gtully <gary.tully@gmail.com>
2017-02-07 13:53:40 +0000

    [AMQ-6587] additional contention window with new sub while gc is in progress - need to verify new sub is actually subscribed to the candidate for deletion. Fix and test

--- activemq-unit-tests/src/test/java/org/apache/activemq/broker/region/DestinationGCStressTest.java
+++ activemq-unit-tests/src/test/java/org/apache/activemq/broker/region/DestinationGCStressTest.java
@@ -148,4 +148,82 @@ public class DestinationGCStressTest {
         assertFalse("failed on unexpected log event", failed.get());
 
     }
+
+    @Test(timeout = 60000)
+    public void testAddRemoveWildcardWithGc() throws Exception {
+
+        org.apache.log4j.Logger log4jLogger =
+                org.apache.log4j.Logger.getLogger(RegionBroker.class);
+        final AtomicBoolean failed = new AtomicBoolean(false);
+
+        Appender appender = new DefaultTestAppender() {
+            @Override
+            public void doAppend(LoggingEvent event) {
+                if (event.getLevel().equals(Level.ERROR) && event.getMessage().toString().startsWith("Failed to remove inactive")) {
+                    logger.info("received unexpected log message: " + event.getMessage());
+                    failed.set(true);
+                }
+            }
+        };
+        log4jLogger.addAppender(appender);
+        try {
+
+            final AtomicInteger max = new AtomicInteger(10000);
+
+            final ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory("vm://localhost?create=false");
+            factory.setWatchTopicAdvisories(false);
+            Connection connection = factory.createConnection();
+            connection.start();
+            final Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+            ExecutorService executorService = Executors.newCachedThreadPool();
+            for (int i = 0; i < 1; i++) {
+                executorService.submit(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Connection c = factory.createConnection();
+                            c.start();
+                            Session s = c.createSession(false, Session.AUTO_ACKNOWLEDGE);
+                            MessageProducer producer = s.createProducer(null);
+                            Message message = s.createTextMessage();
+                            int j;
+                            while ((j = max.decrementAndGet()) > 0) {
+                                producer.send(new ActiveMQTopic("A." + j), message);
+                            }
+                        } catch (Exception ignored) {
+                            ignored.printStackTrace();
+                        }
+                    }
+                });
+            }
+
+            executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < 100; i++) {
+                        try {
+                            MessageConsumer messageConsumer = session.createConsumer(new ActiveMQTopic(">"));
+                            messageConsumer.close();
+
+                        } catch (Exception ignored) {
+                            ignored.printStackTrace();
+                        }
+                    }
+                }
+            });
+
+            executorService.shutdown();
+            executorService.awaitTermination(60, TimeUnit.SECONDS);
+
+            logger.info("Done");
+
+            connection.close();
+
+        } finally {
+            log4jLogger.removeAppender(appender);
+        }
+        assertFalse("failed on unexpected log event", failed.get());
+
+    }
 }
