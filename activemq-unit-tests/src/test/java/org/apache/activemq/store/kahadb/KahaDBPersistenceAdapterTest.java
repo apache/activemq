a359d8152cfee6f2fe95d34fd1b2296f6ed2670c
gtully <gary.tully@gmail.com>
2017-09-15 13:48:03 +0100

    [AMQ-6277] take account of producer audit not being updatated on recovery check, avoid unnecessary partial journal replay

--- activemq-unit-tests/src/test/java/org/apache/activemq/store/kahadb/KahaDBPersistenceAdapterTest.java
+++ activemq-unit-tests/src/test/java/org/apache/activemq/store/kahadb/KahaDBPersistenceAdapterTest.java
@@ -16,11 +16,18 @@
  */
 package org.apache.activemq.store.kahadb;
 
-import java.io.File;
-import java.io.IOException;
-
+import org.apache.activemq.broker.BrokerService;
 import org.apache.activemq.store.PersistenceAdapter;
 import org.apache.activemq.store.PersistenceAdapterTestSupport;
+import org.apache.activemq.util.DefaultTestAppender;
+import org.apache.log4j.Appender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggingEvent;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * 
@@ -35,5 +42,41 @@ public class KahaDBPersistenceAdapterTest extends PersistenceAdapterTestSupport 
             kaha.deleteAllMessages();
         }
         return kaha;
+    }
+
+    public void testNoReplayOnStop() throws Exception {
+        brokerService.getPersistenceAdapter().checkpoint(true);
+        brokerService.stop();
+
+        final AtomicBoolean gotSomeReplay = new AtomicBoolean(Boolean.FALSE);
+        final AtomicBoolean trappedLogMessages = new AtomicBoolean(Boolean.FALSE);
+
+        Appender appender = new DefaultTestAppender() {
+            @Override
+            public void doAppend(LoggingEvent event) {
+                trappedLogMessages.set(true);
+                if (event.getLevel().equals(Level.INFO)) {
+                    if (event.getMessage().toString().contains("Recovery replayed ")) {
+                        gotSomeReplay.set(true);
+                    }
+                }
+            }
+        };
+
+        try {
+            Logger.getLogger(MessageDatabase.class.getName()).addAppender(appender);
+            Logger.getLogger(MessageDatabase.class.getName()).setLevel(Level.INFO);
+
+            brokerService = new BrokerService();
+            pa = createPersistenceAdapter(false);
+            brokerService.setPersistenceAdapter(pa);
+            brokerService.start();
+
+        } finally {
+            Logger.getRootLogger().removeAppender(appender);
+            Logger.getLogger(MessageDatabase.class.getName()).removeAppender(appender);
+        }
+        assertTrue("log capture working", trappedLogMessages.get());
+        assertFalse("no replay message in the log", gotSomeReplay.get());
     }
 }
