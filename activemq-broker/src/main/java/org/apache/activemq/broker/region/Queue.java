185160c0daf955bf7ef43a9440cf5bc9672bd461
gtully <gary.tully@gmail.com>
2017-11-01 11:26:36 +0000

    [AMQ-6847] limit the retry loop to one iteration overa all pending messages such that new additions are not replayed to avoid duplicates
    (cherry picked from commit 03b19b9da4d50c3bb8985f930e93596c7d994d26)

--- activemq-broker/src/main/java/org/apache/activemq/broker/region/Queue.java
+++ activemq-broker/src/main/java/org/apache/activemq/broker/region/Queue.java
@@ -1443,8 +1443,8 @@ public class Queue extends BaseDestination implements Task, UsageListener, Index
             try {
                 messages.rollback(m.getMessageId());
                 if (isDLQ()) {
-                    DeadLetterStrategy stratagy = getDeadLetterStrategy();
-                    stratagy.rollback(m.getMessage());
+                    DeadLetterStrategy strategy = getDeadLetterStrategy();
+                    strategy.rollback(m.getMessage());
                 }
             } finally {
                 messagesLock.writeLock().unlock();
@@ -1523,6 +1523,9 @@ public class Queue extends BaseDestination implements Task, UsageListener, Index
             throw new Exception("Retry of message is only possible on Dead Letter Queues!");
         }
         int restoredCounter = 0;
+        // ensure we deal with a snapshot to avoid potential duplicates in the event of messages
+        // getting immediate dlq'ed
+        long numberOfRetryAttemptsToCheckAllMessagesOnce = this.destinationStatistics.getMessages().getCount();
         Set<MessageReference> set = new LinkedHashSet<MessageReference>();
         do {
             doPageIn(true);
@@ -1534,6 +1537,7 @@ public class Queue extends BaseDestination implements Task, UsageListener, Index
             }
             List<MessageReference> list = new ArrayList<MessageReference>(set);
             for (MessageReference ref : list) {
+                numberOfRetryAttemptsToCheckAllMessagesOnce--;
                 if (ref.getMessage().getOriginalDestination() != null) {
 
                     moveMessageTo(context, (QueueMessageReference)ref, ref.getMessage().getOriginalDestination());
@@ -1543,7 +1547,7 @@ public class Queue extends BaseDestination implements Task, UsageListener, Index
                     }
                 }
             }
-        } while (set.size() < this.destinationStatistics.getMessages().getCount() && set.size() < maximumMessages);
+        } while (numberOfRetryAttemptsToCheckAllMessagesOnce > 0 && set.size() < this.destinationStatistics.getMessages().getCount());
         return restoredCounter;
     }
 
