86fbf957e9ee4a005da838cd7306292ab975e745
Christopher L. Shannon (cshannon) <christopher.l.shannon@gmail.com>
2018-04-18 12:35:01 -0400

    Revert "AMQ-6947 - Update Queue metrics on expiration"
    This reverts commit bca0af4133a6e226783f8e953f1abe7d4e165743.

--- activemq-broker/src/main/java/org/apache/activemq/broker/region/Queue.java
+++ activemq-broker/src/main/java/org/apache/activemq/broker/region/Queue.java
@@ -1842,17 +1842,13 @@ public class Queue extends BaseDestination implements Task, UsageListener, Index
             pagedInMessagesLock.writeLock().lock();
             try {
                 if (pagedInMessages.remove(reference) != null) {
-                    updateMetricsOnMessageDrop();
+                    getDestinationStatistics().getDequeues().increment();
+                    getDestinationStatistics().getMessages().decrement();
                 }
             } finally {
                 pagedInMessagesLock.writeLock().unlock();
             }
         }
-    }
-
-    private void updateMetricsOnMessageDrop() {
-        getDestinationStatistics().getDequeues().increment();
-        getDestinationStatistics().getMessages().decrement();
     }
 
     public void messageExpired(ConnectionContext context, MessageReference reference) {
@@ -2011,11 +2007,6 @@ public class Queue extends BaseDestination implements Task, UsageListener, Index
                         if (processExpired && ref.isExpired()) {
                             if (broker.isExpired(ref)) {
                                 messageExpired(createConnectionContext(), ref);
-
-                                //We need to update the metrics here because the drop message
-                                //method will only update if the message was removed from the
-                                //pagedInMessages list which won't happen in this case
-                                updateMetricsOnMessageDrop();
                             } else {
                                 ref.decrementReferenceCount();
                             }
