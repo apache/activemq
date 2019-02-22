55983905bf89b556b3ec177b1580f0f96540e82d
Jonathan Gallimore <jon@jrg.me.uk>
2019-02-22 16:23:58 +0000

    Remove dependency activemq-http and activemq-stomp

--- activemq-client/src/main/java/org/apache/activemq/util/XStreamSupport.java
+++ activemq-client/src/main/java/org/apache/activemq/util/XStreamSupport.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.activemq.transport.stomp;
+package org.apache.activemq.util;
 
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.security.AnyTypePermission;
