18dee6139a34e4ad21c219428346934a56f2be8a
Pascal Schumacher <pascalschumacher@gmx.net>
2020-03-21 14:44:41 +0100

    Remove unused import in publisher.py.
    (cherry picked from commit 8cf12550bd0349ee05dc9103ef28608a550864bc)

--- assembly/src/release/examples/stomp/python/stomppy/publisher.py
+++ assembly/src/release/examples/stomp/python/stomppy/publisher.py
@@ -15,8 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ------------------------------------------------------------------------
- 
-import time
+
 import sys
 import os
 import stomp
