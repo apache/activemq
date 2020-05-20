48c6a370946df2bff09407580ec3db4ca6391da9
gtully <gary.tully@gmail.com>
2020-05-20 15:15:48 +0100

    [AMQ-7394] use canRecoveryNextMessage to base blocking on shared usage, the existing fix regressed AMQ5266Test

--- activemq-jdbc-store/src/main/java/org/apache/activemq/store/jdbc/JDBCMessageStore.java
+++ activemq-jdbc-store/src/main/java/org/apache/activemq/store/jdbc/JDBCMessageStore.java
@@ -360,7 +360,7 @@ public class JDBCMessageStore extends AbstractMessageStore {
 
                 @Override
                 public boolean recoverMessage(long sequenceId, byte[] data) throws Exception {
-                    if (listener.hasSpace()) {
+                    if (listener.canRecoveryNextMessage()) {
                         Message msg = (Message) wireFormat.unmarshal(new ByteSequence(data));
                         msg.getMessageId().setBrokerSequenceId(sequenceId);
                         msg.getMessageId().setFutureOrSequenceLong(sequenceId);
