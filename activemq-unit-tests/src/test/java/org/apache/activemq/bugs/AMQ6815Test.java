c9e4abf2745bc22f56d6c76366e5b90a011db66f
jbonofre <jbonofre@apache.org>
2020-11-19 08:43:50 +0100

    Give more time to AMQ6815Test (for Jenkins)

--- activemq-unit-tests/src/test/java/org/apache/activemq/bugs/AMQ6815Test.java
+++ activemq-unit-tests/src/test/java/org/apache/activemq/bugs/AMQ6815Test.java
@@ -71,7 +71,7 @@ public class AMQ6815Test {
          brokerService.stop();
       }
 
-      @Test(timeout = 120000)
+      @Test(timeout = 240000)
       public void testHeapUsage() throws Exception {
          Runtime.getRuntime().gc();
          final long initUsedMemory = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
