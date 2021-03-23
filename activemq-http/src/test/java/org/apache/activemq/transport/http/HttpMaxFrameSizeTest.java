1e2a0de5ccbff057ac0201b335ec30dc266eb0b5
jbonofre <jbonofre@apache.org>
2021-03-23 16:07:02 +0100

    [AMQ-8194] Upgrade to commons-lang3 3.12.0

--- activemq-http/src/test/java/org/apache/activemq/transport/http/HttpMaxFrameSizeTest.java
+++ activemq-http/src/test/java/org/apache/activemq/transport/http/HttpMaxFrameSizeTest.java
@@ -19,7 +19,7 @@ package org.apache.activemq.transport.http;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.broker.BrokerService;
 import org.apache.activemq.command.ActiveMQQueue;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
