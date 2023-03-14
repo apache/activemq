355a12e28dcdc3467e50865eb1ce1550b33fc77e
JB Onofré <jbonofre@apache.org>
2023-03-14 11:14:49 +0100

    Update activemq-broker/src/main/java/org/apache/activemq/broker/BrokerService.java
    Co-authored-by: Christopher L. Shannon <christopher.l.shannon@gmail.com>

--- activemq-broker/src/main/java/org/apache/activemq/broker/BrokerService.java
+++ activemq-broker/src/main/java/org/apache/activemq/broker/BrokerService.java
@@ -2947,7 +2947,7 @@ public class BrokerService implements Service {
     }
 
     /**
-     * Deprecated: this method will be renamed to not use slave wording
+     * @deprecated this method will be renamed to not use slave wording
      */
     @Deprecated(forRemoval = true)
     public void setWaitForSlaveTimeout(long waitForSlaveTimeout) {
