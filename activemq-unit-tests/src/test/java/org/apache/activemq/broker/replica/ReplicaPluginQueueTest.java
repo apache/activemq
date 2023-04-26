0c52cfb7bdf3bf15c5c02b7568222a4ac8d41260
Nikita Shupletsov <nshuplet@amazon.com>
2023-04-26 15:09:17 -0700

    [AMQ-8354] plugin test: queue operations

--- activemq-unit-tests/src/test/java/org/apache/activemq/broker/replica/ReplicaPluginQueueTest.java
+++ activemq-unit-tests/src/test/java/org/apache/activemq/broker/replica/ReplicaPluginQueueTest.java
@@ -17,17 +17,21 @@
 package org.apache.activemq.broker.replica;
 
 import org.apache.activemq.ScheduledMessage;
+import org.apache.activemq.broker.jmx.BrokerViewMBean;
 import org.apache.activemq.broker.jmx.QueueViewMBean;
 import org.apache.activemq.command.ActiveMQQueue;
 import org.apache.activemq.command.ActiveMQTextMessage;
 import org.apache.activemq.command.ActiveMQTopic;
+import org.junit.Ignore;
 
 import javax.jms.Connection;
+import javax.jms.Destination;
 import javax.jms.Message;
 import javax.jms.MessageConsumer;
 import javax.jms.MessageProducer;
 import javax.jms.Queue;
 import javax.jms.Session;
+import javax.jms.TemporaryQueue;
 import javax.jms.TextMessage;
 import javax.jms.Topic;
 import javax.jms.XAConnection;
@@ -38,6 +42,8 @@ import javax.management.MalformedObjectNameException;
 import javax.management.ObjectName;
 import javax.transaction.xa.XAResource;
 import javax.transaction.xa.Xid;
+import java.util.Arrays;
+import java.util.UUID;
 
 public class ReplicaPluginQueueTest extends ReplicaPluginTestSupport {
 
@@ -424,6 +430,144 @@ public class ReplicaPluginQueueTest extends ReplicaPluginTestSupport {
 
         firstBrokerSession.close();
         secondBrokerSession.close();
+    }
+
+    @Ignore ("Skipped because Pause Queue event is not replicated")
+    public void pauseQueueAndResume() throws Exception {
+
+        Session firstBrokerSession = firstBrokerConnection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+        MessageProducer firstBrokerProducer = firstBrokerSession.createProducer(destination);
+
+        Session secondBrokerSession = secondBrokerConnection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+        MessageConsumer secondBrokerConsumer = secondBrokerSession.createConsumer(destination);
+
+        ActiveMQTextMessage message  = new ActiveMQTextMessage();
+        message.setText(getName());
+        firstBrokerProducer.send(message);
+        Thread.sleep(LONG_TIMEOUT);
+
+        QueueViewMBean firstBrokerDestinationQueueView = getQueueView(firstBroker, destination.getPhysicalName());
+        firstBrokerDestinationQueueView.pause();
+        assertTrue(firstBrokerDestinationQueueView.isPaused());
+        Thread.sleep(LONG_TIMEOUT);
+
+        QueueViewMBean secondBrokerDestinationQueueView = getQueueView(secondBroker, destination.getPhysicalName());
+        assertTrue(secondBrokerDestinationQueueView.isPaused());
+
+        Message receivedMessage = secondBrokerConsumer.receive(LONG_TIMEOUT);
+        assertNull(receivedMessage);
+
+        firstBrokerDestinationQueueView.resume();
+        Thread.sleep(LONG_TIMEOUT);
+
+        receivedMessage = secondBrokerConsumer.receive(LONG_TIMEOUT);
+        assertNotNull(receivedMessage);
+        assertTrue(receivedMessage instanceof TextMessage);
+        assertEquals(getName(), ((TextMessage) receivedMessage).getText());
+
+        firstBrokerSession.close();
+        secondBrokerSession.close();
+    }
+
+    public void testBrowseMessage() throws Exception {
+        Session firstBrokerSession = firstBrokerConnection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+        MessageProducer firstBrokerProducer = firstBrokerSession.createProducer(destination);
+        MessageConsumer firstBrokerConsumer = firstBrokerSession.createConsumer(destination);
+
+        ActiveMQTextMessage message  = new ActiveMQTextMessage();
+        message.setText(getName());
+        firstBrokerProducer.send(message);
+        Thread.sleep(LONG_TIMEOUT);
+
+        QueueViewMBean secondBrokerDestinationQueueView = getQueueView(secondBroker, destination.getPhysicalName());
+        assertEquals(secondBrokerDestinationQueueView.browseMessages().size(), 1);
+        TextMessage destinationMessage = (TextMessage) secondBrokerDestinationQueueView.browseMessages().get(0);
+        assertEquals(destinationMessage.getText(), getName());
+
+        assertEquals(secondBrokerDestinationQueueView.getProducerCount(), 0);
+        assertEquals(secondBrokerDestinationQueueView.getConsumerCount(), 0);
+
+        Message receivedMessage = firstBrokerConsumer.receive(SHORT_TIMEOUT);
+        assertNotNull(receivedMessage);
+        receivedMessage.acknowledge();
+        Thread.sleep(LONG_TIMEOUT);
+        assertEquals(secondBrokerDestinationQueueView.getDequeueCount(), 1);
+        firstBrokerSession.close();
+    }
+
+    public void testDeleteMessage() throws Exception {
+        Session firstBrokerSession = firstBrokerConnection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+        MessageProducer firstBrokerProducer = firstBrokerSession.createProducer(destination);
+        ActiveMQTextMessage message  = new ActiveMQTextMessage();
+        message.setText(getName());
+        firstBrokerProducer.send(message);
+        Thread.sleep(LONG_TIMEOUT);
+
+        MBeanServer secondBrokerMbeanServer = secondBroker.getManagementContext().getMBeanServer();
+        ObjectName secondBrokerViewMBeanName = assertRegisteredObjectName(secondBrokerMbeanServer, secondBroker.getBrokerObjectName().toString());
+        BrokerViewMBean secondBrokerMBean = MBeanServerInvocationHandler.newProxyInstance(secondBrokerMbeanServer, secondBrokerViewMBeanName, BrokerViewMBean.class, true);
+        assertEquals(secondBrokerMBean.getQueues().length, 2);
+        assertEquals(Arrays.stream(secondBrokerMBean.getQueues())
+                .map(ObjectName::toString)
+                .filter(name -> name.contains(destination.getPhysicalName()))
+                .count(), 1);
+
+        MBeanServer firstBrokerMbeanServer = firstBroker.getManagementContext().getMBeanServer();
+        ObjectName firstBrokerViewMBeanName = assertRegisteredObjectName(firstBrokerMbeanServer, firstBroker.getBrokerObjectName().toString());
+        BrokerViewMBean firstBrokerMBean = MBeanServerInvocationHandler.newProxyInstance(firstBrokerMbeanServer, firstBrokerViewMBeanName, BrokerViewMBean.class, true);
+        firstBrokerMBean.removeQueue(destination.getPhysicalName());
+        Thread.sleep(LONG_TIMEOUT);
+
+        assertEquals(secondBrokerMBean.getQueues().length, 1);
+        assertEquals(Arrays.stream(secondBrokerMBean.getQueues())
+                .map(ObjectName::toString)
+                .filter(name -> name.contains(destination.getPhysicalName()))
+                .count(), 0);
+
+        firstBrokerSession.close();
+    }
+
+    public void testTemporaryQueueIsNotReplicated() throws Exception {
+        Session firstBrokerSession = firstBrokerConnection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+        MessageProducer firstBrokerProducer = firstBrokerSession.createProducer(destination);
+        TemporaryQueue tempQueue = firstBrokerSession.createTemporaryQueue();
+
+        TextMessage message  = firstBrokerSession.createTextMessage(getName());
+        String id = UUID.randomUUID().toString();
+        message.setJMSReplyTo(tempQueue);
+        message.setJMSCorrelationID(id);
+        firstBrokerProducer.send(message);
+        Thread.sleep(LONG_TIMEOUT);
+
+        MessageConsumer firstBrokerDestinationConsumer = firstBrokerSession.createConsumer(destination);
+        Message firstBrokerMessageDestinationReceived = firstBrokerDestinationConsumer.receive(LONG_TIMEOUT);
+        if (firstBrokerMessageDestinationReceived instanceof TextMessage) {
+            TextMessage textMessage = (TextMessage) firstBrokerMessageDestinationReceived;
+            Destination replyBackQueue = textMessage.getJMSReplyTo();
+            MessageProducer producer = firstBrokerSession.createProducer(replyBackQueue);
+
+            TextMessage msg = firstBrokerSession.createTextMessage("Message Received : " + textMessage.getText());
+            producer.send(msg);
+        }
+
+        MessageConsumer firstBrokerTempQueueConsumer = firstBrokerSession.createConsumer(tempQueue);
+        Message firstBrokerMessageReceived = firstBrokerTempQueueConsumer.receive(LONG_TIMEOUT);
+        assertNotNull(firstBrokerMessageReceived);
+        assertTrue(((TextMessage) firstBrokerMessageReceived).getText().contains(getName()));
+
+        String tempQueueJMXName = tempQueue.getQueueName().replaceAll(":", "_");
+        MBeanServer firstBrokerMbeanServer = firstBroker.getManagementContext().getMBeanServer();
+        ObjectName firstBrokerViewMBeanName = assertRegisteredObjectName(firstBrokerMbeanServer, firstBroker.getBrokerObjectName().toString());
+        BrokerViewMBean firstBrokerMBean = MBeanServerInvocationHandler.newProxyInstance(firstBrokerMbeanServer, firstBrokerViewMBeanName, BrokerViewMBean.class, true);
+        assertEquals(firstBrokerMBean.getTemporaryQueues().length, 1);
+        assertTrue(firstBrokerMBean.getTemporaryQueues()[0].toString().contains(tempQueueJMXName));
+
+        MBeanServer secondBrokerMbeanServer = secondBroker.getManagementContext().getMBeanServer();
+        ObjectName secondBrokerViewMBeanName = assertRegisteredObjectName(secondBrokerMbeanServer, secondBroker.getBrokerObjectName().toString());
+        BrokerViewMBean secondBrokerMBean = MBeanServerInvocationHandler.newProxyInstance(secondBrokerMbeanServer, secondBrokerViewMBeanName, BrokerViewMBean.class, true);
+        assertEquals(secondBrokerMBean.getTemporaryQueues().length, 0);
+
+        firstBrokerSession.close();
     }
 
     private ObjectName assertRegisteredObjectName(MBeanServer mbeanServer, String name) throws MalformedObjectNameException, NullPointerException {
