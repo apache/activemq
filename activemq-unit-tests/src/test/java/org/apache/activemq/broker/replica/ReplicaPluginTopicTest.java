0fc8143326fe12bab6868dcb5646a0b6173a7efe
Nikita Shupletsov <nshuplet@amazon.com>
2023-04-26 15:10:16 -0700

    [AMQ-8354] plugin test: Topic operations

--- activemq-unit-tests/src/test/java/org/apache/activemq/broker/replica/ReplicaPluginTopicTest.java
+++ activemq-unit-tests/src/test/java/org/apache/activemq/broker/replica/ReplicaPluginTopicTest.java
@@ -17,19 +17,30 @@
 package org.apache.activemq.broker.replica;
 
 import org.apache.activemq.ScheduledMessage;
+import org.apache.activemq.broker.BrokerService;
+import org.apache.activemq.broker.jmx.BrokerViewMBean;
+import org.apache.activemq.broker.jmx.TopicViewMBean;
 import org.apache.activemq.command.ActiveMQTextMessage;
 
 import javax.jms.Connection;
+import javax.jms.Destination;
 import javax.jms.Message;
 import javax.jms.MessageConsumer;
 import javax.jms.MessageProducer;
 import javax.jms.Session;
+import javax.jms.TemporaryTopic;
 import javax.jms.TextMessage;
 import javax.jms.Topic;
 import javax.jms.XAConnection;
 import javax.jms.XASession;
+import javax.management.MBeanServer;
+import javax.management.MBeanServerInvocationHandler;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
 import javax.transaction.xa.XAResource;
 import javax.transaction.xa.Xid;
+import java.util.Arrays;
+import java.util.UUID;
 
 public class ReplicaPluginTopicTest extends ReplicaPluginTestSupport {
 
@@ -410,4 +421,160 @@ public class ReplicaPluginTopicTest extends ReplicaPluginTestSupport {
         firstBrokerSession.close();
         secondBrokerSession.close();
     }
+
+    public void testBrowseMessage() throws Exception {
+        Session firstBrokerSession = firstBrokerConnection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+        MessageProducer firstBrokerProducer = firstBrokerSession.createProducer(destination);
+        MessageConsumer firstBrokerConsumer = firstBrokerSession.createDurableSubscriber((Topic) destination, CLIENT_ID_XA);
+
+        ActiveMQTextMessage message  = new ActiveMQTextMessage();
+        message.setText(getName());
+        firstBrokerProducer.send(message);
+        Thread.sleep(LONG_TIMEOUT);
+
+        TopicViewMBean secondBrokerDestinationTopicView = getTopicView(secondBroker, destination.getPhysicalName());
+        assertEquals(secondBrokerDestinationTopicView.browseMessages().size(), 1);
+        TextMessage destinationMessage = (TextMessage) secondBrokerDestinationTopicView.browseMessages().get(0);
+        assertEquals(destinationMessage.getText(), getName());
+
+        assertEquals(secondBrokerDestinationTopicView.getProducerCount(), 0);
+        assertEquals(secondBrokerDestinationTopicView.getConsumerCount(), 1);
+
+        Message receivedMessage = firstBrokerConsumer.receive(SHORT_TIMEOUT);
+        assertNotNull(receivedMessage);
+        receivedMessage.acknowledge();
+        Thread.sleep(LONG_TIMEOUT);
+        assertEquals(secondBrokerDestinationTopicView.getDequeueCount(), 1);
+        firstBrokerSession.close();
+    }
+
+    public void testDeleteTopic() throws Exception {
+        Session firstBrokerSession = firstBrokerConnection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+        MessageProducer firstBrokerProducer = firstBrokerSession.createProducer(destination);
+        MessageConsumer firstBrokerConsumer = firstBrokerSession.createDurableSubscriber((Topic) destination, CLIENT_ID_XA);
+
+        ActiveMQTextMessage message  = new ActiveMQTextMessage();
+        message.setText(getName());
+        firstBrokerProducer.send(message);
+        Thread.sleep(LONG_TIMEOUT);
+
+        MBeanServer secondBrokerMbeanServer = secondBroker.getManagementContext().getMBeanServer();
+        ObjectName secondBrokerViewMBeanName = assertRegisteredObjectName(secondBrokerMbeanServer, secondBroker.getBrokerObjectName().toString());
+        BrokerViewMBean secondBrokerMBean = MBeanServerInvocationHandler.newProxyInstance(secondBrokerMbeanServer, secondBrokerViewMBeanName, BrokerViewMBean.class, true);
+        assertEquals(Arrays.stream(secondBrokerMBean.getTopics())
+            .map(ObjectName::toString)
+            .peek(name -> System.out.println("topic name: " + name))
+            .filter(name -> name.contains("destinationName=" + destination.getPhysicalName()))
+            .count(), 1);
+
+        MBeanServer firstBrokerMbeanServer = firstBroker.getManagementContext().getMBeanServer();
+        ObjectName firstBrokerViewMBeanName = assertRegisteredObjectName(firstBrokerMbeanServer, firstBroker.getBrokerObjectName().toString());
+        BrokerViewMBean firstBrokerMBean = MBeanServerInvocationHandler.newProxyInstance(firstBrokerMbeanServer, firstBrokerViewMBeanName, BrokerViewMBean.class, true);
+        firstBrokerMBean.removeTopic(destination.getPhysicalName());
+        Thread.sleep(LONG_TIMEOUT);
+
+        assertEquals(Arrays.stream(secondBrokerMBean.getQueues())
+            .map(ObjectName::toString)
+            .filter(name -> name.contains("destinationName=" + destination.getPhysicalName()))
+            .count(), 0);
+
+        firstBrokerSession.close();
+    }
+
+    public void testDurableSubscribers() throws Exception {
+        Session firstBrokerSession = firstBrokerConnection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+        MessageProducer firstBrokerProducer = firstBrokerSession.createProducer(destination);
+        MessageConsumer firstBrokerConsumerOne = firstBrokerSession.createDurableSubscriber((Topic) destination, CLIENT_ID_ONE);
+        ActiveMQTextMessage message  = new ActiveMQTextMessage();
+        message.setText(getName() + "No. 1");
+        firstBrokerProducer.send(message);
+        Thread.sleep(LONG_TIMEOUT);
+
+        TopicViewMBean secondBrokerDestinationTopicView = getTopicView(secondBroker, destination.getPhysicalName());
+        TopicViewMBean firstBrokerDestinationTopicView = getTopicView(firstBroker, destination.getPhysicalName());
+        assertEquals(firstBrokerDestinationTopicView.getConsumerCount(), 1);
+        assertEquals(secondBrokerDestinationTopicView.getConsumerCount(), 1);
+
+
+        firstBrokerSession.createDurableSubscriber((Topic) destination, CLIENT_ID_TWO);
+        message  = new ActiveMQTextMessage();
+        message.setText(getName() + "No. 2");
+        firstBrokerProducer.send(message);
+        Thread.sleep(LONG_TIMEOUT);
+        assertEquals(secondBrokerDestinationTopicView.getConsumerCount(), 2);
+        assertEquals(firstBrokerDestinationTopicView.getConsumerCount(), 2);
+
+        firstBrokerConsumerOne.close();
+        firstBrokerSession.unsubscribe(CLIENT_ID_ONE);
+        message  = new ActiveMQTextMessage();
+        message.setText(getName() + "No. 3");
+        firstBrokerProducer.send(message);
+        Thread.sleep(LONG_TIMEOUT);
+        assertEquals(firstBrokerDestinationTopicView.getConsumerCount(), 1);
+//        TODO: remove durable subscription is not replicated
+//        assertEquals(secondBrokerDestinationTopicView.getConsumerCount(), 1);
+        firstBrokerSession.close();
+    }
+
+    public void testTemporaryTopicIsNotReplicated() throws Exception {
+        Session firstBrokerSession = firstBrokerConnection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+        MessageProducer firstBrokerProducer = firstBrokerSession.createProducer(destination);
+        MessageConsumer firstBrokerConsumer = firstBrokerSession.createDurableSubscriber((Topic) destination, CLIENT_ID_ONE);
+
+        TemporaryTopic temporaryTopic = firstBrokerSession.createTemporaryTopic();
+        MessageConsumer firstBrokerTempTopicConsumer = firstBrokerSession.createConsumer(temporaryTopic);
+        TextMessage message  = firstBrokerSession.createTextMessage(getName());
+        String id = UUID.randomUUID().toString();
+        message.setJMSReplyTo(temporaryTopic);
+        message.setJMSCorrelationID(id);
+        firstBrokerProducer.send(message);
+        Thread.sleep(LONG_TIMEOUT);
+
+        Message firstBrokerMessageDestinationReceived = firstBrokerConsumer.receive(LONG_TIMEOUT);
+        if (firstBrokerMessageDestinationReceived instanceof TextMessage) {
+            TextMessage textMessage = (TextMessage) firstBrokerMessageDestinationReceived;
+            Destination replyBackTopic = textMessage.getJMSReplyTo();
+            MessageProducer producer = firstBrokerSession.createProducer(replyBackTopic);
+
+            TextMessage msg = firstBrokerSession.createTextMessage("Message Received : " + textMessage.getText());
+            producer.send(msg);
+        }
+
+        Message firstBrokerMessageReceived = firstBrokerTempTopicConsumer.receive(LONG_TIMEOUT);
+        assertNotNull(firstBrokerMessageReceived);
+        assertTrue(((TextMessage) firstBrokerMessageReceived).getText().contains(getName()));
+
+        String tempTopicJMXName = temporaryTopic.getTopicName().replaceAll(":", "_");
+        MBeanServer firstBrokerMbeanServer = firstBroker.getManagementContext().getMBeanServer();
+        ObjectName firstBrokerViewMBeanName = assertRegisteredObjectName(firstBrokerMbeanServer, firstBroker.getBrokerObjectName().toString());
+        BrokerViewMBean firstBrokerMBean = MBeanServerInvocationHandler.newProxyInstance(firstBrokerMbeanServer, firstBrokerViewMBeanName, BrokerViewMBean.class, true);
+        assertEquals(firstBrokerMBean.getTemporaryTopics().length, 1);
+        assertTrue(firstBrokerMBean.getTemporaryTopics()[0].toString().contains(tempTopicJMXName));
+
+        MBeanServer secondBrokerMbeanServer = secondBroker.getManagementContext().getMBeanServer();
+        ObjectName secondBrokerViewMBeanName = assertRegisteredObjectName(secondBrokerMbeanServer, secondBroker.getBrokerObjectName().toString());
+        BrokerViewMBean secondBrokerMBean = MBeanServerInvocationHandler.newProxyInstance(secondBrokerMbeanServer, secondBrokerViewMBeanName, BrokerViewMBean.class, true);
+        assertEquals(secondBrokerMBean.getTemporaryTopics().length, 0);
+
+        firstBrokerSession.close();
+    }
+
+    private TopicViewMBean getTopicView(BrokerService broker, String topicName) throws MalformedObjectNameException {
+        MBeanServer mbeanServer = broker.getManagementContext().getMBeanServer();
+        String objectNameStr = broker.getBrokerObjectName().toString();
+        objectNameStr += ",destinationType=Topic,destinationName="+topicName;
+        ObjectName topicViewMBeanName = assertRegisteredObjectName(mbeanServer, objectNameStr);
+        return MBeanServerInvocationHandler.newProxyInstance(mbeanServer, topicViewMBeanName, TopicViewMBean.class, true);
+    }
+
+    private ObjectName assertRegisteredObjectName(MBeanServer mbeanServer, String name) throws MalformedObjectNameException, NullPointerException {
+        ObjectName objectName = new ObjectName(name);
+        if (mbeanServer.isRegistered(objectName)) {
+            System.out.println("Bean Registered: " + objectName);
+        } else {
+            fail("Could not find MBean!: " + objectName);
+        }
+        return objectName;
+    }
+
 }
