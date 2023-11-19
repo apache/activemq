6b59cdf6ded878a3cc94152071fcc69d5600e6f8
huiyang <huiyang_chi@Outlook.com>
2023-11-18 20:34:32 -0600

    Improve test reliability by resolving nondeterministic order of Json
    (cherry picked from commit 7d6362da73a0b972298a358e1fe48cdef2f938e4)

--- activemq-stomp/src/test/java/org/apache/activemq/transport/stomp/SamplePojo.java
+++ activemq-stomp/src/test/java/org/apache/activemq/transport/stomp/SamplePojo.java
@@ -52,4 +52,16 @@ public class SamplePojo implements Serializable {
     public void setName(String name) {
         this.name = name;
     }
+
+    //implement equals
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof SamplePojo) {
+            SamplePojo other = (SamplePojo) obj;
+            return name.equals(other.name) && city.equals(other.city);
+        }
+        return false;
+    }
+
 }
