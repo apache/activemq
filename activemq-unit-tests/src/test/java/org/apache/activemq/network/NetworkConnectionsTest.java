d7b3027b18f080529c938dea688e638d13abce72
Romain Manni-Bucau <rmannibucau@gmail.com>
2023-12-12 18:25:58 +0100

    s/javax/jakarta/ after rebase

--- activemq-unit-tests/src/test/java/org/apache/activemq/network/NetworkConnectionsTest.java
+++ activemq-unit-tests/src/test/java/org/apache/activemq/network/NetworkConnectionsTest.java
@@ -24,11 +24,11 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.jms.Connection;
-import javax.jms.Message;
-import javax.jms.MessageConsumer;
-import javax.jms.MessageProducer;
-import javax.jms.Session;
+import jakarta.jms.Connection;
+import jakarta.jms.Message;
+import jakarta.jms.MessageConsumer;
+import jakarta.jms.MessageProducer;
+import jakarta.jms.Session;
 
 public class NetworkConnectionsTest extends TestCase {
     private static final Logger LOG = LoggerFactory.getLogger(NetworkConnectionsTest.class);
