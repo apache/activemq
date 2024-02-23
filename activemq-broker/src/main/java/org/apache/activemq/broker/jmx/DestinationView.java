db2e7e4425884aff12f2f7dc92177ec9fd50da92
Matt Pavlovich <mattrpav@apache.org>
2024-02-23 08:51:17 -0600

    [AMQ-9426] Add destination AdvancedStatisticsEnabled flag and networkEnqueue/networkDequeue stats

--- activemq-broker/src/main/java/org/apache/activemq/broker/jmx/DestinationView.java
+++ activemq-broker/src/main/java/org/apache/activemq/broker/jmx/DestinationView.java
@@ -599,4 +599,25 @@ public class DestinationView implements DestinationViewMBean {
     public long getMaxUncommittedExceededCount() {
         return destination.getDestinationStatistics().getMaxUncommittedExceededCount().getCount();
     }
+
+    @Override
+    public boolean isAdvancedStatisticsEnabled() {
+        return destination.isAdvancedStatisticsEnabled();
+    }
+
+    @Override
+    public void setAdvancedStatisticsEnabled(boolean advancedStatisticsEnabled) {
+        destination.setAdvancedStatisticsEnabled(advancedStatisticsEnabled);
+    }
+
+    @Override
+    public long getNetworkEnqueues() {
+        return destination.getDestinationStatistics().getNetworkEnqueues().getCount();
+    }
+
+    @Override
+    public long getNetworkDequeues() {
+        return destination.getDestinationStatistics().getNetworkDequeues().getCount();
+    }
+
 }
