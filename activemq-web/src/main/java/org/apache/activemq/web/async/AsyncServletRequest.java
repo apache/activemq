dc892d3dfc2f4b7d0402d947f40431ce4e50d92d
Christopher L. Shannon <cshannon@apache.org>
2024-04-11 09:59:46 -0400

    AMQ-9330 - Return 204 code when polling empty destinations (#1203)
    Previously a timeout was thrown and complete was not called so Jetty was
    returning a 500 error
    
    (cherry picked from commit 8b6072d03e273b02288e71cac6eef0539c48ea02)
    (cherry picked from commit 45a1bd54d3e1202d775388cf5b7d63e4c96183a5)

--- activemq-web/src/main/java/org/apache/activemq/web/async/AsyncServletRequest.java
+++ activemq-web/src/main/java/org/apache/activemq/web/async/AsyncServletRequest.java
@@ -27,6 +27,8 @@ import javax.servlet.AsyncListener;
 import javax.servlet.DispatcherType;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletRequestWrapper;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -110,6 +112,17 @@ public class AsyncServletRequest implements AsyncListener  {
         this.expired.set(true);
         if (LOG.isDebugEnabled()) {
             LOG.debug("ActiveMQAsyncRequest " + event + " timeout.");
+        }
+
+        final AsyncContext context = event.getAsyncContext();
+        if (context != null) {
+            // We must call complete and then set the status code to prevent a 500
+            // error. The spec requires a 500 error on timeout unless complete() is called.
+            context.complete();
+            final ServletResponse response = context.getResponse();
+            if (response instanceof HttpServletResponse) {
+                ((HttpServletResponse) response).setStatus(HttpServletResponse.SC_NO_CONTENT);
+            }
         }
     }
 
