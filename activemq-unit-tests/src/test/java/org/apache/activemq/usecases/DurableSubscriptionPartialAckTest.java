98805bc3fc795ba8bda2b0557d8ebdfc7cfdc564
Christopher L. Shannon <cshannon@apache.org>
2024-11-21 10:32:53 -0500

    AMF-9530 - Fix javax imports

--- activemq-unit-tests/src/test/java/org/apache/activemq/usecases/DurableSubscriptionPartialAckTest.java
+++ activemq-unit-tests/src/test/java/org/apache/activemq/usecases/DurableSubscriptionPartialAckTest.java
@@ -16,12 +16,12 @@
  */
 package org.apache.activemq.usecases;
 
-import jakarta.jms.Connection;
-import jakarta.jms.Message;
-import jakarta.jms.MessageProducer;
-import jakarta.jms.Session;
-import jakarta.jms.TextMessage;
-import jakarta.jms.TopicSubscriber;
+import javax.jms.Connection;
+import javax.jms.Message;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.TopicSubscriber;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.ActiveMQSession;
 import org.apache.activemq.broker.BrokerFactory;
