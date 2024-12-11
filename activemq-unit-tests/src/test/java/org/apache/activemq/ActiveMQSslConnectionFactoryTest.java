4d36caf69ae71e1abf82cf2f4c6a45a0cb54361b
pa-deasy <83254994+pa-deasy@users.noreply.github.com>
2024-12-11 15:47:21 -0800

    AMQ-9478 Add unit tests for tcp & ssl socket properties (#1360)
    AMQ-9478 Add unit tests for tcp & ssl socket properties.
    
    (cherry picked from commit de0d68b15a83e4ddc437467d581096bc2b3335de)

--- activemq-unit-tests/src/test/java/org/apache/activemq/ActiveMQSslConnectionFactoryTest.java
+++ activemq-unit-tests/src/test/java/org/apache/activemq/ActiveMQSslConnectionFactoryTest.java
@@ -20,19 +20,28 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.net.Socket;
 import java.security.KeyStore;
 import java.security.SecureRandom;
 import java.security.UnrecoverableKeyException;
 
 import javax.net.ssl.KeyManager;
 import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLSocket;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.TrustManagerFactory;
 
 import org.apache.activemq.broker.BrokerService;
 import org.apache.activemq.broker.SslBrokerService;
+import org.apache.activemq.transport.Transport;
+import org.apache.activemq.transport.TransportFilter;
+import org.apache.activemq.transport.tcp.SslTransport;
+import org.apache.activemq.transport.tcp.TcpTransport;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
+import static org.junit.Assert.assertArrayEquals;
 
 public class ActiveMQSslConnectionFactoryTest extends CombinationTestSupport {
     private static final Log LOG = LogFactory.getLog(ActiveMQSslConnectionFactoryTest.class);
@@ -72,6 +81,26 @@ public class ActiveMQSslConnectionFactoryTest extends CombinationTestSupport {
         brokerStop();
     }
 
+    public void testCreateTcpConnectionWithSocketParameters() throws Exception {
+        // Control case: check that the factory can create an ordinary (non-ssl) connection.
+        String tcpUri = "tcp://localhost:61610?socket.OOBInline=true&socket.keepAlive=true&tcpNoDelay=true";
+        broker = createBroker(tcpUri);
+
+        // This should create the connection.
+        ActiveMQSslConnectionFactory cf = getFactory(tcpUri);
+        connection = (ActiveMQConnection)cf.createConnection();
+        assertNotNull(connection);
+
+        Socket socket = getSocketFromConnection(connection);
+        assertTrue(socket.getOOBInline());
+        assertTrue(socket.getKeepAlive());
+        assertTrue(socket.getTcpNoDelay());
+
+        connection.start();
+        connection.stop();
+        brokerStop();
+    }
+
     public void testCreateFailoverTcpConnectionUsingKnownPort() throws Exception {
         // Control case: check that the factory can create an ordinary (non-ssl) connection.
         broker = createBroker("tcp://localhost:61610?wireFormat.tcpNoDelayEnabled=true");
@@ -98,6 +127,30 @@ public class ActiveMQSslConnectionFactoryTest extends CombinationTestSupport {
         connection = (ActiveMQConnection)cf.createConnection();
         LOG.info("Created client connection");
         assertNotNull(connection);
+        connection.start();
+        connection.stop();
+        brokerStop();
+    }
+
+    public void testCreateSslConnectionWithSocketParameters() throws Exception {
+        // Create SSL/TLS connection with trusted cert from truststore.
+        String sslUri = "ssl://localhost:61611?socket.enabledProtocols=TLSv1.3&socket.enableSessionCreation=true&socket.needClientAuth=true";
+        broker = createSslBroker(sslUri);
+        assertNotNull(broker);
+
+        // This should create the connection.
+        ActiveMQSslConnectionFactory cf = getFactory(sslUri);
+        cf.setTrustStore("server.keystore");
+        cf.setTrustStorePassword("password");
+        connection = (ActiveMQConnection)cf.createConnection();
+        assertNotNull(connection);
+
+        SSLSocket socket = (SSLSocket) getSocketFromConnection(connection);
+        String[] expectedProtocols = {"TLSv1.3"};
+        assertArrayEquals(expectedProtocols, socket.getEnabledProtocols());
+        assertTrue(socket.getEnableSessionCreation());
+        assertTrue(socket.getNeedClientAuth());
+
         connection.start();
         connection.stop();
         brokerStop();
@@ -369,6 +422,20 @@ public class ActiveMQSslConnectionFactoryTest extends CombinationTestSupport {
             cause = cause.getCause();
         }
         return rootCause;
+    }
+
+    private Socket getSocketFromConnection(ActiveMQConnection connection) throws Exception {
+        Transport transport = connection.getTransport();
+        while(!(transport instanceof TcpTransport)) {
+            transport = ((TransportFilter) transport).getNext();
+        }
+        Class<?> transportClass = transport.getClass();
+        if (transport instanceof SslTransport) {
+            transportClass = transportClass.getSuperclass();
+        }
+        Field socket = transportClass.getDeclaredField("socket");
+        socket.setAccessible(true);
+        return (Socket) socket.get(transport);
     }
 
 }
