229de3e3f39617c84954fcb0a031858aa3078efc
Alexandre Chapellon <alexandre.chapellon@hyland.com>
2025-01-15 17:24:46 +0100

    always use simpleAuth (temp approach)

--- assembly/src/docker/entrypoint.sh
+++ assembly/src/docker/entrypoint.sh
@@ -19,16 +19,18 @@
 ################################################################################
 
 # Transport/connection security
-if [ -n "${ACTIVEMQ_CONNECTION_USER}" ]; then
+#if [ -n "${ACTIVEMQ_CONNECTION_USER}" ]; then
   if [ -f "${ACTIVEMQ_HOME}/conf/connection.security.enabled" ]; then
     echo "ActiveMQ Connection Security enabled"
   else
     echo "Enabling ActiveMQ Connection Security"
-    sed -i "s/activemq.username=system/activemq.username=${ACTIVEMQ_CONNECTION_USER}/" ${ACTIVEMQ_HOME}/conf/credentials.properties
-    sed -i "s/activemq.password=manager/activemq.password=${ACTIVEMQ_CONNECTION_PASSWORD}/" ${ACTIVEMQ_HOME}/conf/credentials.properties
+    #sed -i "s/activemq.username=system/activemq.username=${ACTIVEMQ_CONNECTION_USER}/" ${ACTIVEMQ_HOME}/conf/credentials.properties
+    #sed -i "s/activemq.password=manager/activemq.password=${ACTIVEMQ_CONNECTION_PASSWORD}/" ${ACTIVEMQ_HOME}/conf/credentials.properties
+    sed -i "s/activemq.username=system//" ${ACTIVEMQ_HOME}/conf/credentials.properties
+    sed -i "s/activemq.password=manager//" ${ACTIVEMQ_HOME}/conf/credentials.properties
     read -r -d '' REPLACE << END
       <plugins>
-        <simpleAuthenticationPlugin>
+        <simpleAuthenticationPlugin anonymousAccessAllowed=$\{activemq.anonymous.enabled\}>
           <users>
             <authenticationUser username="$\{activemq.username}" password="$\{activemq.password}"/>
           </users>
@@ -42,7 +44,7 @@ END
     sed -i "s/<\/broker>/$REPLACE/" ${ACTIVEMQ_HOME}/conf/activemq.xml
     touch "${ACTIVEMQ_HOME}/conf/connection.security.enabled"
   fi
-fi
+#fi
 
 # JMX security
 if [ -n "${ACTIVEMQ_JMX_USER}" ]; then
