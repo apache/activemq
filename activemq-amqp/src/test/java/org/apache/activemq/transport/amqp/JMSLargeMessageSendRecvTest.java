4809b1eed6a5a2398a2ee4c25aa8eeffd8ebeaff
Jean-Louis Monteiro <jeanouii@gmail.com>
2026-02-12 15:40:58 +0100

    For connections created outside the global field, use try-with-resour… (#1667)
    For connections created outside the global field, use try-with-resources to avoid leaks

--- activemq-amqp/src/test/java/org/apache/activemq/transport/amqp/JMSLargeMessageSendRecvTest.java
+++ activemq-amqp/src/test/java/org/apache/activemq/transport/amqp/JMSLargeMessageSendRecvTest.java
@@ -95,32 +95,32 @@ public class JMSLargeMessageSendRecvTest extends AmqpClientTestSupport {
         String payload = createLargeString(expectedSize);
         assertEquals(expectedSize, payload.getBytes().length);
 
-        Connection connection = JMSClientContext.INSTANCE.createConnection(getBrokerAmqpConnectionURI());
-        long startTime = System.currentTimeMillis();
-        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-        Queue queue = session.createQueue(testName.getMethodName());
-        MessageProducer producer = session.createProducer(queue);
-        TextMessage message = session.createTextMessage();
-        message.setText(payload);
-        producer.send(message);
-        long endTime = System.currentTimeMillis();
-        LOG.info("Returned from send after {} ms", endTime - startTime);
+        try (Connection connection = JMSClientContext.INSTANCE.createConnection(getBrokerAmqpConnectionURI())) {
+            long startTime = System.currentTimeMillis();
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Queue queue = session.createQueue(testName.getMethodName());
+            MessageProducer producer = session.createProducer(queue);
+            TextMessage message = session.createTextMessage();
+            message.setText(payload);
+            producer.send(message);
+            long endTime = System.currentTimeMillis();
+            LOG.info("Returned from send after {} ms", endTime - startTime);
 
-        startTime = System.currentTimeMillis();
-        MessageConsumer consumer = session.createConsumer(queue);
-        connection.start();
-        LOG.info("Calling receive");
-        Message receivedMessage = consumer.receive();
-        assertNotNull(receivedMessage);
-        assertTrue(receivedMessage instanceof TextMessage);
-        TextMessage receivedTextMessage = (TextMessage) receivedMessage;
-        assertNotNull(receivedMessage);
-        endTime = System.currentTimeMillis();
-        LOG.info("Returned from receive after {} ms", endTime - startTime);
-        String receivedText = receivedTextMessage.getText();
-        assertEquals(expectedSize, receivedText.getBytes().length);
-        assertEquals(payload, receivedText);
-        connection.close();
+            startTime = System.currentTimeMillis();
+            MessageConsumer consumer = session.createConsumer(queue);
+            connection.start();
+            LOG.info("Calling receive");
+            Message receivedMessage = consumer.receive();
+            assertNotNull(receivedMessage);
+            assertTrue(receivedMessage instanceof TextMessage);
+            TextMessage receivedTextMessage = (TextMessage) receivedMessage;
+            assertNotNull(receivedMessage);
+            endTime = System.currentTimeMillis();
+            LOG.info("Returned from receive after {} ms", endTime - startTime);
+            String receivedText = receivedTextMessage.getText();
+            assertEquals(expectedSize, receivedText.getBytes().length);
+            assertEquals(payload, receivedText);
+        }
     }
 
     @Test(timeout = 60 * 1000)
@@ -152,34 +152,33 @@ public class JMSLargeMessageSendRecvTest extends AmqpClientTestSupport {
         byte[] payload = createLargeByteArray(expectedSize);
         assertEquals(expectedSize, payload.length);
 
-        Connection connection = JMSClientContext.INSTANCE.createConnection(getBrokerAmqpConnectionURI());
-        long startTime = System.currentTimeMillis();
-        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-        Queue queue = session.createQueue(testName.getMethodName());
-        MessageProducer producer = session.createProducer(queue);
-        BytesMessage message = session.createBytesMessage();
-        message.writeBytes(payload);
-        producer.send(message);
-        long endTime = System.currentTimeMillis();
-        LOG.info("Returned from send after {} ms", endTime - startTime);
+        try (Connection connection = JMSClientContext.INSTANCE.createConnection(getBrokerAmqpConnectionURI())) {
+            long startTime = System.currentTimeMillis();
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Queue queue = session.createQueue(testName.getMethodName());
+            MessageProducer producer = session.createProducer(queue);
+            BytesMessage message = session.createBytesMessage();
+            message.writeBytes(payload);
+            producer.send(message);
+            long endTime = System.currentTimeMillis();
+            LOG.info("Returned from send after {} ms", endTime - startTime);
 
-        startTime = System.currentTimeMillis();
+            startTime = System.currentTimeMillis();
 
-        MessageConsumer consumer = session.createConsumer(queue);
-        connection.start();
-        LOG.info("Calling receive");
-        Message receivedMessage = consumer.receive();
-        assertNotNull(receivedMessage);
-        assertTrue(receivedMessage instanceof BytesMessage);
-        BytesMessage receivedBytesMessage = (BytesMessage) receivedMessage;
-        assertNotNull(receivedMessage);
-        endTime = System.currentTimeMillis();
-        LOG.info("Returned from receive after {} ms", endTime - startTime);
-        byte[] receivedBytes = new byte[(int) receivedBytesMessage.getBodyLength()];
-        receivedBytesMessage.readBytes(receivedBytes);
-        assertEquals(expectedSize, receivedBytes.length);
-        assertArrayEquals(payload, receivedBytes);
-        connection.close();
+            MessageConsumer consumer = session.createConsumer(queue);
+            connection.start();
+            LOG.info("Calling receive");
+            Message receivedMessage = consumer.receive();
+            assertNotNull(receivedMessage);
+            assertTrue(receivedMessage instanceof BytesMessage);
+            BytesMessage receivedBytesMessage = (BytesMessage) receivedMessage;
+            endTime = System.currentTimeMillis();
+            LOG.info("Returned from receive after {} ms", endTime - startTime);
+            byte[] receivedBytes = new byte[(int) receivedBytesMessage.getBodyLength()];
+            receivedBytesMessage.readBytes(receivedBytes);
+            assertEquals(expectedSize, receivedBytes.length);
+            assertArrayEquals(payload, receivedBytes);
+        }
     }
 
     private String createLargeString(int sizeInBytes) {
