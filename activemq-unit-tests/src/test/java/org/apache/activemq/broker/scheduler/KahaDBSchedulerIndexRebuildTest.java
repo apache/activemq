8bb2b9ed70df10dc5826e379d3d695da3cab3b17
Jean-Louis Monteiro <jlmonteiro@tomitribe.com>
2026-02-17 23:07:13 +0100

    Add parallel test annotations to multiple scheduler test classes

--- activemq-unit-tests/src/test/java/org/apache/activemq/broker/scheduler/KahaDBSchedulerIndexRebuildTest.java
+++ activemq-unit-tests/src/test/java/org/apache/activemq/broker/scheduler/KahaDBSchedulerIndexRebuildTest.java
@@ -44,7 +44,10 @@ import org.junit.Test;
 import org.junit.rules.TestName;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.junit.experimental.categories.Category;
+import org.apache.activemq.test.annotations.ParallelTest;
 
+@Category(ParallelTest.class)
 public class KahaDBSchedulerIndexRebuildTest {
 
     static final Logger LOG = LoggerFactory.getLogger(KahaDBSchedulerIndexRebuildTest.class);
