a3e667c50408894bcadae3e755a85667d949560b
JB Onofré <jbonofre@apache.org>
2026-03-18 22:56:41 +0100

    Backport: remove parallel annotation on ProtocolConverterTest (#1793)

--- activemq-stomp/src/test/java/org/apache/activemq/transport/stomp/ProtocolConverterTest.java
+++ activemq-stomp/src/test/java/org/apache/activemq/transport/stomp/ProtocolConverterTest.java
@@ -29,7 +29,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
-@Category(ParallelTest.class)
 public class ProtocolConverterTest {
 
     private final StompTransport transport = mock(StompTransport.class);
