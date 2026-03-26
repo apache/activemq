61bbd5d6303b4ffb2ce1eb0d8ea5d384cfe6b425
Matt Pavlovich <mattrpav@apache.org>
2026-03-26 11:54:16 -0500

    [#1823] Align DestinationView and DestinationViewMBean (#1824) (#1841)
    (cherry picked from commit 5658dc7aadf87feec1c293ac0fa9bb9f607f99aa)

--- activemq-broker/src/main/java/org/apache/activemq/broker/jmx/DestinationView.java
+++ activemq-broker/src/main/java/org/apache/activemq/broker/jmx/DestinationView.java
@@ -137,6 +137,12 @@ public class DestinationView implements DestinationViewMBean {
         return messageStore != null ? messageStore.getMessageStoreStatistics().getMessageSize().getTotalSize() : 0;
     }
 
+    @Override
+    public long getMessagesCachedCount() {
+        return destination.getDestinationStatistics().getMessagesCached().getCount();
+    }
+
+    @Deprecated(forRemoval = true) // see: getMessagesCachedCount() instead. This method name is inconsistent
     public long getMessagesCached() {
         return destination.getDestinationStatistics().getMessagesCached().getCount();
     }
@@ -438,10 +444,12 @@ public class DestinationView implements DestinationViewMBean {
         return destination.getMaxProducersToAudit();
     }
 
+    @Override
     public boolean isEnableAudit() {
         return destination.isEnableAudit();
     }
 
+    @Override
     public void setEnableAudit(boolean enableAudit) {
         destination.setEnableAudit(enableAudit);
     }
