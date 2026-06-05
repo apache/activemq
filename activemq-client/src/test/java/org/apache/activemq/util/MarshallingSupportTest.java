6fc46e520bac7d9763ba5762cc2de603f19ed517
Christopher L. Shannon <cshannon@apache.org>
2026-06-05 18:41:51 -0400

    Add validation for WireFormatInfo (#2080)
    The max number of properties in the map for the WireFormatInfo
    command was lowered to 64 and a new max value buffer size was
    added to validate buffers created during unmarshalling of the properties
    map. The max buffer allowed has been set to 512 bytes.
    
    This also adds a maxDepth check for handling nested collections inside
    the map. It is set to 0 which will block list/map from being used as
    a value for a property.

--- activemq-client/src/test/java/org/apache/activemq/util/MarshallingSupportTest.java
+++ activemq-client/src/test/java/org/apache/activemq/util/MarshallingSupportTest.java
@@ -16,8 +16,20 @@
  */
 package org.apache.activemq.util;
 
+import static org.apache.activemq.util.MarshallingSupport.unmarshalPrimitiveList;
+import static org.apache.activemq.util.MarshallingSupport.unmarshalPrimitiveMap;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import org.junit.Test;
@@ -41,5 +53,88 @@ public class MarshallingSupportTest {
         String str = MarshallingSupport.propertiesToString(props);
         Properties props2 = MarshallingSupport.stringToProperties(str);
         assertEquals(props, props2);
+    }
+
+    @Test
+    public void testUnmarshalPrimitiveMap() throws Exception {
+        ByteArrayOutputStream bytesOut = new ByteArrayOutputStream();
+        DataOutputStream dataOut = new DataOutputStream(bytesOut);
+
+        Map<String, Object> testMap = new HashMap<>();
+        testMap.put("key1", "value1");
+        // nested map with nested collection, should require depth of 2 being allowed
+        testMap.put("key2", Map.of("string","string", "nested",
+                List.of("one")));
+
+        MarshallingSupport.marshalPrimitiveMap(testMap, dataOut);
+        DataInputStream dataIn = new DataInputStream(new ByteArrayInputStream(bytesOut.toByteArray()));
+
+        //allowed
+        assertEquals(2, unmarshalPrimitiveMap(dataIn, 100, 1024, 10).size());
+
+        // too many properties
+        dataIn.reset();
+        assertException(() -> unmarshalPrimitiveMap(dataIn, 1, 1024, 10),
+                "map is larger than the allowed size");
+
+        // buffers too large
+        dataIn.reset();
+        assertException(() -> unmarshalPrimitiveMap(dataIn, 100, 2, 10),
+                "Max buffer size: 2 exceeded, size: 6");
+
+        // max depth violated
+        dataIn.reset();
+        assertException(() -> unmarshalPrimitiveMap(dataIn, 100, 1024, 1),
+                "Max unmarshaling depth: 1 exceeded, depth: 2");
+    }
+
+    @Test
+    public void testUnmarshalPrimitiveList() throws Exception {
+        ByteArrayOutputStream bytesOut = new ByteArrayOutputStream();
+        DataOutputStream dataOut = new DataOutputStream(bytesOut);
+
+        List<Object> testList = new ArrayList<>();
+        testList.add("value");
+        // nested list with nested collection, should require depth of 2 being allowed
+        testList.add(List.of(List.of("one")));
+        testList.add(Map.of("one","two","three","four"));
+
+        MarshallingSupport.marshalPrimitiveList(testList, dataOut);
+        DataInputStream dataIn = new DataInputStream(new ByteArrayInputStream(bytesOut.toByteArray()));
+
+        //allowed
+        assertEquals(3, unmarshalPrimitiveList(dataIn, 100, 1024, 2).size());
+
+        // Max property size applies to maps, so this will check the nested map in the list
+        // and should fail because the map has 2 properties
+        dataIn.reset();
+        assertException(() -> unmarshalPrimitiveList(dataIn, 1, 1024, 2),
+                "map is larger than the allowed size");
+
+        // buffers too large
+        dataIn.reset();
+        assertException(() -> unmarshalPrimitiveList(dataIn, 100, 2, 2),
+                "Max buffer size: 2 exceeded, size: 3");
+
+        // max depth violated
+        dataIn.reset();
+        assertException(() -> unmarshalPrimitiveList(dataIn, 100, 1024, 1),
+                "Max unmarshaling depth: 1 exceeded, depth: 2");
+    }
+
+    private void assertException(ThrowableRunnable<IOException> runnable, String error) throws Exception {
+        try {
+            // Trigger unmarshaling the properties. This should throw an error because
+            // the buffer is too large
+            runnable.run();
+            fail("should have IO exception");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            assertTrue(e.getMessage().contains(error));
+        }
+    }
+
+    private interface ThrowableRunnable<T extends Throwable> {
+        void run() throws T;
     }
 }
