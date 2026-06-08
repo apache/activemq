apache/activemq

commit ea5a33ce87a203e0c8335f46f976e0a29c90f18a

Adding missing jsp escapes (#2089)

activemq-web-console/src/main/webapp/browse.jsp
@@ -48,7 +48,7 @@
 <td><a href="<c:url value="message.jsp">
                  <c:param name="id" value="${row.JMSMessageID}" />
                  <c:param name="JMSDestination" value="${requestContext.queueBrowser.JMSDestination}"/></c:url>"
-    title="<c:out value="${row.properties}"/>">${row.JMSMessageID}</a></td>
+    title="<c:out value="${row.properties}"/>"><c:out value="${row.JMSMessageID}"/></a>
 <td><c:out value="${row.JMSCorrelationID}"/></td>
 <td><jms:persistent message="${row}"/></td>
 <td><c:out value="${row.JMSPriority}"/></td>

activemq-web-console/src/main/webapp/queueConsumers.jsp
@@ -60,7 +60,7 @@
 	<td>
         <c:choose>
             <c:when test="${row.network}">
-                <a href="network.jsp">${row.clientId}</a><br/>
+                <a href="network.jsp"><c:out value="${row.clientId}"/></a><br/>
             </c:when>
             <c:otherwise>
                 <a href="<c:out value="connection.jsp?connectionID=${row.clientId}"/>"><c:out value="${row.clientId}" /></a><br/>
@@ -90,4 +90,3 @@
 
 </body>
 </html>
-	

commit 9ee7367cc027053c348bcbf5ceaf3a6bf4725cb5

Add back missing closing tag
Fix typo introduced in #2089

activemq-web-console/src/main/webapp/browse.jsp
@@ -48,7 +48,7 @@
 <td><a href="<c:url value="message.jsp">
                  <c:param name="id" value="${row.JMSMessageID}" />
                  <c:param name="JMSDestination" value="${requestContext.queueBrowser.JMSDestination}"/></c:url>"
-    title="<c:out value="${row.properties}"/>"><c:out value="${row.JMSMessageID}"/></a>
+    title="<c:out value="${row.properties}"/>"><c:out value="${row.JMSMessageID}"/></a></td>
 <td><c:out value="${row.JMSCorrelationID}"/></td>
 <td><jms:persistent message="${row}"/></td>
 <td><c:out value="${row.JMSPriority}"/></td>