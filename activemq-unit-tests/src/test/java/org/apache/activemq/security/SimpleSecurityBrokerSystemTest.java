106d18106865e1dc935ac713d2a45a546fbe9180
Christopher L. Shannon <cshannon@apache.org>
2026-07-15 11:14:30 -0400

    Ensure composite temp dests are handled in AuthorizationBroker (#2219) (#2221)
    For temporary destinations we need to iterate over the list of composite
    destinations and compute the union using the same strategy as
    DefaultAuthorizationMap.
    
    (cherry picked from commit 91e35e07def30d4fc7602bb4b2f775c56f02cf76)

--- activemq-unit-tests/src/test/java/org/apache/activemq/security/SimpleSecurityBrokerSystemTest.java
+++ activemq-unit-tests/src/test/java/org/apache/activemq/security/SimpleSecurityBrokerSystemTest.java
@@ -16,7 +16,12 @@
  */
 package org.apache.activemq.security;
 
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 import java.lang.management.ManagementFactory;
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.security.Principal;
 import java.util.Arrays;
@@ -26,12 +31,22 @@ import java.util.Map;
 import java.util.Set;
 
 import junit.framework.Test;
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQMessageTransformation;
+import org.apache.activemq.ActiveMQSession;
 import org.apache.activemq.CombinationTestSupport;
 import org.apache.activemq.broker.Broker;
 import org.apache.activemq.broker.BrokerPlugin;
 import org.apache.activemq.broker.BrokerService;
+import org.apache.activemq.command.ActiveMQDestination;
 import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.activemq.command.ActiveMQTempQueue;
+import org.apache.activemq.command.ActiveMQTempTopic;
 import org.apache.activemq.command.ActiveMQTopic;
+import org.apache.activemq.command.ConsumerId;
+import org.apache.activemq.command.ConsumerInfo;
+import org.apache.activemq.command.ExceptionResponse;
+import org.apache.activemq.command.Response;
 import org.apache.activemq.filter.DestinationMap;
 import org.apache.activemq.jaas.GroupPrincipal;
 import org.slf4j.Logger;
@@ -111,6 +126,175 @@ public class SimpleSecurityBrokerSystemTest extends SecurityTestSupport {
         assertEquals("system",  m.getStringProperty("JMSXUserID"));
     }
 
+    public void testComposite() throws Exception {
+        // user is authorized for both
+        destination = new ActiveMQQueue("USERS.queue.1,USERS.queue.2");
+        Connection connection = factory.createConnection("user", "password");
+        connections.add(connection);
+        connection.start();
+
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        sendMessages(session, destination, 1);
+        MessageConsumer consumer = session.createConsumer(destination);
+        // there should be 2 messages because a copy gets sent to each dest in the composite list
+        assertNotNull(consumer.receive(100));
+        assertNotNull(consumer.receive(100));
+    }
+
+    public void testCompositeFail() throws Exception {
+        destination = new ActiveMQQueue("USERS.queue.1,GUEST.queue.2");
+        Connection connection = factory.createConnection("guest", "password");
+        connections.add(connection);
+        connection.start();
+
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        assertThrows(JMSSecurityException.class, () -> sendMessages(session, destination, 1));
+    }
+
+    public void testCompositeFailMulti() throws Exception {
+        destination = new ActiveMQQueue("GUEST.queue.1,GUEST.queue.2");
+        // Add child composite that is not authorized
+        Arrays.stream(destination.getCompositeDestinations()).findFirst().orElseThrow().setCompositeDestinations(
+                new ActiveMQDestination[]{new ActiveMQQueue("USERS.queue.1")});
+        Connection connection = factory.createConnection("guest", "password");
+        connections.add(connection);
+        connection.start();
+
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        assertThrows(JMSSecurityException.class, () -> sendMessages(session, destination, 1));
+    }
+
+    public void testCompositeTempWriteDestsExist() throws Exception {
+        // pre-create dests
+        broker.getBroker().addDestination(broker.getAdminConnectionContext(),
+                new ActiveMQQueue("USERS.queue.1"), false);
+        broker.getBroker().addDestination(broker.getAdminConnectionContext(),
+                new ActiveMQQueue("GUEST.queue.1"), false);
+
+        // Test that authorization works when dests already exist
+        testCompositeTempWrite();
+    }
+
+    public void testCompositeTempWrite() throws Exception {
+        // Test that authorization works on composite temp
+        destination = new ActiveMQTempQueue("queue://USERS.queue.1,queue://GUEST.queue.1");
+        Connection connection = factory.createConnection("guest", "password");
+        connections.add(connection);
+        connection.start();
+
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        JMSSecurityException e = assertThrows(JMSSecurityException.class, () -> sendMessages(session, destination, 1));
+        assertTrue(e.getMessage().contains("User guest is not authorized to write to:"));
+    }
+
+    public void testCompositeTempMultiLevel() throws Exception {
+        broker.getBroker().addDestination(broker.getAdminConnectionContext(),
+                new ActiveMQQueue("GUEST.queue.1"), false);
+        broker.getBroker().addDestination(broker.getAdminConnectionContext(),
+                new ActiveMQQueue("GUEST.queue.2"), false);
+        broker.getBroker().addDestination(broker.getAdminConnectionContext(),
+                new ActiveMQQueue("USERS.queue.1"), false);
+
+        // Test that authorization works on composite temp
+        // The top level composite list would be authorized, verify it fails because one of the
+        // composite dests itself contains a dest that isn't authorized
+        destination = new ActiveMQTempQueue("queue://GUEST.queue.1,queue://GUEST.queue.2");
+        Arrays.stream(destination.getCompositeDestinations()).findFirst().orElseThrow().setCompositeDestinations(
+                new ActiveMQDestination[]{new ActiveMQQueue("USERS.queue.1")});
+        Connection connection = factory.createConnection("guest", "password");
+        connections.add(connection);
+        connection.start();
+
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        JMSSecurityException e = assertThrows(JMSSecurityException.class, () -> sendMessages(session, destination, 1));
+        assertTrue(e.getMessage().contains("User guest is not authorized to write to:"));
+    }
+
+    public void testCompositeTempRead() throws Exception {
+        broker.getBroker().addDestination(broker.getAdminConnectionContext(),
+                new ActiveMQQueue("USERS.queue.1"), false);
+        broker.getBroker().addDestination(broker.getAdminConnectionContext(),
+                new ActiveMQQueue("GUEST.queue.1"), false);
+
+        // Test that authorization works on composite temp
+        testCompositeTempRead(new ActiveMQTempQueue("queue://USERS.queue.1,queue://GUEST.queue.1"));
+    }
+
+    public void testCompositeTempReadMultiLevel() throws Exception {
+        broker.getBroker().addDestination(broker.getAdminConnectionContext(),
+                new ActiveMQQueue("GUEST.queue.1"), false);
+        broker.getBroker().addDestination(broker.getAdminConnectionContext(),
+                new ActiveMQQueue("GUEST.queue.2"), false);
+        broker.getBroker().addDestination(broker.getAdminConnectionContext(),
+                new ActiveMQQueue("USERS.queue.1"), false);
+
+        // test unauthorized dest that is a child of another composite fails
+        ActiveMQDestination dest = new ActiveMQTempQueue("queue://GUEST.queue.1,queue://GUEST.queue.2");
+                Arrays.stream(dest.getCompositeDestinations()).findFirst().orElseThrow().setCompositeDestinations(
+                        new ActiveMQDestination[]{new ActiveMQQueue("USERS.queue.1")});
+
+        // Test that authorization works on composite temp
+        testCompositeTempRead(dest);
+    }
+
+    private void testCompositeTempRead(ActiveMQDestination dest) throws Exception {
+        destination = dest;
+
+        // Test that authorization works on composite temp
+        ActiveMQConnection connection = (ActiveMQConnection) factory.createConnection("guest", "password");
+        connections.add(connection);
+        connection.start();
+
+        // Consumers should be blocked
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Method nextId = ActiveMQSession.class.getDeclaredMethod("getNextConsumerId");
+        nextId.setAccessible(true);
+
+        // the client normally blocks this so bypass the check to test the broker auth
+        ConsumerInfo info = new ConsumerInfo((ConsumerId) nextId.invoke(session));
+        info.setDestination(destination);
+        Object result = connection.getTransport().request(info, 500);
+        assertTrue(result instanceof ExceptionResponse);
+        assertTrue(((Response) result).isException());
+        assertTrue(((ExceptionResponse) result).getException() instanceof SecurityException);
+        assertTrue(((ExceptionResponse) result).getException().getMessage()
+                .contains("User guest is not authorized to read from"));
+    }
+
+    public void testCompositeTempQueueWithTopics() throws Exception {
+        broker.getBroker().addDestination(broker.getAdminConnectionContext(),
+                new ActiveMQTopic("USERS.topic.1"), false);
+        broker.getBroker().addDestination(broker.getAdminConnectionContext(),
+                new ActiveMQTopic("GUEST.topic.1"), false);
+
+        // Test that authorization works with contained topic instead of queues
+        destination = new ActiveMQTempQueue("topic://USERS.topic.1,topic://GUEST.topic.1");
+        Connection connection = factory.createConnection("guest", "password");
+        connections.add(connection);
+        connection.start();
+
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        JMSSecurityException e = assertThrows(JMSSecurityException.class, () -> sendMessages(session, destination, 1));
+        assertTrue(e.getMessage().contains("User guest is not authorized to write to:"));
+    }
+
+    public void testCompositeTempTopicWithQueues() throws Exception {
+        broker.getBroker().addDestination(broker.getAdminConnectionContext(),
+                new ActiveMQQueue("USERS.queue.1"), false);
+        broker.getBroker().addDestination(broker.getAdminConnectionContext(),
+                new ActiveMQQueue("GUEST.queue.1"), false);
+
+        // Test that authorization works with contained queues when temp is of type topic
+        destination = new ActiveMQTempTopic("queue://USERS.queue.1,queue://GUEST.queue.1");
+        Connection connection = factory.createConnection("guest", "password");
+        connections.add(connection);
+        connection.start();
+
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        JMSSecurityException e = assertThrows(JMSSecurityException.class, () -> sendMessages(session, destination, 1));
+        assertTrue(e.getMessage().contains("User guest is not authorized to write to:"));
+    }
+
     public static AuthorizationMap createAuthorizationMap() {
         DestinationMap readAccess = new DefaultAuthorizationMap();
         readAccess.put(new ActiveMQQueue(">"), ADMINS);
@@ -141,7 +325,19 @@ public class SimpleSecurityBrokerSystemTest extends SecurityTestSupport {
         adminAccess.put(new ActiveMQQueue(">"), USERS);
         adminAccess.put(new ActiveMQQueue(">"), GUESTS);
 
-        return new SimpleAuthorizationMap(writeAccess, readAccess, adminAccess);
+        var authorizationMap = new SimpleAuthorizationMap(writeAccess, readAccess, adminAccess);
+        var tempDestinationAuthorizationEntry = new TempDestinationAuthorizationEntry();
+        try {
+            tempDestinationAuthorizationEntry.setAdmin("admins, users, guests");
+            tempDestinationAuthorizationEntry.setRead("admins, users, guests");
+            tempDestinationAuthorizationEntry.setWrite("admins, users, guests");
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+        authorizationMap.setTempDestinationAuthorizationEntry(tempDestinationAuthorizationEntry);
+
+        return authorizationMap;
+
     }
 
     public static class SimpleAuthenticationFactory implements BrokerPlugin {
